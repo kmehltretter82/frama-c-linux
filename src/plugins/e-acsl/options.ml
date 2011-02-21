a1f1dfd3320ba90af4403b45166820b232c0537c
Julien Signoles <julien.signoles@cea.fr>
2011-02-21 15:17:44 +0000

    new e-acsl plug-in (only a useless skeleton right now)

---
+++ src/plugins/e-acsl/options.ml
@@ -0,0 +1,35 @@
+(**************************************************************************)
+(*                                                                        *)
+(*  This file is part of Frama-C.                                         *)
+(*                                                                        *)
+(*  Copyright (C) 2007-2010                                               *)
+(*    CEA (Commissariat à l'Énergie Atomique)                             *)
+(*                                                                        *)
+(*  you can redistribute it and/or modify it under the terms of the GNU   *)
+(*  Lesser General Public License as published by the Free Software       *)
+(*  Foundation, version 2.1.                                              *)
+(*                                                                        *)
+(*  It is distributed in the hope that it will be useful,                 *)
+(*  but WITHOUT ANY WARRANTY; without even the implied warranty of        *)
+(*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *)
+(*  GNU Lesser General Public License for more details.                   *)
+(*                                                                        *)
+(*  See the GNU Lesser General Public License version 2.1                 *)
+(*  for more details (enclosed in the file licenses/LGPLv2.1).            *)
+(*                                                                        *)
+(**************************************************************************)
+
+module P = Plugin.Register
+  (struct
+     let name = "E-ACSL"
+     let shortname = "e-acsl"
+     let help = "Executable ANSI/ISO C Specification Language --- runtime \
+assertion checker generator"
+   end)
+include P
+
+(*
+Local Variables:
+compile-command: "make"
+End:
+*)
