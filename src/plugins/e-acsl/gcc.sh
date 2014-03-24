982ff45905a7d9f7083d0f22252122267677a848
Julien Signoles <julien.signoles@cea.fr>
2014-03-24 10:43:52 +0100

    info messages

--- src/plugins/e-acsl/gcc.sh
+++ src/plugins/e-acsl/gcc.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-gcc -std=c99 -pedantic -Wall -Wno-long-long -Wno-attributes -Wno-unused-but-set-variable -fno-builtin -o $1.out ./share/e-acsl/e_acsl.c ./share/e-acsl/memory_model/e_acsl_bittree.c ./share/e-acsl/memory_model/e_acsl_mmodel.c $1 -lgmp && $1.out
+#export SHARE=`frama-c -print-share-path`
+export SHARE=./share
+
+echo compiling $1
+gcc -std=c99 -pedantic -Wall -Wno-long-long -Wno-attributes -Wno-unused-but-set-variable -fno-builtin -o $1.out $SHARE/e-acsl/e_acsl.c $SHARE/e-acsl/memory_model/e_acsl_bittree.c $SHARE/e-acsl/memory_model/e_acsl_mmodel.c $1 -lgmp
+
+echo executing $1
+./$1.out
