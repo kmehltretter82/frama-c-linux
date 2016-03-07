30b7d40b0fac01c1cbd9702b399f933e283b6325
Kostyantyn Vorobyov <kostyantyn.vorobyov@cea.fr>
2016-03-07 16:37:45 +0100

    [Tests] Added new non-regression tests for \initialized and \offset predicates

---
+++ src/plugins/e-acsl/tests/e-acsl-runtime/initialized.c
@@ -0,0 +1,66 @@
+/* run.config
+   COMMENT: Behaviours of the \initialized E-ACSL predicate
+*/
+
+#include <stdlib.h>
+
+int A = 0;
+int B;
+
+int main(void) {
+  /* All globals are initialized, even if the initializer is not given */
+  int *p = &A;
+  int *q = &B;
+  /*@assert \initialized(&A) ; */
+  /*@assert \initialized(&B) ; */
+  /*@assert \initialized(p) ;  */
+  /*@assert \initialized(q) ;  */
+
+  /* A local variable without an initializer is uninitialized */
+  int a = 0;
+  int b;
+  long c[2] = { 1, 1 };
+  long d[2];
+  p = &a;
+  q = &b;
+
+  /*@assert \initialized(&a) ; */
+  /*@assert ! \initialized(&b) ; */
+  /*@assert \initialized(p) ; */
+  /*@assert ! \initialized(q) ; */
+  /*@assert \initialized(&c) ; */
+  /*@assert ! \initialized(&d) ; */
+
+  /* Local variables also initialized by assignments */
+  b = 0;
+  d[0] = 1;
+  /*@assert \initialized(&b) ; */
+  /*@assert \initialized(q) ;  */
+  /*@assert ! \initialized(&d) ;  */
+  d[1] = 1;
+  /*@assert \initialized(&d) ;  */
+
+  /* Malloc allocates un-initialized memory */
+  p = (int*)malloc(sizeof(int*));
+  /*@assert ! \initialized(p) ; */
+
+  /* Calloc allocates initialized memory */
+  q = (int*)calloc(1, sizeof(int));
+  /*@ assert \initialized(q) ; */
+
+  /* Block reallocared using `realloc' carries initialization of the of the
+   * existing fragment but does not initialize the newly allocated one */
+  q = (int*)realloc(q, 2*sizeof(int));
+  /*@assert \initialized(q) ; */
+  q++;
+  /*@assert ! \initialized(q) ; */
+  q--;
+
+  /* Initialized on un-allocated regions is always false. This does not lead
+   * to undefined bevaviours in production mode or assertion failures in debug
+   * mode. */
+  free(p);
+  free(q);
+  /*@assert ! \initialized(p) ; */
+  /*@assert ! \initialized(q) ; */
+}
