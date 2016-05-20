65d43430574ac3d92a430f8128fddf33b910ba33
Julien Signoles <julien.signoles@cea.fr>
2016-05-20 19:29:05 +0200

    doc infer.mli

--- src/plugins/e-acsl/interval.mli
+++ src/plugins/e-acsl/interval.mli
@@ -28,21 +28,30 @@
 type interv = private { lower: Integer.t; upper: Integer.t }
 include Datatype.S with type t = interv
 
+(** Environment for interval computations. *)
 module Env: sig
   val clear: unit -> unit
   val add: Cil_types.logic_var -> interv -> unit
+(** Map an interval to a given logic variable *)
 end
 
 val interv_of_typ: Cil_types.typ -> t
+(** @return the smallest interval which contains the given C type. *)
 
 val add: t -> Integer.t -> t
+(** Extend an interval in order to contain the given integer. *)
+
+(** {3 Intervals as a lattice} *)
+
 val join: t -> t -> t
 val meet: t -> t -> t
+
+(** {3 Inference system} *)
 
 exception Not_an_integer
 val infer: Cil_types.term -> t
 (** [infer t] infers the smallest possible integer interval which the values
-    of the term can fit in. Assume than the type of [t] is an integral type.
+    of the term can fit in.
     @raise Not_an_integer if the type of the term is not a subtype of
     [Linteger]. *)
 
