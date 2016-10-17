d16d7a35d17faeb4dea28272518c5c4871866f8d
Kostyantyn Vorobyov <kostyantyn.vorobyov@cea.fr>
2016-10-17 14:31:33 +0200

    Update bts2252 test to include pointer arithmetic operation

--- src/plugins/e-acsl/tests/bts/bts2252.c
+++ src/plugins/e-acsl/tests/bts/bts2252.c
@@ -2,9 +2,12 @@
    COMMENT: bts #2252, failures due to typing of offsets
 */
 
+int *p;
 int main(void) {
   int i = -1;
   int t[10];
   /*@ assert ! \valid_read(t+i); */
+  p = t;
+  /*@ assert ! \valid_read(p+i); */
   return 0;
 }
