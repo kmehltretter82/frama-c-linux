7f1a8d44ebe7fc209fb023df374021d09ce5887a
Kostyantyn Vorobyov <kostyantyn.vorobyov@cea.fr>
2017-01-25 18:04:10 +0100

    Change bts2252 test case to the original program from ITC benchmark

--- src/plugins/e-acsl/tests/bts/bts2252.c
+++ src/plugins/e-acsl/tests/bts/bts2252.c
@@ -2,12 +2,24 @@
    COMMENT: bts #2252, failures due to typing of offsets
 */
 
-int *p;
-int main(void) {
-  int i = -1;
-  int t[10];
-  /*@ assert ! \valid_read(t+i); */
-  p = t;
-  /*@ assert ! \valid_read(p+i); */
-  return 0;
+#include <stdlib.h>
+
+int main() {
+  char* srcbuf = "Test Code";
+  int i, loc = 1;
+
+  char * destbuf = (char*)malloc(10*sizeof(char));
+  char ch = 'o';
+
+  if (destbuf != NULL) {
+    for (i = -1; i < 0; i++) {
+      /*@ assert ! \valid_read(srcbuf + i); */
+      if (srcbuf[i] == ch) { /* ERROR: Buffer Underrun */
+        loc = i;
+      }
+    }
+
+    strncpy (&destbuf[loc], &srcbuf[loc], 1);
+    free(destbuf);
+  }
 }
