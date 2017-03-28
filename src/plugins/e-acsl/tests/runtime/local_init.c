3e2d2787a74cbc796204d40904cf81c9fca5ca49
Virgile Prevosto <virgile.prevosto@m4x.org>
2017-03-28 09:23:32 +0200

    Add test case for local_init with annotation and update gcc-e-acsl script

--- src/plugins/e-acsl/tests/runtime/local_init.c
+++ src/plugins/e-acsl/tests/runtime/local_init.c
@@ -1,6 +1,8 @@
 /* run.config
    COMMENT: test of a local initializer which contains an annotation
-   STDOPT: #"-val -value-verbose 0 -lib-entry -e-acsl-prepare -machdep gcc_x86_64 -then"
+   LOG: gen_@PTEST_NAME@.c
+   OPT: -e-acsl-prepare -val -lib-entry -then -e-acsl -then-last -load-script tests/print.cmxs -print -ocode @PTEST_DIR@/result/gen_@PTEST_NAME@.c
+   EXEC: ./scripts/testrun.sh @PTEST_NAME@ runtime "" "--frama-c=@frama-c@ -F -val -F -lib-entry --then --e-acsl-extra=-no-lib-entry --e-acsl-extra=-no-val"
 */
 
 int X = 0;
