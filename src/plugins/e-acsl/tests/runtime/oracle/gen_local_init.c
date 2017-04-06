a438982be3d3ba4b62a0ef1c42c5ac3ca1a64eac
Virgile Prevosto <virgile.prevosto@m4x.org>
2017-04-06 10:20:53 +0200

    update oracle

--- src/plugins/e-acsl/tests/runtime/oracle/gen_local_init.c
+++ src/plugins/e-acsl/tests/runtime/oracle/gen_local_init.c
@@ -7,7 +7,8 @@ int f(void)
   /*@ assert Value: mem_access: \valid_read(p); */
   {
     int __gen_e_acsl_valid_read;
-    __gen_e_acsl_valid_read = __e_acsl_valid_read((void *)p,sizeof(int));
+    __gen_e_acsl_valid_read = __e_acsl_valid_read((void *)p,sizeof(int),
+                                                  (void *)p,(void *)(& p));
     __e_acsl_assert(__gen_e_acsl_valid_read,(char *)"Assertion",(char *)"f",
                     (char *)"Value: mem_access: \\valid_read(p)",12);
   }
