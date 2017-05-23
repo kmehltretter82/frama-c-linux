bd6ceb876e517e40edb79f115e0bc7b28f41a986
Kostyantyn Vorobyov <kostyantyn.vorobyov@cea.fr>
2017-05-23 17:26:05 +0200

    Remove dead code from e_acsl_alias.h

--- src/plugins/e-acsl/share/e-acsl/e_acsl_alias.h
+++ src/plugins/e-acsl/share/e-acsl/e_acsl_alias.h
@@ -32,26 +32,7 @@
 /* Concatenation of 2 tokens */
 # define preconcat(x,y) x ## y
 # define concat(x,y) preconcat(x,y)
-# define ext_prefix __e_acsl_
-# define export_alias(_n) concat(public_prefix, _n)
-
-/* 'strong' and 'weak' aliases are not portable */
-
-/** Define `aliasname` as a strong alias for `name`. */
-# define strong_alias(name, aliasname) _strong_alias(name, aliasname)
-# define _strong_alias(name, aliasname) \
-  extern __typeof (name) aliasname __attribute__ ((alias (#name)));
-
-/** Define `aliasname` as a weak alias for `name`. */
-# define weak_alias(name, aliasname) _weak_alias (name, aliasname)
-# define _weak_alias(name, aliasname) \
-  extern __typeof (name) aliasname __attribute__ ((weak, alias (#name)));
-
-/** Prefix added to public functions of E-ACSL public API */
-# define public_prefix __e_acsl_
-/** Make a strong alias from some function named `f` to __e_acsl_f */
-# define public_alias(f) strong_alias(f, concat(public_prefix,f))
-/** Make a strong alias from some function named `f1` to __e_acsl_f2 */
-# define public_alias2(f1,f2) strong_alias(f1, concat(public_prefix,f2))
+# define export_prefix __e_acsl_
+# define export_alias(_n) concat(export_prefix, _n)
 
 #endif
