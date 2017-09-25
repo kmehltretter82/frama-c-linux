4a782b94acc5f18e53dbac1c6ac8a4266d353ca9
François Bobot <francois.bobot@cea.fr>
2017-09-25 11:21:17 +0200

    [Eva] puts the documentations in the .mli
          which was already present...

--- src/plugins/value/eval.ml
+++ src/plugins/value/eval.ml
@@ -240,9 +240,6 @@ type ('loc, 'value) call = {
   arguments: ('loc, 'value) argument list;
   rest: (exp * ('loc, 'value) assigned) list;
   return: varinfo option;
-  (** Special variable (same for all calls) that makes the
-      link between the result of the function and the assignment in
-      the caller. *)
   recursive: bool;
 }
 
