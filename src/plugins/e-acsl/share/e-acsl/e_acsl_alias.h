484d9a16d0ca5ffd317474d76983ed40c8fba008
Kostyantyn Vorobyov <kostyantyn.vorobyov@cea.fr>
2017-09-28 10:32:36 +0200

    Couple of comments

--- src/plugins/e-acsl/share/e-acsl/e_acsl_alias.h
+++ src/plugins/e-acsl/share/e-acsl/e_acsl_alias.h
@@ -32,7 +32,9 @@
 /* Concatenation of 2 tokens */
 # define preconcat(x,y) x ## y
 # define concat(x,y) preconcat(x,y)
+/** Prefix of public functions */
 # define export_prefix __e_acsl_
+/** Add public prefix to an identifier */
 # define export_alias(_n) concat(export_prefix, _n)
 
 #endif
