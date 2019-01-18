4ce11c1b31dd824bf2ca9f24d5eb444c6e7fb0e7
Andre Maroneze <andre.oliveiramaroneze@cea.fr>
2019-01-18 19:59:07 +0100

    [fc-script] use relative paths in results of find-fun

--- share/analysis-scripts/find_fun.py
+++ share/analysis-scripts/find_fun.py
@@ -101,8 +101,8 @@ else:
    if possible_declarators != []:
       print("Possible declarations for function '%s' in the following file(s):"
             % fname)
-      print("  " + "\n  ".join(possible_declarators))
+      print("  " + "\n  ".join(map(os.path.relpath, possible_declarators)))
    if possible_definers != []:
       print("Possible definitions for function '%s' in the following file(s):"
             % fname)
-      print("  " + "\n  ".join(possible_definers))
+      print("  " + "\n  ".join(map(os.path.relpath, possible_definers)))
