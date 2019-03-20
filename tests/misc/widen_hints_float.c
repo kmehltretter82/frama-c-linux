419daba1c7306f8d2991cd264751cb132590489f
Valentin Perrelle <valentin.perrelle@cea.fr>
2019-03-20 14:53:45 +0100

    [Eva] More float widen hints tests

--- tests/misc/widen_hints_float.c
+++ tests/misc/widen_hints_float.c
@@ -1,7 +1,12 @@
+/* run.config*
+   STDOPT: #"-val-subdivide-non-linear 20"
+*/
+
+
 #include "__fc_builtin.h"
+#include <math.h>
 
-int main() {
-
+void parabola(void) {
   /*
     The expression is a parabola p
       where p([0.;64.]) = [0.;64.] and p([64.;128.]) = [0.;64.].
@@ -28,6 +33,42 @@ int main() {
   for(int i = 0; i < 100; i++){
     f3 = (64*64 - (f3 - 64) * (f3 - 64))/64;
   }
+}
 
-  return 0;
+void trigo(void) {
+  double f1 = 0.0;
+
+  for (int i = 0; i < 100; i++) {
+    f1 = sin(f1 + Frama_C_double_interval(-0.01, 0.01));
+  }
+}
+
+void first_order_filter(void) {
+  float f1 = 0.0;
+  for (int i = 1; i < 100; i++)
+    f1 = f1 * 0.8 + Frama_C_double_interval(-1.0, 1.0);
+
+  float f2 = 0.0;
+  //@ loop widen_hints f2, 5., -5.;
+  for (int i = 1; i < 100; i++)
+    f2 = f2 * 0.8 + Frama_C_double_interval(-1.0, 1.0);
+}
+
+void newton_sqrt(void) {
+  double f1 = 2.0;
+  for (int i = 1; i < 100; i++)
+    f1 = (f1 + 2.0 / f1) / 2.0;
+
+  double f2 = 2.0;
+  //@ loop widen_hints f2, 1.4;
+  for (int i = 1; i < 100; i++) {
+    f2 = (f2 + 2.0 / f2) / 2.0;
+  }
+}
+
+void main(voi) {
+  parabola();
+  trigo();
+  first_order_filter();
+  newton_sqrt();
 }
