ffe08ca5ed8b9e4668a8f7c1b1e004c88a433392
Valentin Perrelle <valentin.perrelle@cea.fr>
2019-05-21 16:14:39 +0200

    [Eva] Add tests for interprocedural trace partitioning

---
+++ tests/value/partitioning-interproc.c
@@ -0,0 +1,53 @@
+/* run.config*
+   GCC:
+   STDOPT: #"-main cassign_test -eva-partition-history 1"
+   STDOPT: #"-main fabs_test -eva-partition-history 1 -eva-equality-domain"
+   */
+
+#include "__fc_builtin.h"
+
+int cassign(int *p)
+{
+  if (Frama_C_nondet(0,1))
+  {
+    *p = 0;
+    return 1;
+  }
+
+  return 0;
+}
+
+void cassign_test(void) {
+  int x, y;
+
+  // First call
+  if (cassign(&x)) {
+    y = x + 1;
+    Frama_C_show_each(y);
+  }
+
+  // Second call with some MemExec hit
+  if (cassign(&x)) {
+    y = x + 1;
+    Frama_C_show_each(y);
+  }
+}
+
+
+double fabs(double x)
+{
+  if (x == 0.0) {
+    return 0.0;
+  } else if (x > 0.0) {
+    return x;
+  } else {
+    return -x;
+  } 
+}
+
+void fabs_test(double x)
+{
+  if (fabs(x) > 1.0) {
+    x = x < 0 ? -1.0 : 1.0;
+  }
+}
