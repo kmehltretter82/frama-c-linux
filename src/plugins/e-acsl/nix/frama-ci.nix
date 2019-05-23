88a1f861186b56b68f01bf5006c09fdf6361d627
François Bobot <francois.bobot@cea.fr>
2019-05-23 22:36:07 +0200

    Update nix/frama-ci.*

--- src/plugins/e-acsl/nix/frama-ci.nix
+++ src/plugins/e-acsl/nix/frama-ci.nix
@@ -5,8 +5,8 @@ let
     src = builtins.fetchGit {
             "url" = "https://bobot:${password}@git.frama-c.com/frama-c/Frama-CI.git";
             "name" = "Frama-CI";
-            "rev" = "70045f4252e668e0facad12d7db2c6ab83fc813b";
-            "ref" = "master";
+            "rev" = "f7d5e96afa5b540feb4476b732e78dc8424bd10f";
+            "ref" = "feature/opam2";
     };
  in
  {
