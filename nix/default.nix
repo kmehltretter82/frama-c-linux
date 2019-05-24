3b0301aed17ecd0e2caa5743a7e3dcdf83c71e76
François Bobot <francois.bobot@cea.fr>
2019-05-24 07:15:19 +0200

    Fix ocp-indent version in continuous intergration

--- nix/default.nix
+++ nix/default.nix
@@ -63,7 +63,8 @@ rec {
   lint = stdenv.mkDerivation {
         name = "frama-c-lint";
         inherit src;
-        buildInputs = (mk_buildInputs {opamPackages = [ "ocp-indent" ];} ) ++ [ pkgs.bc plugins.headache.installed ];
+        buildInputs = (mk_buildInputs { opamPackages = [ { name = "ocp-indent"; constraint = "=1.6.1"; } ];} )
+                      ++ [ pkgs.bc plugins.headache.installed ];
         outputs = [ "out" ];
         postPatch = ''
                patchShebangs .
