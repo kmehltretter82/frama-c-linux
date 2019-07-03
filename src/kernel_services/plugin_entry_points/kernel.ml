f7ccf2bcf15cfe4fa9c27b74dd9eb86535f57fd8
David Bühler <david.buhler@cea.fr>
2019-07-03 17:59:14 +0200

    [Kernel] Makes the -explain option independent of -then.

--- src/kernel_services/plugin_entry_points/kernel.ml
+++ src/kernel_services/plugin_entry_points/kernel.ml
@@ -430,7 +430,7 @@ let run_list_all_plugin_options () =
 let () = Cmdline.run_after_exiting_stage run_list_all_plugin_options
 
 let () = Parameter_customize.set_group help
-let () = Parameter_customize.set_cmdline_stage Cmdline.Exiting
+let () = Parameter_customize.set_cmdline_stage Cmdline.Extending
 let () = Parameter_customize.do_not_journalize ()
 let () = Parameter_customize.set_negative_option_name ""
 module Explain =
