155fd511f02e1b628990e87e45ceb9cce2fe8e17
David Bühler <david.buhler@cea.fr>
2019-07-15 17:18:26 +0200

    [Eva] New "subdivide" annotation to override -eva-subdivide-non-linear option.
    This annotation sets the maximum number of subdivisions in the evaluation
    of expressions at a given statement.

--- src/plugins/value/utils/partitioning_annots.mli
+++ src/plugins/value/utils/partitioning_annots.mli
@@ -34,6 +34,7 @@ type flow_annotation =
 val get_slevel_annot : Cil_types.stmt -> slevel_annotation option
 val get_unroll_annot : Cil_types.stmt -> unroll_annotation list
 val get_flow_annot : Cil_types.stmt -> flow_annotation list
+val get_subdivision_annot : Cil_types.stmt -> int list
 
 val add_slevel_annot : emitter:Emitter.t -> loc:Cil_types.location ->
   Cil_types.stmt -> slevel_annotation -> unit
@@ -41,3 +42,5 @@ val add_unroll_annot : emitter:Emitter.t -> loc:Cil_types.location ->
   Cil_types.stmt -> unroll_annotation -> unit
 val add_flow_annot : emitter:Emitter.t -> loc:Cil_types.location ->
   Cil_types.stmt -> flow_annotation -> unit
+val add_subdivision_annot : emitter:Emitter.t -> loc:Cil_types.location ->
+  Cil_types.stmt -> int -> unit
--- src/plugins/value/utils/value_util.ml
+++ src/plugins/value/utils/value_util.ml
@@ -71,11 +71,21 @@ let get_slevel kf =
   try Value_parameters.SlevelFunction.find kf
   with Not_found -> Value_parameters.SemanticUnrollingLevel.get ()
 
-let get_subdivision stmt =
+let get_subdivision_option stmt =
   try
     let kf = Kernel_function.find_englobing_kf stmt in
     Value_parameters.LinearLevelFunction.find kf
   with Not_found -> Value_parameters.LinearLevel.get ()
+
+let get_subdivision stmt =
+  match Partitioning_annots.get_subdivision_annot stmt with
+  | [] -> get_subdivision_option stmt
+  | [x] -> x
+  | x :: _ ->
+    Value_parameters.warning ~current:true ~once:true
+      "Several subdivision annotations at the same statement; selecting %i\
+       and ignoring the others." x;
+    x
 
 let pretty_actuals fmt actuals =
   let pp fmt (e,x,_) = Cvalue.V.pretty_typ (Some (Cil.typeOf e)) fmt x in
