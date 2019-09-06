853287e024d861f3ffcb543c165a75391bb83784
Allan Blanchard <allan.blanchard@cea.fr>
2019-09-06 10:45:24 +0200

    [Kernel] Adds two warnings categories for ghost code

--- src/kernel_services/plugin_entry_points/kernel.mli
+++ src/kernel_services/plugin_entry_points/kernel.mli
@@ -130,6 +130,12 @@ val dkey_visitor: category
 val wkey_annot_error: warn_category
 (** error in annotation. If only a warning, annotation will just be ignored. *)
 
+val wkey_ghost_bad_non_ghost: warn_category
+(** error in non ghost code related to the use of ghost elements *)
+
+val wkey_ghost_bad_use: warn_category
+(** error in ghost code *)
+
 val wkey_acsl_float_compare: warn_category
 
 val wkey_drop_unused: warn_category
