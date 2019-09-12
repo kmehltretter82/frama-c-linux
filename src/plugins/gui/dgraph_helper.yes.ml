204b9733dd6cd7c75f0f838c9eb9cfd6c2427b0e
Virgile Prevosto <virgile.prevosto@m4x.org>
2019-09-12 08:56:48 +0200

    headers for new files

--- src/plugins/gui/dgraph_helper.yes.ml
+++ src/plugins/gui/dgraph_helper.yes.ml
@@ -1,3 +1,25 @@
+(**************************************************************************)
+(*                                                                        *)
+(*  This file is part of Frama-C.                                         *)
+(*                                                                        *)
+(*  Copyright (C) 2007-2019                                               *)
+(*    CEA (Commissariat à l'énergie atomique et aux énergies              *)
+(*         alternatives)                                                  *)
+(*                                                                        *)
+(*  you can redistribute it and/or modify it under the terms of the GNU   *)
+(*  Lesser General Public License as published by the Free Software       *)
+(*  Foundation, version 2.1.                                              *)
+(*                                                                        *)
+(*  It is distributed in the hope that it will be useful,                 *)
+(*  but WITHOUT ANY WARRANTY; without even the implied warranty of        *)
+(*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *)
+(*  GNU Lesser General Public License for more details.                   *)
+(*                                                                        *)
+(*  See the GNU Lesser General Public License version 2.1                 *)
+(*  for more details (enclosed in the file licenses/LGPLv2.1).            *)
+(*                                                                        *)
+(**************************************************************************)
+
 let graph_window ~parent ~title make_view =
   let height = int_of_float (float parent#default_height *. 3. /. 4.) in
   let width = int_of_float (float parent#default_width *. 3. /. 4.) in
