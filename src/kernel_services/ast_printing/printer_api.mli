4a5b331b32253bb8ea13f5fbbca64e2ef877808f
Julien Signoles <julien.signoles@cea.fr>
2019-09-19 18:38:01 +0200

    [printer] improved comment

--- src/kernel_services/ast_printing/printer_api.mli
+++ src/kernel_services/ast_printing/printer_api.mli
@@ -85,14 +85,14 @@ class type extensible_printer_type = object
     ?block:bool ->
     (unit -> unit)
     -> unit
-  (** Open a ghost context if the the first [bool] is true and we are not already
-      in a ghost context. [post_fmt] is a format like ["%t"] and is used to define
-      the format of the end of the ghost context. [block] indicates whether we
-      should open a C block or not (and defaults to [true]). The last parameter is
-      the function to perform in the ghost context (generally some AST element).
+  (** Open a ghost context if the the first [bool] is true and we are not
+     already in a ghost context. [post_fmt] is a format like ["%t"] and is used
+     to define the format at the end of the ghost context. [block] indicates
+     whether we should open a C block or not (defaults to [true]). The last
+     parameter is the function to be applied in the ghost context (generally
+     some AST element).
 
-      @since 19.0-Potassium+dev
-  *)
+      @since 19.0-Potassium+dev *)
 
   method private current_stmt: stmt option
   (** @return the [stmt] being printed *)
