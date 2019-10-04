a1e9797875ed00a3561cf2648b8393cf6fae62a7
Virgile Prevosto <virgile.prevosto@m4x.org>
2019-10-04 14:37:33 +0200

    [typing] avoid name clashes when generating names for anonymous enum
    More specifically, mark explicitely anonymous symbols from Frama-C's stdlib
    as such if there's no other meaningful suffix to append to __anon{kind}
    
    Fixes #672

---
+++ tests/syntax/anon_enum_libc.c
@@ -0,0 +1,13 @@
+/* run.config
+OPT: -cpp-extra-args="-I @PTEST_DIR@" -ocode @PTEST_DIR@/result/@PTEST_NAME@.c -print -then -ocode="" @PTEST_DIR@/result/@PTEST_NAME@.c -print
+*/
+
+struct { int x; float y; } s1;
+
+enum { BLA=4, BLI=12 };
+
+#include "anon_enum_libc.h"
+
+int f() { return BLA + s1.x; }
+
+int g() { return FOO + s2.t; }
