ee28c2070b9b472ca823b67ab7ab6404a85fcd7f
Virgile Prevosto <virgile.prevosto@m4x.org>
2019-10-16 14:44:46 +0200

    [markdown] preliminary move

--- src/libraries/utils/markdown.ml
+++ src/libraries/utils/markdown.ml
@@ -1,136 +1,71 @@
-(**************************************************************************)
-(*                                                                        *)
-(*  This file is part of Frama-C.                                         *)
-(*                                                                        *)
-(*  Copyright (C) 2007-2019                                               *)
-(*    CEA (Commissariat à l'énergie atomique et aux énergies              *)
-(*         alternatives)                                                  *)
-(*                                                                        *)
-(*  you can redistribute it and/or modify it under the terms of the GNU   *)
-(*  Lesser General Public License as published by the Free Software       *)
-(*  Foundation, version 2.1.                                              *)
-(*                                                                        *)
-(*  It is distributed in the hope that it will be useful,                 *)
-(*  but WITHOUT ANY WARRANTY; without even the implied warranty of        *)
-(*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *)
-(*  GNU Lesser General Public License for more details.                   *)
-(*                                                                        *)
-(*  See the GNU Lesser General Public License version 2.1                 *)
-(*  for more details (enclosed in the file licenses/LGPLv2.1).            *)
-(*                                                                        *)
-(**************************************************************************)
+type align = Left | Center | Right
 
-(* -------------------------------------------------------------------------- *)
-(* --- Markdown Documentation Generation Utility                          --- *)
-(* -------------------------------------------------------------------------- *)
+type href =
+  | URL of string
+  | Page of string
+  | Name of string
+  | Section of string * string
 
-type md = Format.formatter -> unit
-type text = md
-type block = md
-type section = md
+type inline =
+  | Plain of string
+  | Emph of string
+  | Bold of string
+  | Inline_code of string
+  | Link of text * href (** [Link(text,url)] *)
+  | Image of string * string (** [Image(alt,location)] *)
 
-let pretty fmt w = w fmt
-let pp_text = pretty
-let pp_block = pretty
-let pp_section = pretty
+and text = inline list
 
-(* -------------------------------------------------------------------------- *)
-(* --- Context                                                            --- *)
-(* -------------------------------------------------------------------------- *)
+type block_element =
+  | Text of text (** single paragraph of text. *)
+  | Block_quote of element list
+  | UL of block list
+  | OL of block list
+  | DL of (text * text) list (** definition list *)
+  | EL of (string option * text) list (** example list *)
+  | Code_block of string * string list
 
-type toc = level:int -> name:string -> title:string -> unit
+and block = block_element list
 
-type context = {
-  page: string ;
-  path: string list ;
-  names: bool ;
-  level: int ;
-  toc: toc option ;
-}
+and element =
+  | Block of block
+  | Raw of string list
+   (** non-markdown. Each element of the list is printed as-is on its own line.
+       A blank line separates the [Raw] node from the next one. *)
+  | Comment of string (** markdown comment, printed <!-- like this --> *)
+  | H1 of text * string option (** optional label. *)
+  | H2 of text * string option
+  | H3 of text * string option
+  | H4 of text * string option
+  | H5 of text * string option
+  | H6 of text * string option
+  | Table of { caption: text option; header: (text * align) list;
+                content: text list list; }
 
-let context = ref {
-    page = "" ;
-    path = [] ;
-    names = false ;
-    level = 0 ;
-    toc = None ;
+type pandoc_markdown =
+  { title: text;
+    authors: text list;
+    date: text;
+    elements: element list
   }
 
-let local ctxt job data =
-  let current = !context in
-  try context := ctxt ; job data ; context := current
-  with err -> context := current ; raise err
+let plain s = [ Plain s]
 
-(* -------------------------------------------------------------------------- *)
-(* --- Combinators                                                        --- *)
-(* -------------------------------------------------------------------------- *)
+let plain_format txt = Format.kasprintf plain txt
 
-let nil _fmt = ()
-let empty= nil
-let space fmt = Format.pp_print_space fmt ()
-let newline fmt = Format.pp_print_newline fmt ()
+let plain_link h =
+  let s = match h with
+    | URL url -> url
+    | Page p -> p
+    | Section (_,s) -> s
+    | Name a -> a
+  in
+  Link ([Inline_code s], URL s)
 
-let merge sep ds fmt =
-  match List.filter (fun d -> d != nil) ds with
-  | [] -> ()
-  | d::ds -> d fmt ; List.iter (fun d -> sep fmt ; d fmt) ds
-
-let glue ?sep ds fmt =
-  match sep with
-  | None -> List.iter (fun d -> d fmt) ds
-  | Some s -> merge s ds fmt
-
-let (<@>) a b =
-  if a == empty then b else
-  if b == empty then a else
-    fun fmt -> a fmt ; b fmt
-
-let (<+>) a b =
-  if a == empty then b else
-  if b == empty then a else
-    fun fmt -> a fmt ; space fmt ; b fmt
-
-let (</>) a b =
-  if a == empty then b else
-  if b == empty then a else
-    fun fmt -> a fmt ; newline fmt ; b fmt
-
-let fmt_text k fmt = Format.fprintf fmt "@[<h 0>%t@]" k
-let fmt_block k fmt = Format.fprintf fmt "@[<v 0>%t@]@\n" k
-
-(* -------------------------------------------------------------------------- *)
-(* --- Elementary Text                                                    --- *)
-(* -------------------------------------------------------------------------- *)
-
-let raw s fmt = Format.pp_print_string fmt s
-let rm s fmt = Format.pp_print_string fmt s
-let it s fmt = Format.fprintf fmt "_%s_" s
-let bf s fmt = Format.fprintf fmt "**%s**" s
-let tt s fmt = Format.fprintf fmt "`%s`" s
-let text = merge space
-let praw s = fmt_block (raw s)
-
-(* -------------------------------------------------------------------------- *)
-(* --- Links                                                              --- *)
-(* -------------------------------------------------------------------------- *)
-
-type href = [
-  | `URL of string
-  | `Page of string
-  | `Name of string
-  | `Section of string * string
-]
-
-let filepath m = String.split_on_char '/' m
-
-let rec relative source target =
-  match source , target with
-  | p::ps , q::qs when p = q -> relative ps qs
-  | [] , _ -> target
-  | _::d , _ -> List.map (fun _ -> "..") d @ target
-
-let lnk target =
-  String.concat "/" (relative !context.path (filepath target))
+let codelines lang pp code =
+  let s = Format.asprintf "@[%a@]" pp code in
+  let lines = String.split_on_char '\n' s in
+  Code_block (lang, lines)
 
 let id m =
   let buffer = Buffer.create (String.length m) in
@@ -149,185 +84,176 @@ let id m =
       | _ -> ()) m ;
   Buffer.contents buffer
 
-let href ?title (h : href) fmt =
-  match title , h with
-  | None , `URL url -> Format.fprintf fmt "%s" url
-  | Some w , `URL url -> Format.fprintf fmt "[%s](%s)" w url
-  | None , `Page p -> Format.fprintf fmt "[%s](%s)" p (lnk p)
-  | Some w , `Page p -> Format.fprintf fmt "[%s](%s)" w (lnk p)
-  | None , `Section(p,s) -> Format.fprintf fmt "[%s](%s#%s)" s (lnk p) (id s)
-  | Some w , `Section(p,s) -> Format.fprintf fmt "[%s](%s#%s)" w (lnk p) (id s)
-  | None , `Name a -> Format.fprintf fmt "[%s](#%s)" a (id a)
-  | Some w , `Name a -> Format.fprintf fmt "[%s](#%s)" w (id a)
+let pp_href fmt = function
+  | URL s | Page s -> Format.pp_print_string fmt s
+  | Section (p,s) -> Format.fprintf fmt "%s#%s" p (id s)
+  | Name a -> Format.fprintf fmt "#%s" (id a)
 
-(* -------------------------------------------------------------------------- *)
-(* --- Blocks                                                             --- *)
-(* -------------------------------------------------------------------------- *)
+let rec pp_inline fmt =
+  function
+  | Plain s -> Format.pp_print_string fmt s
+  | Emph s -> Format.fprintf fmt "_%s_" (String.trim s)
+  | Bold s -> Format.fprintf fmt "**%s**" (String.trim s)
+  | Inline_code s -> Format.fprintf fmt "`%s`" (String.trim s)
+  | Link (text,url) ->
+    Format.fprintf fmt "@[<h>[%a](%a)@]@ " pp_text text pp_href url
+  | Image (alt,url) -> Format.fprintf fmt "@[<h>![%s](%s)@]@ " alt url
 
-let aname anchor fmt =
-  Format.fprintf fmt "<a name=\"%s\"></a>@\n" (id anchor)
+and pp_text fmt l =
+  match l with
+  | [] -> ()
+  | [ elt ] -> pp_inline fmt elt
+  | elt :: text -> Format.fprintf fmt "%a@ %a" pp_inline elt pp_text text
 
-let title h ?name title fmt =
-  begin
-    let { level ; names ; toc } = !context in
-    let level = max 0 (min 5 (level + h - 1)) in
-    Format.fprintf fmt "%s %s" (String.make level '#') title ;
-    if names || name <> None || toc <> None then
-      begin
-        let anchor = match name with None -> title | Some a -> a in
-        Format.fprintf fmt " {#%s}" (id anchor) ;
-        (match toc with
-         | None -> ()
-         | Some callback ->
-           callback ~level ~name:anchor ~title) ;
-      end ;
-    Format.pp_print_newline fmt () ;
-  end
+let pp_lab fmt = function
+  | None -> ()
+  | Some lab -> Format.fprintf fmt " {#%s}" lab
 
-let h1 = title 1
-let h2 = title 2
-let h3 = title 3
-let h4 = title 4
+let test_size txt = String.length (Format.asprintf "%a" pp_text txt)
 
-let indent h w fmt = local { !context with level = !context.level + h } w fmt
+let pp_dashes fmt size =
+  let dashes = String.make (size + 2) '-' in
+  Format.fprintf fmt "%s+" dashes
 
-let in_h1 = indent 1
-let in_h2 = indent 2
-let in_h3 = indent 3
-let in_h4 = indent 4
+let pp_sep_line fmt sizes =
+Format.fprintf fmt "@[<h>+";
+List.iter (pp_dashes fmt) sizes;
+Format.fprintf fmt "@]@\n"
 
-let hrule fmt = Format.pp_print_string fmt "-------------------------@."
+let pp_header fmt (t,_) size =
+  let real_size = test_size t in
+  let spaces = String.make (size - real_size) ' ' in
+  Format.fprintf fmt " %a%s |" pp_text t spaces
 
-let par w fmt = Format.fprintf fmt "@[<hov 0>%t@]@." w
+let pp_headers fmt l sizes =
+  Format.fprintf fmt "@[<h>|";
+  List.iter2 (pp_header fmt) l sizes;
+  Format.fprintf fmt "@]@\n"
 
-let list ws fmt =
-  List.iter
-    (fun w -> Format.fprintf fmt "@[<hov 2>- %t@]@." w) ws
+let compute_sizes headers contents =
+  let check_line i m line =
+    try max m (test_size (List.nth line i) + 2)
+    with Failure _ -> m
+  in
+  let column_size (i,l) (h,_) =
+    let max = List.fold_left (check_line i) (test_size h) contents in
+    (i+1, max :: l)
+  in
+  let (_,sizes) = List.fold_left column_size (0,[]) headers in
+  List.rev sizes
 
-let enum ws fmt =
-  let k = ref 0 in
-  List.iter
-    (fun w -> incr k ; Format.fprintf fmt "@[<hov 3>%d. %t@]@." !k w) ws
+let pp_align fmt align size =
+  let sep = String.make size '=' in
+  match align with
+  | (_,Left) -> Format.fprintf fmt ":%s=+" sep
+  | (_,Center) -> Format.fprintf fmt ":%s:+" sep
+  | (_,Right) -> Format.fprintf fmt "%s=:+" sep
 
-let description items fmt =
-  List.iter
-    (fun (a,w) -> Format.fprintf fmt "@[<hov 2>- **%s** %t@]@." a w) items
+let pp_aligns fmt headers sizes =
+  Format.fprintf fmt "@[<h>+";
+  List.iter2 (pp_align fmt) headers sizes;
+  Format.fprintf fmt "@]@\n"
 
-let code ?(lang="") pp fmt =
-  begin
-    Format.fprintf fmt "@[<v 0>```%s" lang ;
-    let buffer = Buffer.create 80 in
-    let bfmt = Format.formatter_of_buffer buffer in
-    pp bfmt ; Format.pp_print_flush bfmt () ;
-    let content = Buffer.contents buffer in
-    let lines = String.split_on_char '\n' content in
-    let rec clean = function [] -> [] | ""::w -> clean w | w -> w in
+let pp_table_cell fmt size t =
+  let real_size = test_size t in
+  let spaces = String.make (size - real_size) ' ' in
+  Format.fprintf fmt " %a%s |" pp_text t spaces
+
+let pp_table_line fmt sizes l =
+  Format.fprintf fmt "@[<h>|";
+  List.iter2 (pp_table_cell fmt) sizes l;
+  Format.fprintf fmt "@]@\n";
+  pp_sep_line fmt sizes
+
+let pp_table_content fmt l sizes =
+  Format.fprintf fmt "@[<v>";
+  List.iter (pp_table_line fmt sizes) l;
+  Format.fprintf fmt "@]"
+
+let rec pp_block_element fmt = function
+  | Text t -> Format.fprintf fmt "@[<hov>%a@]@\n" pp_text t
+  | Block_quote l -> pp_quote fmt l
+  | UL l -> pp_list "*" fmt l
+  | OL l -> pp_list "#." fmt l
+  | DL l ->
     List.iter
-      (fun l -> Format.fprintf fmt "@\n%s" l)
-      (List.rev (clean (List.rev (clean lines)))) ;
-    Format.fprintf fmt "@\n```@]@."
-  end
-
-type column = [
-  | `Left of string
-  | `Right of string
-  | `Center of string
-]
-
-let table columns rows fmt =
-  begin
-    Format.fprintf fmt "@[<v 0>" ;
+      (fun (term,def) ->
+         Format.fprintf fmt "@[<h>%a@]@\n@\n@[<hov 2>: %a@]@\n@\n"
+           pp_text term pp_text def)
+      l
+  | EL l ->
     List.iter
-      (function `Left h | `Right h | `Center h -> Format.fprintf fmt "| %s " h)
-      columns ;
-    Format.fprintf fmt "|@\n" ;
-    List.iter (fun column ->
-        let dash h k = String.make (max 3 (String.length h + k)) '-' in
-        match column with
-        | `Left h -> Format.fprintf fmt "|:%s" (dash h 1)
-        | `Right h -> Format.fprintf fmt "|%s:" (dash h 1)
-        | `Center h -> Format.fprintf fmt "|:%s:" (dash h 0)
-      ) columns ;
-    Format.fprintf fmt "|@\n" ;
-    List.iter (fun row ->
-        List.iter (fun col -> Format.fprintf fmt "| @[<h 0>%t@] " col) row ;
-        Format.fprintf fmt "|@\n" ;
-      ) rows ;
-    Format.fprintf fmt "@]@." ;
-  end
+      (fun (lab,txt) ->
+         match lab with
+         | None -> Format.fprintf fmt "@[<hov 4>(@@) %a@]@\n" pp_text txt
+         | Some s -> Format.fprintf fmt "@[<hov 4>(@@%s) %a@]@\n" s pp_text txt)
+      l
+  | Code_block (language, lines) ->
+    Format.fprintf fmt "@[<h>```%s@]@\n" language;
+    List.iter (fun line -> Format.fprintf fmt "@[<h>%s@]@\n" line) lines;
+    Format.fprintf fmt "```@\n"
 
-let concat ps = merge newline (List.filter (fun p -> p != empty) ps)
+and pp_list prefix fmt l =
+  List.iter
+    (fun item ->
+       Format.fprintf fmt "@[<v 4>@[<hov>%s %a@]@]" prefix pp_block item)
+    l
 
-(* -------------------------------------------------------------------------- *)
-(* --- Refs                                                               --- *)
-(* -------------------------------------------------------------------------- *)
+and pp_block fmt l =
+  match l with
+  | [ elt ] -> pp_block_element fmt elt
+  | _ ->
+    Format.fprintf fmt "%a@\n"
+      (Format.pp_print_list ~pp_sep:Format.pp_force_newline pp_block_element) l
 
-let mk f fmt = (f ()) fmt
-let mk_text = mk
-let mk_block = mk
+and pp_quote fmt l =
+  List.iter
+    (fun elt -> Format.fprintf fmt "@[<v>> %a@]" pp_element elt) l
 
-(* -------------------------------------------------------------------------- *)
-(* --- Sections                                                           --- *)
-(* -------------------------------------------------------------------------- *)
+and pp_element fmt = function
+  | Block b -> Format.fprintf fmt "@[<v>%a@]" pp_block b
+  | Raw l ->
+    Format.(
+      fprintf fmt "%a"
+        (pp_print_list ~pp_sep:pp_force_newline pp_print_string) l)
+  | Comment s ->
+    Format.fprintf fmt
+      "@[<hv>@[<hov 5><!-- %a@]@ -->@]" Format.pp_print_text s
+  | H1(t,lab) -> Format.fprintf fmt "@[<h># %a%a@]" pp_text t pp_lab lab
+  | H2(t,lab) -> Format.fprintf fmt "@[<h>## %a%a@]" pp_text t pp_lab lab
+  | H3(t,lab) -> Format.fprintf fmt "@[<h>### %a%a@]" pp_text t pp_lab lab
+  | H4(t,lab) -> Format.fprintf fmt "@[<h>#### %a%a@]" pp_text t pp_lab lab
+  | H5(t,lab) -> Format.fprintf fmt "@[<h>##### %a%a@]" pp_text t pp_lab lab
+  | H6(t,lab) -> Format.fprintf fmt "@[<h>###### %a%a@]" pp_text t pp_lab lab
+  | Table { caption; header; content } ->
+    (match caption with
+     | None -> ()
+     | Some t -> Format.fprintf fmt "@[<h>Table: %a@]@\n@\n" pp_text t);
+    let sizes = compute_sizes header content in
+    pp_sep_line fmt sizes;
+    pp_headers fmt header sizes;
+    pp_aligns fmt header sizes;
+    pp_table_content fmt content sizes
 
-let document s = s
+let pp_elements fmt l =
+  let pp_sep fmt () =
+    Format.pp_print_newline fmt ();
+    Format.pp_print_newline fmt ()
+  in
+  Format.pp_print_list ~pp_sep pp_element fmt l
 
-let subsections section subsections = section </> in_h1 (merge newline subsections)
+let pp_authors fmt l =
+  List.iter (fun t -> Format.fprintf fmt "@[<h>- %a@]@\n" pp_text t) l
 
-let section ?name ~title content subsections =
-  h1 ?name title </> content </> in_h1 (merge newline subsections)
-
-(* -------------------------------------------------------------------------- *)
-(* --- Include File                                                       --- *)
-(* -------------------------------------------------------------------------- *)
-
-let from_file path fmt =
-  let inc = open_in path in
-  try
-    while true do
-      let line = input_line inc in
-      Format.pp_print_string fmt line ;
-      Format.pp_print_newline fmt () ;
-    done
-  with
-  | End_of_file -> close_in inc
-  | exn -> close_in inc ; raise exn
-
-let read_block = from_file
-let read_section = from_file
-let read_text path fmt = Format.fprintf fmt "@[<h 0>%t@]" (from_file path)
-
-(* -------------------------------------------------------------------------- *)
-(* --- Dump to File                                                       --- *)
-(* -------------------------------------------------------------------------- *)
-
-let rec mkdir root page =
-  let dir = Filename.dirname page in
-  if dir <> "." && dir <> ".." then
-    let path = Printf.sprintf "%s/%s" root dir in
-    if not (Sys.file_exists path) then
-      begin
-        mkdir root dir ;
-        try Unix.mkdir path 0o755
-        with Unix.Unix_error _ ->
-          failwith (Printf.sprintf "Can not create direcoty '%s'" dir)
-      end
-
-let dump ~root ~page ?(names=false) ?toc doc =
-  local
-    { page ; path = filepath page ; level = 1 ; toc ; names = names }
-    begin fun () ->
-      mkdir root page ;
-      let out = open_out (Printf.sprintf "%s/%s" root page) in
-      let fmt = Format.formatter_of_out_channel out in
-      try
-        doc fmt ;
-        Format.pp_print_newline fmt () ;
-        close_out out ;
-      with err ->
-        Format.pp_print_newline fmt () ;
-        close_out out ;
-        raise err
-    end ()
-
-(* -------------------------------------------------------------------------- *)
+let pp_pandoc fmt { title; authors; date; elements } =
+  Format.fprintf fmt "@[<v>";
+  if title <> [] || authors <> [] || date <> [] then begin
+    Format.fprintf fmt "@[<h>---@]@\n";
+    Format.fprintf fmt "@[<h>title: %a@]@\n" pp_text title;
+    Format.fprintf fmt "@[<h>author:@]@\n%a" pp_authors authors;
+    Format.fprintf fmt "@[<h>date: %a@]@\n" pp_text date;
+    Format.fprintf fmt "@[<h>...@]@\n";
+    Format.pp_print_newline fmt ();
+  end;
+  pp_elements fmt elements;
+  Format.fprintf fmt "@]%!"
