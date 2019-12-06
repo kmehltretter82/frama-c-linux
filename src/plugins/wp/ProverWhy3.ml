c566ff303d7a373dd00d001e9ebc73a8e0d025f7
Loïc Correnson <loic.correnson@cea.fr>
2019-12-06 14:56:11 +0100

    [wp/why3] more accurate timeout

--- src/plugins/wp/ProverWhy3.ml
+++ src/plugins/wp/ProverWhy3.ml
@@ -1045,7 +1045,8 @@ type prover_call = {
   prover : Why3Provers.t ;
   call : Why3.Call_provers.prover_call ;
   steps : int option ;
-  timeover : float option ;
+  timeout : int ;
+  mutable timeover : float option ;
   mutable interrupted : bool ;
   mutable killed : bool ;
 }
@@ -1055,7 +1056,9 @@ let ping_prover_call p =
   | NoUpdates
   | ProverStarted ->
       let () = match p.timeover with
-        | None -> ()
+        | None ->
+            let started = Unix.time () in
+            p.timeover <- Some (started +. 2.0 +. float p.timeout)
         | Some timeout ->
             let time = Unix.time () in
             if time > timeout then
@@ -1080,7 +1083,7 @@ let ping_prover_call p =
         | OutOfMemory -> VCS.failed "out of memory"
         | StepLimitExceeded -> VCS.result ?steps:p.steps VCS.Stepout
         | Unknown _ -> VCS.unknown
-        | _ when p.interrupted -> VCS.timeout (int_of_float pr.pr_time)
+        | _ when p.interrupted -> VCS.timeout p.timeout
         | Failure s -> VCS.failed s
         | HighFailure ->
             let alt_ergo_hack =
@@ -1113,15 +1116,12 @@ let call_prover ~timeout ~steplimit drv prover prover_config task =
     Why3.Whyconf.print_prover prover
     (Why3.Opt.get_def (-1) timeout)
     (Why3.Opt.get_def (-1) steps);
-  let timeover = match timeout with
-    | None -> None | Some tlimit ->
-        let started = Unix.time () in
-        Some (started +. 2.0 +. float tlimit) in
+  let timeout = match timeout with None -> 0 | Some tlimit -> tlimit in
   let pcall = {
     call ; prover ;
     killed = false ;
     interrupted = false ;
-    steps ; timeover ;
+    steps ; timeout ; timeover = None ;
   } in
   let ping = function
     | Task.Kill ->
