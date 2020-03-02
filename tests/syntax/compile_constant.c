429f27175385bfd399d1eff6eeda3e1f1d9745b2
Virgile Prevosto <virgile.prevosto@m4x.org>
2020-03-02 20:02:52 +0100

    [typing] fix long-standing bug in evaluation of conditional in constant expr
    Actually we can't completely avoid typing the branch that won't be evaluated:
    we need its type in order to convert the value of the other branch if needed.

--- tests/syntax/compile_constant.c
+++ tests/syntax/compile_constant.c
@@ -4,3 +4,9 @@ char pixels[] = {M0(0.0), M0(1), M0(2.0f)};
 char test_neg = { (-0.) ? 1. : 2. };
 
 char test_ge = { ((-1.) >= 0.) ? 1. : 2. };
+
+char test_cast = { 1 >= (0?1U:(-1)) ? 1. : 2. };
+
+extern int f(void);
+
+char no_call[] = { 1 ? 1 : f(), 0?f():2 };
