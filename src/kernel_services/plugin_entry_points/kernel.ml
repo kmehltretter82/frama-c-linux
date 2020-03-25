7df1a6d436e5a6f6089cbcea112609d2f5c8b88e
David Bühler <david.buhler@cea.fr>
2020-03-25 15:03:18 +0100

    [Kernel] Adds the new option -warn-pointer-downcast.

--- src/kernel_services/plugin_entry_points/kernel.ml
+++ src/kernel_services/plugin_entry_points/kernel.ml
@@ -1440,6 +1440,17 @@ module UnsignedDowncast =
                   destination range"
     end)
 
+(* Pointer downcasts are undefined behaviors. *)
+let () = Parameter_customize.set_group analysis_options
+let () = Parameter_customize.do_not_reset_on_copy ()
+module PointerDowncast =
+  True
+    (struct
+      let module_name = "PointerDowncast"
+      let option_name = "-warn-pointer-downcast"
+      let help = "generate alarms when a pointer is converted into an integer \
+                  but may not be in the range of the destination type."
+    end)
 
 (* Not finite floats are ok, but might not always be a behavior the programmer
    wants. *)
