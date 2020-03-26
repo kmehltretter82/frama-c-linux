568460fbdd2de21be42b744909e48dd3d17103b9
Virgile Prevosto <virgile.prevosto@m4x.org>
2020-03-26 13:47:31 +0100

    [libc] Avoid confusing Eva on the value of SIG_IGN, SIG_DFL and SIG_ERR

--- share/libc/signal.h
+++ share/libc/signal.h
@@ -46,13 +46,13 @@ typedef void (*__fc_sighandler_t) (int);
 /* for BSD 4.4 */
 typedef __fc_sighandler_t sig_t;
 
-extern sig_t __fc_sig_dfl;
-extern sig_t __fc_sig_ign;
-extern sig_t __fc_sig_err;
+extern void __fc_sig_dfl(int);
+extern void __fc_sig_ign(int);
+extern void __fc_sig_err(int);
 
-#define SIG_DFL __fc_sig_dfl     /* default signal handling */
-#define SIG_IGN __fc_sig_ign     /* ignore signal */
-#define SIG_ERR __fc_sig_err    /* error return from signal */
+#define SIG_DFL (&__fc_sig_dfl)     /* default signal handling */
+#define SIG_IGN (&__fc_sig_ign)     /* ignore signal */
+#define SIG_ERR (&__fc_sig_err)     /* error return from signal */
 
 #define SIG_BLOCK 0
 #define SIG_UNBLOCK 1
