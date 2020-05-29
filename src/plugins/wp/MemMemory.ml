b0fb8cd73f21c545ad5070768eb15ae238e16c95
Allan Blanchard <allan.blanchard@cea.fr>
2020-05-29 09:42:01 +0200

    [wp] Slightly reorganize memory.mlw/MemMemory

--- src/plugins/wp/MemMemory.ml
+++ src/plugins/wp/MemMemory.ml
@@ -74,7 +74,6 @@ let p_separated = Lang.extern_fp ~library "separated"
 let p_included = Lang.extern_fp ~library "included"
 let p_eqmem = Lang.extern_fp ~library "eqmem"
 let f_havoc = Lang.extern_f ~library ~typecheck:ty_fst_arg ~link:l_havoc "havoc"
-let p_monotonic = Lang.extern_fp ~library "monotonic_init"
 let f_region = Lang.extern_f ~library ~result:L.Int "region" (* base -> region *)
 let p_framed = Lang.extern_fp ~library "framed" (* m-pointer -> prop *)
 let p_linked = Lang.extern_fp ~library "linked" (* allocation-table -> prop *)
@@ -85,6 +84,7 @@ let f_set_init =
   Lang.extern_f ~library ~typecheck:ty_fst_arg ~link:l_set_init "set_init"
 let p_cinits = Lang.extern_fp ~library "cinits" (* initializaton-table -> prop *)
 let p_is_init_r = Lang.extern_fp ~library "is_init_range"
+let p_monotonic = Lang.extern_fp ~library "monotonic_init"
 
 let f_addr_of_int = Lang.extern_f
     ~category:L.Injection
