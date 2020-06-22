a0b7ef444d97e2be19c2d3de837c6387c1c09db3
David Bühler <david.buhler@cea.fr>
2020-06-22 16:51:25 +0200

    [Dive] Server interface: new request 'add_node' adds a localizable to the graph.
    This unified request replaces both requests 'add_var' and 'add_function_alarms'.

--- src/plugins/dive/server_interface.ml
+++ src/plugins/dive/server_interface.ml
@@ -52,86 +52,6 @@ struct
   let to_json = fun (g,d) -> Imprecision_graph.diff_to_json g d
 end
 
-module Variable = Data.Collection (struct
-    let name = "dive-variable-name"
-    let descr = Markdown.plain "The name of variable of the program"
-
-    let signature = Data.Record.signature ~page ~name ~descr ()
-
-    let _fun_field = Data.Record.option signature
-        ~descr:(Markdown.plain "owner function for a local variable")
-        (module Data.Jstring)
-
-    let _var_field = Data.Record.field signature
-        ~descr:(Markdown.plain "variable name")
-        (module Data.Jstring)
-
-    type t = Cil_types.varinfo
-    module R =
-      (val (Data.Record.publish signature): Data.Record.S with type r = t)
-
-    let syntax = R.syntax
-
-    let to_json v =
-      let varname = v.Cil_types.vname in
-      let fields =  [ "var", `String varname ] in
-      let fields = match Kernel_function.find_defining_kf v with
-        | Some kf -> ("fun", `String (Kernel_function.get_name kf)) :: fields
-        | None -> fields
-      in
-      `Assoc fields
-
-    let of_json json =
-      let open Yojson.Basic.Util in
-      let funname =
-        try Some (json |> member "fun" |> to_string)
-        with Not_found -> None
-      and varname = json |> member "var" |> to_string in
-      match funname with
-      | Some name ->
-        let kf =
-          try
-            Globals.Functions.find_by_name name
-          with Not_found ->
-            Data.failure "no function '%s'" name
-        in
-        let vi =
-          try Globals.Vars.find_from_astinfo varname (Cil_types.VLocal kf)
-          with Not_found ->
-          try Globals.Vars.find_from_astinfo varname (Cil_types.VFormal kf)
-          with Not_found ->
-            Data.failure "no variable '%s' in function '%s'"
-              varname name
-        in
-        vi
-      | None ->
-        match
-          Globals.Syntactic_search.find_in_scope varname Cil_types.Program
-        with
-        | Some vi -> vi
-        | None ->
-          Data.failure "no global variable '%s'" varname
-  end)
-
-module Function = Data.Collection (struct
-    type t = Cil_types.kernel_function
-
-    let syntax = Syntax.publish ~page ~name:"dive-function-name"
-        ~synopsis:Syntax.string
-        ~descr:(Markdown.plain "The name of a function of the program") ()
-
-    let to_json kf =
-      `String (Kernel_function.get_name kf)
-
-    let of_json json =
-      let open Yojson.Basic.Util in
-      let name = to_string json in
-      try
-        Globals.Functions.find_by_name name
-      with Not_found ->
-        Data.failure "no function '%s'" name
-  end)
-
 module Node = Data.Collection (struct
     type t = Graph_types.node
 
@@ -165,24 +85,13 @@ let () = Request.register ~page
     (fun () -> Build.clear (get_graph ()))
 
 let () = Request.register ~page
-    ~kind:`EXEC ~name:"dive.add_var"
-    ~descr:(Markdown.plain "Add a variable to the graph")
-    ~input:(module Variable) ~output:(module GraphDiff)
-    begin fun var ->
+    ~kind:`EXEC ~name:"dive.add_node"
+    ~descr:(Markdown.plain "Add a node to the graph")
+    ~input:(module Kernel_ast.Marker) ~output:(module GraphDiff)
+    begin fun loc ->
       let depth = Self.DepthLimit.get () in
       let g = get_graph () in
-      Build.add_var ~depth g var;
-      Build.get_graph g, Build.take_last_differences g
-    end
-
-let () = Request.register ~page
-    ~kind:`EXEC ~name:"dive.add_function_alarms"
-    ~descr:(Markdown.plain "Add all alarms of the given function")
-    ~input:(module Function) ~output:(module GraphDiff)
-    begin fun kf ->
-      let depth = Self.DepthLimit.get () in
-      let g = get_graph () in
-      Build.add_function_alarms ~depth g kf;
+      Build.add_localizable ~depth g loc;
       Build.get_graph g, Build.take_last_differences g
     end
 
