66734aa875b36f374da70be446ce6d309d5fe68c
Loïc Correnson <loic.correnson@cea.fr>
2020-07-09 19:47:45 +0200

    [dome] linted layout

--- ivette/src/dome/src/misc/utils.ts
+++ ivette/src/dome/src/misc/utils.ts
@@ -2,28 +2,43 @@
 // --- Utilities
 // --------------------------------------------------------------------------
 
-type specClass =
+import type { CSSProperties } from 'react';
+
+export type ClassSpec =
   undefined | boolean | null | string |
   { [cname: string]: boolean | null | undefined };
 
 export function classes(
-  ...args: specClass[]
+  ...args: ClassSpec[]
 ): string {
   const buffer: string[] = [];
-  args.forEach((spec) => {
-    if (spec !== undefined && spec !== null) {
-      if (typeof (spec) === 'string' && spec !== '') buffer.push(spec);
-      else if (typeof (spec) === 'object') {
-        const cs = Object.keys(spec);
-        cs.forEach((c) => { if (spec[c]) buffer.push(c); });
+  args.forEach((cla) => {
+    if (cla) {
+      if (typeof (cla) === 'string' && cla !== '') buffer.push(cla);
+      else if (typeof (cla) === 'object') {
+        const cs = Object.keys(cla);
+        cs.forEach((c) => { if (cla[c]) buffer.push(c); });
       }
     }
   });
   return buffer.join(' ');
 }
 
-// --- please the linter
+export type StyleSpec =
+  undefined | boolean | null | CSSProperties;
 
-export default {};
+export function styles(
+  ...args: StyleSpec[]
+): CSSProperties | undefined {
+  let empty = true;
+  let buffer = {};
+  args.forEach((sty) => {
+    if (sty && typeof (sty) === 'object') {
+      empty = false;
+      buffer = { ...buffer, ...sty };
+    }
+  });
+  return (empty ? undefined : buffer);
+}
 
 // --------------------------------------------------------------------------
