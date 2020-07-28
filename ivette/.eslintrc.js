c1f8f21a27206c315c5d8f9568e473dee6589579
Michele Alberti <michele.alberti@cea.fr>
2020-07-28 11:33:11 +0200

    [linter] Add rule no-var to disallow the use of var in favor of let and const.

--- ivette/.eslintrc.js
+++ ivette/.eslintrc.js
@@ -77,5 +77,7 @@ module.exports = {
     "react/destructuring-assignment": "off",
     // Allow console errors and warnings
     "no-console": ["error", { allow: ["warn", "error"] }],
+    // Disallow the use of var in favor of let and const
+    "no-var": "error",
   }
 };
