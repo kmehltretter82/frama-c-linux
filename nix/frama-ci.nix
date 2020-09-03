45ef4ae8524746f20d447ef1a7921b5c91a1c7b3
Virgile Prevosto <virgile.prevosto@m4x.org>
2020-09-03 09:56:37 +0200

    update Frama-CI revision so as to have MetAcsl

--- nix/frama-ci.nix
+++ nix/frama-ci.nix
@@ -5,8 +5,8 @@ let
     src = builtins.fetchGit {
             "url" = "https://bobot:${password}@git.frama-c.com/frama-c/Frama-CI.git";
             "name" = "Frama-CI";
-            "rev" = "4add9658dfda894a5db9de59aa7a5657c8cf7b2b";
-            "ref" = "feature/ci/update-opam";
+            "rev" = "36e99854d59cda2c97d0b52f1f098f6beb3baf04";
+            "ref" = "master";
     };
  in
  {
