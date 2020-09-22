db55a2e6cd832005f31e4147e739ff79170624bd
Basile Desloges <basile.desloges@cea.fr>
2020-09-22 10:32:31 +0200

    [eacsl:codegen] Update translation to use the `Contract`

--- src/plugins/e-acsl/src/code_generator/injector.ml
+++ src/plugins/e-acsl/src/code_generator/injector.ml
@@ -232,7 +232,7 @@ let add_new_block_in_stmt env kf stmt =
   in
   let mk_post_env env stmt =
     Annotations.fold_code_annot
-      (fun _ a env -> Translate.translate_post_code_annotation kf stmt env a)
+      (fun _ a env -> Translate_annots.post_code_annotation kf stmt env a)
       stmt
       env
   in
@@ -252,7 +252,7 @@ let add_new_block_in_stmt env kf stmt =
           let env = mk_post_env env stmt in
           (* also handle the postcondition of the function and clear the
              env *)
-          Translate.translate_post_spec kf Kglobal env (Annotations.funspec kf)
+          Translate_annots.post_funspec kf Kglobal env
         else
           env
       in
@@ -450,7 +450,7 @@ and inject_in_stmt env kf stmt =
       (* translate the precondition of the function *)
       if Functions.check kf then
         let funspec = Annotations.funspec kf in
-        Translate.translate_pre_spec kf Kglobal env funspec
+        Translate_annots.pre_funspec kf Kglobal env funspec
       else env
     else
       env
@@ -459,7 +459,7 @@ and inject_in_stmt env kf stmt =
   let env =
     if Functions.check kf then
       Annotations.fold_code_annot
-        (fun _ a env -> Translate.translate_pre_code_annotation kf stmt env a)
+        (fun _ a env -> Translate_annots.pre_code_annotation kf stmt env a)
         stmt
         env
     else
