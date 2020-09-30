875a08ce89c5efb77764d6343b8ead1c82cc61d4
François Bobot <francois.bobot@cea.fr>
2020-09-30 18:55:13 +0200

    [tests] add jcdb

--- tests/jcdb/jcdb.c
+++ tests/jcdb/jcdb.c
@@ -1,10 +1,10 @@
 /* run.config
-DEPS: jcdb2.c with_arguments.json compile_commands.json
+DEPS: jcdb2.c with_arguments.json compile_commands.json file_without_main.c
 CMXS: @PTEST_NAME@
 OPT: -json-compilation-database ./ -print
 OPT: jcdb2.c -json-compilation-database with_arguments.json -print
 OPT: -json-compilation-database with_arguments.json -load-module %{dep:@PTEST_NAME@.cmxs}
-EXECNOW: LOG list_files.res LOG list_files.err %{read:../syntax/framac_share_path}/analysis-scripts/list_files.py %{dep:compile_commands_working.json} > list_files.res 2> list_files.err
+EXECNOW: LOG list_files.res LOG list_files.err %{read:../../syntax/framac_share_path}/analysis-scripts/list_files.py %{dep:compile_commands_working.json} > list_files.res 2> list_files.err
 */
 #include <stdio.h>
 
--- tests/jcdb/oracle/list_files.res
+++ tests/jcdb/oracle/list_files.res
@@ -1,24 +1,8 @@
 # Paths as seen by a makefile inside subdirectory '.frama-c':
 SRCS=\
-<<<<<<< HEAD
-file_without_main.c \
-jcdb.c \
-||||||| 362083a770
-tests/jcdb/file_without_main.c \
-tests/jcdb/jcdb.c \
-=======
-../tests/jcdb/file_without_main.c \
-../tests/jcdb/jcdb.c \
->>>>>>> origin/master
+../file_without_main.c \
+../jcdb.c \
 
 
-<<<<<<< HEAD
-# Possible definition of main function in the following file(s):
-jcdb.c
-||||||| 362083a770
-# Possible definition of main function in the following file(s):
-tests/jcdb/jcdb.c
-=======
 # Possible definition of main function in the following file(s), as seen from '.frama-c':
-../tests/jcdb/jcdb.c
->>>>>>> origin/master
+../jcdb.c
