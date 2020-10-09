f3f07661aa6f38081e66efbc93c980a01440013b
Basile Desloges <basile.desloges@cea.fr>
2020-10-09 16:00:16 +0200

    [eacsl] Remove `-variadic-no-translation` when calling E-ACSL

--- src/plugins/e-acsl/scripts/e-acsl-gcc.sh
+++ src/plugins/e-acsl/scripts/e-acsl-gcc.sh
@@ -763,8 +763,7 @@ LDFLAGS="$OPTION_LDFLAGS"
 
 # Extra Frama-C Flags E-ACSL needs
 FRAMAC_FLAGS="$FRAMAC_FLAGS \
-  -remove-unused-specified-functions \
-  -variadic-no-translation"
+  -remove-unused-specified-functions"
 
 # C, CPP and LD flags for compilation of E-ACSL-generated sources
 EACSL_CFLAGS="$OPTION_EXTERNAL_ASSERT"
