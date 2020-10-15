86ef7bc83de3d929047b2e8d58b8a7ad5cfb8d1d
Virgile Prevosto <virgile.prevosto@m4x.org>
2020-10-15 16:37:06 +0200

    [offsetmap] make fold_offset tailrec

--- src/kernel_services/abstract_interp/offsetmap.ml
+++ src/kernel_services/abstract_interp/offsetmap.ml
@@ -510,14 +510,9 @@ module Make (V : module type of Offsetmap_lattice_with_isotropy) = struct
        | Node (max, _, _, _, _, r, m, v, _) ->
          let abs_max = max +~ o in
          let now = f (o, abs_max) (v, m, r) pre in
-         let no, nt, nz =
-           match move_right o t z with
-           | t -> t
-           | exception End_reached ->
-             abs_max, Empty, z (* End the recursion at next iteration *)
-         in
-         aux_fold no nt nz now
-
+         match move_right o t z with
+         | no, nt, nz -> aux_fold no nt nz now
+         | exception End_reached -> now
      in
      aux_fold o n z acc
  ;;
