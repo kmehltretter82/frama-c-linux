3cdd905bbe357f64682f787f7a618aa36cb8c381
Allan Blanchard <allan.blanchard@cea.fr>
2020-10-19 17:03:33 +0200

    [wp] Keep contract on result compilation failure

--- src/plugins/wp/cfgWP.ml
+++ src/plugins/wp/cfgWP.ml
@@ -1131,13 +1131,17 @@ struct
   let cc_result call = match call.loc_result with
     | None -> []
     | Some(tr,obj,loc) ->
-        (* [LC,VP] : the C left unspecified where to compute the lv *)
-        (* [LC,BY] : lv computed before, like in Value Analysis *)
-        let vr = M.load call.seq_result.post obj loc in
-        let re = L.in_frame call.frame_post L.result () in
-        let te = L.in_frame call.frame_post L.return () in
-        let value = C.result call.sigma_pre tr re in
-        [ C.equal_typ tr vr (C.cast tr te (Val value)) ]
+        let handler () = [ p_true ] in
+        let compile () =
+          (* [LC,VP] : the C left unspecified where to compute the lv *)
+          (* [LC,BY] : lv computed before, like in Value Analysis *)
+          let vr = M.load call.seq_result.post obj loc in
+          let re = L.in_frame call.frame_post L.result () in
+          let te = L.in_frame call.frame_post L.return () in
+          let value = C.result call.sigma_pre tr re in
+          [ C.equal_typ tr vr (C.cast tr te (Val value)) ]
+        in
+        Warning.handle ~handler ~severe:false ~effect:"Hide \\result" compile ()
 
   let cc_status f_caller f_callee =
     p_equal
