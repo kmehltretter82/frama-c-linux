a14bd15eadb7c7a998a5d174e5bc705ae5be7dec
Valentin Perrelle <valentin.perrelle@cea.fr>
2020-11-18 20:30:36 +0100

    [Aorai] Setup Eva to automatically use value partitioning on aorai state and history

--- src/plugins/aorai/aorai_eva_analysis.enabled.ml
+++ src/plugins/aorai/aorai_eva_analysis.enabled.ml
@@ -36,5 +36,15 @@ let add_slevel_annotation vi kind =
 let add_slevel_annotations () =
   Aorai_visitors.Aux_funcs.iter add_slevel_annotation
 
+let add_partitioning varname =
+  match Data_for_aorai.get_varinfo_option varname with
+  | None -> ()
+  | Some vi -> Eva.Value_parameters.use_global_value_partitioning vi
+
+let add_state_variables_partitioning () =
+  add_partitioning Data_for_aorai.curState;
+  List.iter add_partitioning (Data_for_aorai.whole_history ())
+
 let setup () =
-  add_slevel_annotations ()
+  add_slevel_annotations ();
+  add_state_variables_partitioning ()
