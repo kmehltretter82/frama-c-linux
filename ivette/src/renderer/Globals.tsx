046bc2d5d4a2a7240becf862b9f5e528230aef7a
Loïc Correnson <loic.correnson@cea.fr>
2020-11-26 17:48:04 +0100

    [ivette] global search bar activated

--- ivette/src/renderer/Globals.tsx
+++ ivette/src/renderer/Globals.tsx
@@ -4,12 +4,41 @@
 
 import React from 'react';
 import { Section, Item } from 'dome/frame/sidebars';
+import type { Hint } from 'dome/frame/toolbars';
 import * as States from 'frama-c/states';
 import { alpha } from 'dome/data/compare';
 import { functions, functionsData } from 'frama-c/api/kernel/ast';
 
 // --------------------------------------------------------------------------
-// --- Globals Section
+// --- Global Search Hints
+// --------------------------------------------------------------------------
+
+export type GlobalHint = Hint<States.Location>;
+
+const makeHint = (fct: functionsData): GlobalHint => ({
+  id: fct.key,
+  label: fct.name,
+  title: fct.signature,
+  value: { function: fct.name },
+});
+
+export function useHints(): [GlobalHint[], (pattern: string) => void] {
+  const fcts = States.useSyncArray(functions).getArray();
+  const [hints, setHints] = React.useState<GlobalHint[]>([]);
+  const onSearch = (pattern: string) => {
+    if (pattern === '') setHints([]);
+    else {
+      const p = pattern.toLowerCase();
+      setHints(fcts.filter((fn) => (
+        0 <= fn.name.toLowerCase().indexOf(p)
+      )).map(makeHint));
+    }
+  };
+  return [hints, onSearch];
+}
+
+// --------------------------------------------------------------------------
+// --- Globals Section(s)
 // --------------------------------------------------------------------------
 
 export default () => {
