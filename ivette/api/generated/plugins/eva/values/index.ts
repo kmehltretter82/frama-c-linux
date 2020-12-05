5fd2023fef95fbfc900b85f6486e9d21d4435e82
Loïc Correnson <loic.correnson@cea.fr>
2020-12-05 15:45:05 +0100

    [ivette/eva] draft value component
    [ivette/eva] draft value component

--- ivette/api/generated/plugins/eva/values/index.ts
+++ ivette/api/generated/plugins/eva/values/index.ts
@@ -24,131 +24,124 @@ import { jMarkerSafe } from 'frama-c/api/kernel/ast';
 //@ts-ignore
 import { marker } from 'frama-c/api/kernel/ast';
 
-/** CallStack */
-export interface callstack {
-  /** Callstack id */
-  id: number;
-  /** Short name for the callstack */
-  short: string;
-  /** Full name for the callstack */
-  full: string;
-}
-
-/** Loose decoder for `callstack` */
-export const jCallstack: Json.Loose<callstack> =
-  Json.jObject({
-    id: Json.jFail(Json.jNumber,'Number expected'),
-    short: Json.jFail(Json.jString,'String expected'),
-    full: Json.jFail(Json.jString,'String expected'),
-  });
-
-/** Safe decoder for `callstack` */
-export const jCallstackSafe: Json.Safe<callstack> =
-  Json.jFail(jCallstack,'Callstack expected');
-
-/** Natural order for `callstack` */
-export const byCallstack: Compare.Order<callstack> =
-  Compare.byFields
-    <{ id: number, short: string, full: string }>({
-    id: Compare.number,
-    short: Compare.string,
-    full: Compare.string,
-  });
-
-/** Data for array rows [`values`](#values)  */
-export interface valuesData {
-  /** Entry identifier. */
-  key: Json.key<'#values'>;
-  /** CallStack */
-  callstack: callstack;
-  /** Value inferred just before the selected point */
-  value_before: string;
-  /** Did the evaluation led to an alarm? */
-  alarm: boolean;
-  /** Value inferred just after the selected point */
-  value_after?: string;
-}
-
-/** Loose decoder for `valuesData` */
-export const jValuesData: Json.Loose<valuesData> =
-  Json.jObject({
-    key: Json.jFail(Json.jKey<'#values'>('#values'),'#values expected'),
-    callstack: jCallstackSafe,
-    value_before: Json.jFail(Json.jString,'String expected'),
-    alarm: Json.jFail(Json.jBoolean,'Boolean expected'),
-    value_after: Json.jString,
-  });
-
-/** Safe decoder for `valuesData` */
-export const jValuesDataSafe: Json.Safe<valuesData> =
-  Json.jFail(jValuesData,'ValuesData expected');
-
-/** Natural order for `valuesData` */
-export const byValuesData: Compare.Order<valuesData> =
-  Compare.byFields
-    <{ key: Json.key<'#values'>, callstack: callstack, value_before: string,
-       alarm: boolean, value_after?: string }>({
-    key: Compare.string,
-    callstack: byCallstack,
-    value_before: Compare.string,
-    alarm: Compare.boolean,
-    value_after: Compare.defined(Compare.string),
-  });
-
-/** Signal for array [`values`](#values)  */
-export const signalValues: Server.Signal = {
-  name: 'plugins.eva.values.signalValues',
+/** Emitted when EVA results has changed */
+export const changed: Server.Signal = {
+  name: 'plugins.eva.values.changed',
 };
 
-const reloadValues_internal: Server.GetRequest<null,null> = {
-  kind: Server.RqKind.GET,
-  name:   'plugins.eva.values.reloadValues',
-  input:  Json.jNull,
-  output: Json.jNull,
-};
-/** Force full reload for array [`values`](#values)  */
-export const reloadValues: Server.GetRequest<null,null>= reloadValues_internal;
-
-const fetchValues_internal: Server.GetRequest<
-  number,
-  { pending: number, updated: valuesData[], removed: Json.key<'#values'>[],
-    reload: boolean }
+const getCallstacks_internal: Server.GetRequest<
+  Json.key<'#stmt'>,
+  Json.index<'#eva-callstack-id'>[]
   > = {
   kind: Server.RqKind.GET,
-  name:   'plugins.eva.values.fetchValues',
-  input:  Json.jNumber,
+  name:   'plugins.eva.values.getCallstacks',
+  input:  Json.jKey<'#stmt'>('#stmt'),
+  output: Json.jList(Json.jIndex<'#eva-callstack-id'>('#eva-callstack-id')),
+};
+/** Callstacks for markers */
+export const getCallstacks: Server.GetRequest<
+  Json.key<'#stmt'>,
+  Json.index<'#eva-callstack-id'>[]
+  >= getCallstacks_internal;
+
+const getCallstackInfo_internal: Server.GetRequest<
+  Json.index<'#eva-callstack-id'>,
+  { calls:
+        { fct: Json.key<'#fct'>, stmt?: Json.key<'#stmt'>, rank: number }[],
+    descr: string }
+  > = {
+  kind: Server.RqKind.GET,
+  name:   'plugins.eva.values.getCallstackInfo',
+  input:  Json.jIndex<'#eva-callstack-id'>('#eva-callstack-id'),
   output: Json.jObject({
-            pending: Json.jFail(Json.jNumber,'Number expected'),
-            updated: Json.jList(jValuesData),
-            removed: Json.jList(Json.jKey<'#values'>('#values')),
-            reload: Json.jFail(Json.jBoolean,'Boolean expected'),
+            calls: Json.jList(
+                     Json.jObject({
+                       fct: Json.jFail(Json.jKey<'#fct'>('#fct'),
+                              '#fct expected'),
+                       stmt: Json.jKey<'#stmt'>('#stmt'),
+                       rank: Json.jFail(Json.jNumber,'Number expected'),
+                     })),
+            descr: Json.jFail(Json.jString,'String expected'),
           }),
 };
-/** Data fetcher for array [`values`](#values)  */
-export const fetchValues: Server.GetRequest<
-  number,
-  { pending: number, updated: valuesData[], removed: Json.key<'#values'>[],
-    reload: boolean }
-  >= fetchValues_internal;
+/** Callstack Description */
+export const getCallstackInfo: Server.GetRequest<
+  Json.index<'#eva-callstack-id'>,
+  { calls:
+        { fct: Json.key<'#fct'>, stmt?: Json.key<'#stmt'>, rank: number }[],
+    descr: string }
+  >= getCallstackInfo_internal;
 
-const values_internal: State.Array<Json.key<'#values'>,valuesData> = {
-  name: 'plugins.eva.values.values',
-  getkey: ((d:valuesData) => d.key),
-  signal: signalValues,
-  fetch: fetchValues,
-  reload: reloadValues,
-  order: byValuesData,
+const getStmtInfo_internal: Server.GetRequest<
+  Json.key<'#stmt'>,
+  { rank: number, fct: Json.key<'#fct'> }
+  > = {
+  kind: Server.RqKind.GET,
+  name:   'plugins.eva.values.getStmtInfo',
+  input:  Json.jKey<'#stmt'>('#stmt'),
+  output: Json.jObject({
+            rank: Json.jFail(Json.jNumber,'Number expected'),
+            fct: Json.jFail(Json.jKey<'#fct'>('#fct'),'#fct expected'),
+          }),
 };
-/** Abstract values inferred by the Eva analysis */
-export const values: State.Array<Json.key<'#values'>,valuesData> = values_internal;
+/** Stmt Information */
+export const getStmtInfo: Server.GetRequest<
+  Json.key<'#stmt'>,
+  { rank: number, fct: Json.key<'#fct'> }
+  >= getStmtInfo_internal;
 
-const getValues_internal: Server.GetRequest<marker,null> = {
+const getProbeInfo_internal: Server.GetRequest<
+  marker,
+  { code?: string, stmt?: Json.key<'#stmt'> }
+  > = {
+  kind: Server.RqKind.GET,
+  name:   'plugins.eva.values.getProbeInfo',
+  input:  jMarker,
+  output: Json.jObject({
+            code: Json.jString,
+            stmt: Json.jKey<'#stmt'>('#stmt'),
+          }),
+};
+/** Probe informations */
+export const getProbeInfo: Server.GetRequest<
+  marker,
+  { code?: string, stmt?: Json.key<'#stmt'> }
+  >= getProbeInfo_internal;
+
+const getValues_internal: Server.GetRequest<
+  { callstacks?: Json.index<'#eva-callstack-id'>, target: marker },
+  { v_else?: string, v_then?: string, v_after?: string, values?: string,
+    alarms: [ "True" | "False" | "Unknown", string ][] }
+  > = {
   kind: Server.RqKind.GET,
   name:   'plugins.eva.values.getValues',
-  input:  jMarker,
-  output: Json.jNull,
+  input:  Json.jObject({
+            callstacks: Json.jIndex<'#eva-callstack-id'>('#eva-callstack-id'),
+            target: jMarkerSafe,
+          }),
+  output: Json.jObject({
+            v_else: Json.jString,
+            v_then: Json.jString,
+            v_after: Json.jString,
+            values: Json.jString,
+            alarms: Json.jList(
+                      Json.jTry(
+                        Json.jPair(
+                          Json.jFail(
+                            Json.jUnion<"True" | "False" | "Unknown">(
+                              Json.jTag("True"),
+                              Json.jTag("False"),
+                              Json.jTag("Unknown"),
+                            ),'Union expected'),
+                          Json.jFail(Json.jString,'String expected'),
+                        ))),
+          }),
 };
-/** Get the abstract values computed for an expression or lvalue */
-export const getValues: Server.GetRequest<marker,null>= getValues_internal;
+/** Abstract values for the given marker */
+export const getValues: Server.GetRequest<
+  { callstacks?: Json.index<'#eva-callstack-id'>, target: marker },
+  { v_else?: string, v_then?: string, v_after?: string, values?: string,
+    alarms: [ "True" | "False" | "Unknown", string ][] }
+  >= getValues_internal;
 
 /* ------------------------------------- */
