9b16829c2d1123e8367d74f004a883781602f01c
Julien Signoles <julien.signoles@cea.fr>
2021-01-12 19:37:40 +0100

    [refman] first major upgrade since the origin independently from propagating ACSL evolutions

--- src/plugins/e-acsl/doc/refman/at_on-purely-logic-variables.c
+++ src/plugins/e-acsl/doc/refman/at_on-purely-logic-variables.c
@@ -1,12 +1,18 @@
 main(void) {
   int m = 2;
   int n = 7;;
-  K: ;
-  n = 875;
+ L1: ;
+  n = 4;
+ L2:
   /*@ assert
-      \let k = 3;
+      \let k = m + 1;
       \exists integer u; 9 <= u < 21 &&
       \forall integer v; -5 < v <= (u < 15 ? u + 6 : k) ==>
         \at(n + u + v > 0, K); */ ;
+  /*@ assert
+      \let k = m + 1;
+      \exists integer u; n <= u < 21 && // [u] depends on [n]
+      \forall integer v; -5 < v <= (u < 15 ? u + 6 : k) ==>
+        \at(n + u + v > 0, L1); */ ;
   return 0;
 }
