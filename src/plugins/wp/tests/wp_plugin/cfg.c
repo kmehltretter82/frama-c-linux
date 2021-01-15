fafaac9eb444cd700798a1ef35ca8df9f4ee91a3
Allan Blanchard <allan.blanchard@cea.fr>
2021-01-15 16:08:31 +0100

    [wp] More tests for buggy cfg

--- src/plugins/wp/tests/wp_plugin/cfg.c
+++ src/plugins/wp/tests/wp_plugin/cfg.c
@@ -1,11 +1,8 @@
 //@ assigns \nothing;
 void foo(void);
 
-/*@
-  ensures BUG_WP: \false;
-  assigns \nothing;
-*/
-void bar()
+/*@ ensures BUG_WP: \false; */
+void f1(void)
 {
   if (0 == 1)
     goto return_label;
@@ -14,5 +11,55 @@ void bar()
     foo();
     goto return_label;
   }
+  return_label: return;
+}
+
+/*@ ensures BUG_WP: \false; */
+void f1_simpler(void)
+{
+  if (0 == 1)
+    goto return_label;
+  {
+    int t1 = 1;
+    goto return_label;
+  }
+  return_label: return;
+}
+
+/*@ ensures BUG_WP: \false; */
+void f1_variant(void)
+{
+  if (0 == 1) L: ;
+  else {
+    int t1 = 1;
+    foo();
+    goto return_label;
+  }
+  return_label: return;
+}
+
+/*@ ensures FAILS_AS_EXPECTED: \false; */
+void f1_variant_invert(void)
+{
+  if (! (0 == 1)) {
+    int t1 = 1;
+    foo();
+    goto return_label;
+  }
+  else L: ;
+  return_label: return;
+}
+
+/*@ ensures BUG_WP: \false; */
+void f2(void)
+{
+  if (0 == 1)
+    goto L;
+  if (0 == 0) {
+    int t1 = 1;
+    foo();
+    goto return_label;
+  }
+  else L: ;
   return_label: return;
 }
