80af2310a4e1c697d8c83743058ae5e53d227f77
Loïc Correnson <loic.correnson@cea.fr>
2021-01-27 14:50:50 +0100

    [wp] fix loop labels

--- src/plugins/wp/cfgCalculus.ml
+++ src/plugins/wp/cfgCalculus.ml
@@ -208,20 +208,21 @@ struct
 
   (* Compute loops *)
   and loop env a s (lc : CfgAnnot.loop_contract) : W.t_prop =
-    begin
-      let loop_current = Clabels.loop_current s in
+    let loop_current = Clabels.loop_current s in
+    let established =
       W.label env.we None loop_current @@
-      List.fold_right (prove_property env) lc.loop_established @@
+      List.fold_right (prove_property env) lc.loop_established W.empty in
+    let presersed =
       List.fold_right (use_assigns env) lc.loop_assigns @@
       W.label env.we None loop_current @@
       List.fold_right (use_property env) lc.loop_invariants @@
       let q =
-        W.label env.we None (Clabels.loop_current s) @@
         List.fold_right (prove_property env) lc.loop_preserved @@
         List.fold_right (prove_assigns env) lc.loop_assigns @@
         W.empty in
       ( Vhash.replace env.wp a (Some q) ; successors env a )
-    end
+    in
+    W.merge env.we established presersed
 
   (* Merge transitions *)
   and successors env (a : vertex) = transitions env (succ env a)
