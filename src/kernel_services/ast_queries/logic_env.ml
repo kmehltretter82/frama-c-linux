ff1a3dcf5097541c8a9110fb186ecd05daa5c3e4
Virgile Prevosto <virgile.prevosto@m4x.org>
2021-01-29 11:07:08 +0100

    [logic] Proper error when 2 axiomatics have the same name.
    Fixes #1012

--- src/kernel_services/ast_queries/logic_env.ml
+++ src/kernel_services/ast_queries/logic_env.ml
@@ -146,6 +146,14 @@ module Lemmas =
       let size = 17
     end)
 
+module Axiomatics =
+  State_builder.Hashtbl(Datatype.String.Hashtbl)(Cil_datatype.Location)
+    (struct
+      let name = "Logic_env.Axiomatics"
+      let dependencies = []
+      let size = 17
+    end)
+
 module Model_info =
   State_builder.Hashtbl
     (Datatype.String.Hashtbl)
@@ -164,6 +172,7 @@ let init_dependencies from =
       Logic_type_info.self;
       Logic_ctor_info.self;
       Lemmas.self;
+      Axiomatics.self;
       Model_info.self;
     ]
 
@@ -330,6 +339,7 @@ let prepare_tables () =
   Logic_type_info.clear ();
   Logic_info.clear ();
   Lemmas.clear ();
+  Axiomatics.clear();
   Model_info.clear ();
   Logic_type_builtin.iter Logic_type_info.add;
   Logic_ctor_builtin.iter Logic_ctor_info.add;
