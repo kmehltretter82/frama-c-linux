22ea48cbfee80cd269475551aa01469df6c6d78d
David Bühler <david.buhler@cea.fr>
2021-02-11 08:47:16 +0100

    [kernel] Improves the AST transformation contract_finite_float.
    - Detects more postconditions leading to special floating-point values.
    - Detects only the special floating-point values forbidden by the kernel option
      -warn-special-float. The transformation now depends on this option.
    - The removed behaviors are also removed from complete and disjoint clauses.
    - Handles the case where no default behavior exists: creates the default
      behavior if needed. Otherwise, does not process the default behavior as the
      others.
    - Special case where all behaviors were complete and disjoint, and only one
      behavior remains after the transformation: promotes this behavior as the
      default behavior.

--- src/kernel_services/ast_transformations/contract_finite_float.ml
+++ src/kernel_services/ast_transformations/contract_finite_float.ml
@@ -22,64 +22,118 @@
 
 open Cil_types
 
-let rec is_infinite_or_nan pred =
-  match pred.pred_content with
-  | Pand(a,b) -> is_infinite_or_nan a || is_infinite_or_nan b
-  | Papp ({l_var_info = { lv_name = ("\\is_plus_infinity"
-                                    |"\\is_minus_infinity"
-                                    |"\\is_infinite"
-                                    |"\\is_NaN")}},[],[_]) -> true
+let is_special_float_term term =
+  match term.term_node with
+  | TLval (TVar {lv_name = "\\plus_infinity"}, _)
+  | TLval (TVar {lv_name = "\\minus_infinity"}, _) ->
+    Kernel.SpecialFloat.get () = "non-finite"
+  | TLval (TVar {lv_name = "\\NaN"}, _) -> true
   | _ -> false
 
-let visit = object
-  inherit Cil.nopCilVisitor
+let rec is_special_float_pred pred =
+  match pred.pred_content with
+  | Pand (a, b) -> is_special_float_pred a || is_special_float_pred b
+  | Por (a, b)  -> is_special_float_pred a && is_special_float_pred b
+  | Papp (logic_info, [], [_]) ->
+    begin
+      match logic_info.l_var_info.lv_name with
+      | "\\is_NaN" -> true
+      | "\\is_plus_infinity" | "\\is_minus_infinity" | "\\is_infinite" ->
+        Kernel.SpecialFloat.get () = "non-finite"
+      | _ -> false
+    end
+  | Pnot {pred_content = Papp
+              ({l_var_info = {lv_name = "\\is_finite"}}, [], [_])} ->
+    true
+  | Prel (Req, t1, t2) ->
+    (is_special_float_term t1) <> (is_special_float_term t2)
+  | _ -> false
 
-  method! vspec spec =
-    let requires, behaviors = Extlib.fold_map_opt
-        (fun acc bhv ->
-           let exists = List.exists (function
-               | (Normal,v) ->
-                 is_infinite_or_nan v.ip_content.tp_statement
-               | _ -> false
-             )
-               bhv.b_post_cond
-           in
-           if exists
-           then
+let ensures_special_float = function
+  | Normal, ip -> is_special_float_pred ip.ip_content.tp_statement
+  | _ -> false
+
+let are_complete_disjoint spec behaviors =
+  match spec.spec_complete_behaviors, spec.spec_disjoint_behaviors with
+  | [complete], [disjoint] ->
+    let mem bhv = List.(mem bhv.b_name complete && mem bhv.b_name disjoint) in
+    List.for_all mem behaviors
+  | _ -> false
+
+let find_behavior name spec =
+  let rec find acc = function
+    | [] -> None, spec.spec_behavior
+    | b :: tl -> if b.b_name = name then Some b, acc @ tl else find (b :: acc) tl
+  in
+  find [] spec.spec_behavior
+
+let update_spec spec =
+  let default, behaviors = find_behavior Cil.default_behavior_name spec in
+  let filter bhv = List.exists ensures_special_float bhv.b_post_cond in
+  let disabled, normal = List.partition filter behaviors in
+  if disabled <> [] then
+    match normal with
+    | [ bhv ] when are_complete_disjoint spec behaviors ->
+      let requires = bhv.b_requires @ bhv.b_assumes in
+      let default =
+        match default with
+        | None ->
+          bhv.b_name <- Cil.default_behavior_name;
+          bhv.b_requires <- requires;
+          bhv.b_assumes <- [];
+          bhv
+        | Some default ->
+          default.b_requires <- default.b_requires @ requires;
+          if bhv.b_assigns <> WritesAny
+          then default.b_assigns <- bhv.b_assigns;
+          if bhv.b_allocation <> FreeAllocAny
+          then default.b_allocation <- bhv.b_allocation;
+          default.b_post_cond <- default.b_post_cond @ bhv.b_post_cond;
+          default.b_extended <- default.b_extended @ bhv.b_extended;
+          default
+      in
+      spec.spec_behavior <- [default];
+      spec.spec_complete_behaviors <- [];
+      spec.spec_disjoint_behaviors <- [];
+    | _ ->
+      let requires =
+        List.map
+          (fun bhv ->
              let neg_assumes =
                List.map
                  (fun e -> (Logic_const.pnot (e.ip_content.tp_statement)))
-                 bhv.b_assumes
+                 (bhv.b_requires @ bhv.b_assumes)
              in
-             Logic_const.(new_predicate (pors neg_assumes))::acc
-           , None
-           else acc, Some bhv
-        )
-        [] spec.spec_behavior
-    in
-    spec.spec_behavior <- behaviors;
-    begin match requires with
-    | [] -> ()
-    | _ ->
-      List.iter
-        (fun bhv ->
-           if bhv.b_name = Cil.default_behavior_name then
-             bhv.b_requires <- requires@bhv.b_requires
-        )
-        spec.spec_behavior
-    end;
-    Cil.SkipChildren
+             Logic_const.(new_predicate (pors neg_assumes)))
+          disabled
+      in
+      let default =
+        match default with
+        | None -> Cil.mk_behavior ~requires ()
+        | Some default ->
+          default.b_requires <- default.b_requires @ requires;
+          default
+      in
+      spec.spec_behavior <- default :: normal;
+      let filter name = List.for_all (fun b -> b.b_name <> name) disabled in
+      let map_filter = List.map (List.filter filter) in
+      spec.spec_complete_behaviors <- map_filter spec.spec_complete_behaviors;
+      spec.spec_disjoint_behaviors <- map_filter spec.spec_disjoint_behaviors
 
+let visitor = object
+  inherit Cil.nopCilVisitor
+  method! vspec spec = update_spec spec; Cil.SkipChildren
 end
 
 let run ast =
-  if Kernel.ContractFiniteFloat.get () then
-    Cil.visitCilFileSameGlobals visit ast
+  if Kernel.ContractFiniteFloat.get ()
+  && Kernel.SpecialFloat.get () <> "none" then
+    Cil.visitCilFileSameGlobals visitor ast
 
 let transform =
   File.register_code_transformation_category "contract_finite_float"
 
 let () =
   File.add_code_transformation_before_cleanup
-    ~deps:[(module Kernel.ContractFiniteFloat)]
+    ~deps:[(module Kernel.ContractFiniteFloat); (module Kernel.SpecialFloat)]
     transform run
