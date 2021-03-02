a6d6f3a59c326dad352de0fc7268f37c9ba974b5
Michele Alberti <michele.alberti@cea.fr>
2021-03-02 13:10:47 +0100

    [Eva] Add second test case for taint analysis.

--- tests/value/taint.c
+++ tests/value/taint.c
@@ -1,5 +1,5 @@
 /* run.config*
-   STDOPT: +" -eva-domains taint -eva-msg-key=d-taint,-d-cvalue"
+   STDOPT: +" -eva-domains taint -eva-msg-key=d-taint,-d-cvalue -eva-auto-loop-unroll 10"
 */
 
 #include <__fc_builtin.h>
@@ -19,11 +19,26 @@ void taint_1(int t) {
   Frama_C_dump_each();
 }
 
+void taint_2(int t) {
+  int x = 0;
+  int buf[5] = { 0 };
+  while (t > 0) {
+      x = x + t;
+      t--;
+  }
+  buf[x] = buf[0] + 1;
+  Frama_C_dump_each();
+}
+
 int main(void) {
    int w;
    w = 0;
    //@ taint w;
    taint_1(w);
+   Frama_C_domain_show_each(w);
+   w = 2;
+   //@ taint w;
+   taint_2(w);
    Frama_C_domain_show_each(w);
    return 0;
 }
