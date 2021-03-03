50f8421128cd6a98c5ea52ed67ca6ea905b1d481
Andre Maroneze <andre.maroneze@cea.fr>
2021-03-03 09:38:49 +0100

    [Libc] fix and improve specification for frexp* and ldexp*
    Due to the absence of a long double version of the pow() logic function,
    and due to likely little demand for ldexpl, this function has not been
    specified.

--- tests/libc/math_h.c
+++ tests/libc/math_h.c
@@ -24,8 +24,8 @@ const double large = 1e38;
 const double huge_val = HUGE_VAL;
 const float huge_valf = HUGE_VALF;
 const long double huge_vall = HUGE_VALL;
-#endif
 const float infinity = INFINITY;
+#endif
 const double fp_ilogb0 = FP_ILOGB0;
 const double fp_ilogbnan = FP_ILOGBNAN;
 volatile int int_top;
@@ -42,7 +42,21 @@ volatile int int_top;
   TEST_VAL_CONST(type,f,large);                 \
   TEST_VAL_CONST(type,f,prefix##top)
 
-void test_simple_specs(void);
+// tests functions with 2 arguments, where the first one changes,
+// but the second one is fixed by the caller.
+// suffix prevents redeclaring variables with the same name
+#define TEST_VAL2_FST_CONST(type,f,cst,snd_arg,suffix) type f##_##cst##suffix = f(cst,snd_arg)
+#define TEST_FUN2_FST_CONSTS(type,f,prefix,snd_arg,suffix)    \
+  TEST_VAL2_FST_CONST(type,f,prefix##pi,snd_arg,suffix);            \
+  TEST_VAL2_FST_CONST(type,f,prefix##half_pi,snd_arg,suffix);  \
+  TEST_VAL2_FST_CONST(type,f,prefix##e,snd_arg,suffix);        \
+  TEST_VAL2_FST_CONST(type,f,zero,snd_arg,suffix);             \
+  TEST_VAL2_FST_CONST(type,f,minus_zero,snd_arg,suffix);       \
+  TEST_VAL2_FST_CONST(type,f,one,snd_arg,suffix);              \
+  TEST_VAL2_FST_CONST(type,f,minus_one,snd_arg,suffix);        \
+  TEST_VAL2_FST_CONST(type,f,large,snd_arg,suffix);            \
+  TEST_VAL2_FST_CONST(type,f,prefix##top,snd_arg,suffix)
+
 int main() {
   TEST_FUN_CONSTS(double,atan,);
   TEST_FUN_CONSTS(float,atanf,f_);
@@ -50,6 +64,22 @@ int main() {
   TEST_FUN_CONSTS(double,fabs,);
   TEST_FUN_CONSTS(float,fabsf,f_);
   TEST_FUN_CONSTS(long double,fabsl,ld_);
+  int exponent;
+  TEST_FUN2_FST_CONSTS(double,frexp,,&exponent,);
+  TEST_FUN2_FST_CONSTS(float,frexpf,f_,&exponent,);
+  TEST_FUN2_FST_CONSTS(long double,frexpl,ld_,&exponent,);
+  TEST_FUN2_FST_CONSTS(double,ldexp,,10,);
+  TEST_FUN2_FST_CONSTS(float,ldexpf,f_,10,);
+  //TEST_FUN2_FST_CONSTS(long double,ldexpl,ld_,10,);
+  TEST_FUN2_FST_CONSTS(double,ldexp,,0,_zero);
+  TEST_FUN2_FST_CONSTS(float,ldexpf,f_,0,_zero);
+  //TEST_FUN2_FST_CONSTS(long double,ldexpl,ld_,0,_zero);
+  TEST_FUN2_FST_CONSTS(double,ldexp,,-5,_minus5);
+  TEST_FUN2_FST_CONSTS(float,ldexpf,f_,-5,_minus5);
+  //TEST_FUN2_FST_CONSTS(long double,ldexpl,ld_,-5,_minus5);
+  TEST_FUN2_FST_CONSTS(double,ldexp,,100000,_huge);
+  TEST_FUN2_FST_CONSTS(float,ldexpf,f_,100000,_huge);
+  //TEST_FUN2_FST_CONSTS(long double,ldexpl,ld_,100000,_huge);
 
 #ifdef NONFINITE
   int r;
@@ -66,20 +96,4 @@ int main() {
   r = isfinite(NAN);
   //@ assert !r;
 #endif
-
-  test_simple_specs();
-}
-
-#define TEST_VAL_VAR(type,fn,...) type res_##fn = fn(__VA_ARGS__)
-
-#define TEST_FUN_VAR(fn,...)                    \
-  TEST_VAL_VAR(double,fn,__VA_ARGS__);          \
-  TEST_VAL_VAR(float,fn##f,__VA_ARGS__);        \
-  TEST_VAL_VAR(long double,fn##l,__VA_ARGS__);
-
-void test_simple_specs() {
-  int exponent;
-  TEST_FUN_VAR(frexp, ld_top, &exponent);
-  TEST_FUN_VAR(ldexp, ld_top, int_top);
-  //@ assert \initialized(&exponent);
 }
