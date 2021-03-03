54a444621e6d89270030deb978dfa1ec5d5c9000
Michele Alberti <michele.alberti@cea.fr>
2021-03-03 10:05:34 +0100

    [Eva] Add control statement information in abstract state for taint analysis.

--- src/plugins/value/domains/taint_domain.ml
+++ src/plugins/value/domains/taint_domain.ml
@@ -23,7 +23,10 @@
 open Cil_types
 open Locations
 
-type taint = Zone.t
+type taint = {
+  zone: Zone.t;
+  control_stmt: stmt option;
+}
 
 module LatticeTaint = struct
 
@@ -35,67 +38,122 @@ module LatticeTaint = struct
 
       let name = "Value.Taint.t"
 
-      let reprs = [ List.hd Zone.reprs; ]
+      let reprs =
+        [ { zone = List.hd Zone.reprs;
+            control_stmt = None; } ]
 
-      let structural_descr = Structural_descr.t_record [| Zone.packed_descr; |]
+      let structural_descr =
+        Structural_descr.t_abstract (* TODO *)
 
-      let compare = Zone.compare
+      let compare t1 t2 =
+        let c = Zone.compare t1.zone t2.zone in
+        if c <> 0
+        then c
+        else
+          Option.compare
+            Cil_datatype.Stmt.compare
+            t1.control_stmt t2.control_stmt
 
       let equal = Datatype.from_compare
 
-      let pretty fmt c = Format.fprintf fmt "@[<hov>%a@]" Zone.pretty c
+      let pretty fmt t =
+        Format.fprintf fmt
+          "@[<v 2>Zones:@ @[<hov>%a@]@]@.\
+           @[<v 2>Control stmt:@ @[<hov>%a@]@]"
+          Zone.pretty t.zone
+          (Pretty_utils.pp_opt Printer.pp_stmt) t.control_stmt
 
-      let hash = Zone.hash
+      let hash t =
+        Hashtbl.hash
+          (Zone.hash t.zone,
+           Hashtbl.hash t.control_stmt)
 
       let copy c = c
 
     end)
 
   (* Initial state at the start of the computation: nothing is tainted yet. *)
-  let empty = Zone.bottom
+  let empty = {
+    zone = Zone.bottom;
+    control_stmt = None;
+  }
 
   (* Top state: everything is tainted. *)
-  let top = Zone.top
+  let top = {
+    zone = Zone.top;
+    control_stmt = None;
+  }
 
-  (* Join: keep to over-approximate. *)
-  let join = Zone.join
+  (* Join: keep pointwise over-approximation. *)
+  let join t1 t2 =
+    let join_control_stmt cs1 cs2 =
+      let error () =
+        Value_parameters.fatal
+          "@[<hv>@[Different control statements:@]@ %a@ %a.@]"
+          (Pretty_utils.pp_opt Printer.pp_stmt) cs1
+          (Pretty_utils.pp_opt Printer.pp_stmt) cs2
+      in
+      match cs1, cs2 with
+      | None, None ->
+        cs1
+      | Some s1, Some s2 ->
+        if Cil_datatype.Stmt.equal s1 s2
+        then cs1
+        else error ()
+      | _ ->
+        error ()
+    in
+    { zone = Zone.join t1.zone t2.zone;
+      control_stmt = join_control_stmt t1.control_stmt t2.control_stmt; }
 
-  (* Remove from state. *)
-  let diff = Zone.diff
+  (* Add zone to state. *)
+  let add t e =
+    { t with zone = Zone.join t.zone e; }
+
+  (* Remove zone from state. *)
+  let remove t e =
+    { t with zone = Zone.diff t.zone e; }
 
   (* The memory locations are finite, so the ascending chain property is
      already verified. We simply use a join. *)
-  let widen _ _ c1 c2 = join c1 c2
+  let widen _ _ t1 t2 = join t1 t2
 
-  let narrow c1 c2 = `Value (Zone.narrow c1 c2)
+  let narrow t1 t2 =
+    `Value {
+      zone = Zone.narrow t1.zone t2.zone;
+      control_stmt = None;      (* TODO *)
+    }
 
   (* Inclusion testing: pointwise. *)
-  let is_included = Zone.is_included
+  let is_included t1 t2 =
+    Zone.is_included t1.zone t2.zone
 
   (* Intersection testing: pointwise. *)
-  let intersects = Zone.intersects
+  let intersects t e =
+    Zone.intersects t.zone e
 
 end
 
 
 module TransferTaint = struct
 
-  let state_of_taint_annot stmt =
+  let zone_of_taint_annot stmt =
     let zone_of_term t =
       match t.term_node with
       | TLval (TVar { lv_origin = Some vi }, TNoOffset) ->
         Locations.zone_of_varinfo vi
       | _ ->
-        (* TODO: Better stop with an error message? *)
-        Zone.bottom
+        (* TODO: Better message. *)
+        Value_parameters.not_yet_implemented ~current:true
+          "@[The taint domain currently supports only variables.@]"
     in
     match Eva_annotations.get_taint_annot stmt with
     | [] ->
-      LatticeTaint.empty
+      Zone.bottom
     | [ tt ] ->
       List.fold_left
-        (fun state t -> LatticeTaint.join state (zone_of_term t))
-        LatticeTaint.empty
+        (fun zones t -> Zone.join zones (zone_of_term t))
+        Zone.bottom
         tt
     | _ ->
       (* No more than one annotation at time. *)
@@ -114,7 +172,7 @@ module TransferTaint = struct
     | Kglobal ->
       `Value state
     | Kstmt stmt ->
-      let state = LatticeTaint.join state (state_of_taint_annot stmt) in
+      let state = LatticeTaint.add state (zone_of_taint_annot stmt) in
       let state =
         let to_loc = loc_of_lval valuation in
         let exp_zone = Value_util.zone_of_expr to_loc exp in
@@ -125,27 +183,27 @@ module TransferTaint = struct
           Value_util.(zone_of_expr to_loc (lval_to_exp lv.Eval.lval))
         in
         let intersect_state =
-          LatticeTaint.intersects exp_zone state ||
-          LatticeTaint.intersects lv_indirect_zone state
+          LatticeTaint.intersects state exp_zone ||
+          LatticeTaint.intersects state lv_indirect_zone
         in
         if intersect_state
-        then LatticeTaint.join lv_state state
-        else LatticeTaint.diff state lv_state
+        then LatticeTaint.add state lv_state
+        else LatticeTaint.remove state lv_state
       in
       `Value state
 
   let assume _stmt _exp _b _valuation state = `Value state
 
   let start_call stmt call valuation state =
-    let state = LatticeTaint.join state (state_of_taint_annot stmt) in
+    let state = LatticeTaint.add state (zone_of_taint_annot stmt) in
     let state =
       let to_loc = loc_of_lval valuation in
       List.fold_left
         (fun s { Eval.concrete; formal; _ } ->
            let concrete_zone = Value_util.zone_of_expr to_loc concrete in
            let formal_zone = Locations.zone_of_varinfo formal in
-           if LatticeTaint.intersects concrete_zone state
-           then LatticeTaint.join s formal_zone
+           if LatticeTaint.intersects state concrete_zone
+           then LatticeTaint.add s formal_zone
            else s)
         state
         call.Eval.arguments
@@ -153,7 +211,7 @@ module TransferTaint = struct
     `Value state
 
   let finalize_call stmt call ~pre ~post =
-    let state = LatticeTaint.join pre (state_of_taint_annot stmt) in
+    let state = LatticeTaint.add pre (zone_of_taint_annot stmt) in
     let state =
       match call.Eval.return with
       | None ->
@@ -161,7 +219,7 @@ module TransferTaint = struct
       | Some vi ->
         let result_zone = Locations.zone_of_varinfo vi in
         if LatticeTaint.intersects post result_zone
-        then LatticeTaint.join state result_zone
+        then LatticeTaint.add state result_zone
         else state
     in
     `Value state
@@ -169,7 +227,7 @@ module TransferTaint = struct
   let show_expr valuation state fmt exp =
     let to_loc = loc_of_lval valuation in
     let exp_zone = Value_util.zone_of_expr to_loc exp in
-    Format.fprintf fmt "%B" (LatticeTaint.intersects exp_zone state)
+    Format.fprintf fmt "%B" (LatticeTaint.intersects state exp_zone)
 
 end
 
@@ -239,13 +297,16 @@ module InternalTaint = struct
   let enter_scope _kind _vars state = state
 
   let leave_scope _kf vars state =
-    let bases =
-      List.fold_left
-        (fun acc v -> Base.Set.add (Base.of_varinfo v) acc)
-        Base.Set.empty
-        vars
+    let remove_unscoped_bases =
+      let bases =
+        List.fold_left
+          (fun acc v -> Base.Set.add (Base.of_varinfo v) acc)
+          Base.Set.empty
+          vars
+      in
+      Zone.filter_base (fun b -> not (Base.Set.mem b bases))
     in
-    Zone.filter_base (fun b -> not (Base.Set.mem b bases)) state
+    { state with zone = remove_unscoped_bases state.zone; }
 
 
   (* Initial state: initializers are singletons, so we store nothing. *)
