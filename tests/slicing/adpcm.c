51d6ea9d720336d405ff9e85b71194a44bee9cf5
Patrick Baudin <patrick.baudin@cea.fr>
2021-03-10 09:49:32 +0100

    [Slicing] fixes the use of -add-symbolic-path in some tests"

--- tests/slicing/adpcm.c
+++ tests/slicing/adpcm.c
@@ -2,7 +2,7 @@
    LIBS: libSelect
    CMXS: @PTEST_NAME@
    DEPS: ../../test/adpcm.c
-   STDOPT: +"-add-symbolic-path TESTS_DIR:../.. -load-module %{dep:@PTEST_NAME@.cmxs} -ulevel -1 -deps -slicing-level 2 -journal-disable"
+   STDOPT: +"-add-symbolic-path ../..:TESTS_DIR -load-module %{dep:@PTEST_NAME@.cmxs} -ulevel -1 -deps -slicing-level 2 -journal-disable"
 */
 
 #include "../../test/adpcm.c"
--- tests/slicing/variadic.c
+++ tests/slicing/variadic.c
@@ -1,10 +1,10 @@
 /* run.config
     DEPS: ../../pdg/variadic.c
-    STDOPT: +"-add-symbolic-path TESTS:../.. -slice-return f3 -no-slice-callers -journal-disable -then-on 'Slicing export' -print"
-    STDOPT: +"-add-symbolic-path TESTS:../.. -slice-return f3 -no-slice-callers -journal-disable -variadic-no-translation -then-last -print"
-    STDOPT: +"-add-symbolic-path TESTS:../.. -slice-return f3 -journal-disable -then-on 'Slicing export' -print"
-    STDOPT: +"-add-symbolic-path TESTS:../.. -slice-return main -journal-disable -then-on 'Slicing export' -print"
-    STDOPT: +"-add-symbolic-path TESTS:../.. -slice-return main -slicing-level 3  -journal-disable -then-on 'Slicing export' -print"
+    STDOPT: +"-add-symbolic-path ../..:TESTS_DIR -slice-return f3 -no-slice-callers -journal-disable -then-on 'Slicing export' -print"
+    STDOPT: +"-add-symbolic-path ../..:TESTS_DIR -slice-return f3 -no-slice-callers -journal-disable -variadic-no-translation -then-last -print"
+    STDOPT: +"-add-symbolic-path ../..:TESTS_DIR -slice-return f3 -journal-disable -then-on 'Slicing export' -print"
+    STDOPT: +"-add-symbolic-path ../..:TESTS_DIR -slice-return main -journal-disable -then-on 'Slicing export' -print"
+    STDOPT: +"-add-symbolic-path ../..:TESTS_DIR -slice-return main -slicing-level 3  -journal-disable -then-on 'Slicing export' -print"
 */
 
 #include "../../pdg/variadic.c"
