5c150f0b864b00aea34f90885ff027cb0e52c022
Loïc Correnson <loic.correnson@cea.fr>
2021-04-14 18:13:30 +0200

    [ivette] headers & distributed files

--- ivette/src/frama-c/api/generated/kernel/properties/index.ts
+++ ivette/src/frama-c/api/generated/kernel/properties/index.ts
@@ -1,3 +1,25 @@
+/* ************************************************************************ */
+/*                                                                          */
+/*   This file is part of Frama-C.                                          */
+/*                                                                          */
+/*   Copyright (C) 2007-2021                                                */
+/*     CEA (Commissariat à l'énergie atomique et aux énergies               */
+/*          alternatives)                                                   */
+/*                                                                          */
+/*   you can redistribute it and/or modify it under the terms of the GNU    */
+/*   Lesser General Public License as published by the Free Software        */
+/*   Foundation, version 2.1.                                               */
+/*                                                                          */
+/*   It is distributed in the hope that it will be useful,                  */
+/*   but WITHOUT ANY WARRANTY; without even the implied warranty of         */
+/*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          */
+/*   GNU Lesser General Public License for more details.                    */
+/*                                                                          */
+/*   See the GNU Lesser General Public License version 2.1                  */
+/*   for more details (enclosed in the file licenses/LGPLv2.1).             */
+/*                                                                          */
+/* ************************************************************************ */
+
 /* --- Generated Frama-C Server API --- */
 
 /**
@@ -70,6 +92,8 @@ export enum propKind {
   assert = 'assert',
   /** Check */
   check = 'check',
+  /** Hypothesis */
+  admit = 'admit',
   /** Clause `@loop invariant` */
   loop_invariant = 'loop_invariant',
   /** Clause `@loop assigns` */
@@ -96,6 +120,8 @@ export enum propKind {
   axiom = 'axiom',
   /** Logical lemma */
   lemma = 'lemma',
+  /** Logical check lemma */
+  check_lemma = 'check_lemma',
 }
 
 /** Loose decoder for `propKind` */
