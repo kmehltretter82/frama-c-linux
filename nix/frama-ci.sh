9c1c959b69b45a80c00a1fc7da524b2f66c01457
Virgile Prevosto <virgile.prevosto@m4x.org>
2021-04-23 16:49:00 +0200

    [ci] keeps results in artifacts

--- nix/frama-ci.sh
+++ nix/frama-ci.sh
@@ -9,4 +9,11 @@ export FRAMA_CI=$(nix-instantiate --eval -E "((import <nixos-20.03> {}).callPack
 FRAMA_CI=${FRAMA_CI#\"}
 FRAMA_CI=${FRAMA_CI%\"}
 
-$FRAMA_CI/compile.sh $@
+PIPE=$(mktemp)
+rm $PIPE
+mkfifo $PIPE
+tee results.log < $PIPE &
+$FRAMA_CI/compile.sh $@ > $PIPE 2>&1
+STATUS=$?
+rm $PIPE
+exit $STATUS
