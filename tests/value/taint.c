9f04f7e9e2cdbfde7ca64632d93207a8abd6e404
Michele Alberti <michele.alberti@cea.fr>
2021-04-29 17:39:30 +0200

    [Eva] Rework test file.

--- tests/value/taint.c
+++ tests/value/taint.c
@@ -5,128 +5,162 @@
 #include <__fc_builtin.h>
 
 volatile int undet;
-int global;
+int tainted;
 
-void taint_1(int t) {
-  int x, y;
-  int buf[5] = { 0 };
-  x = t + 1;
+void taint_basic(int t) {
+  int u, w, x, y = 0;
+  int buf[2] = { 0, 1 };
+
+  /* Basic data-dependency: since 't' is tainted, 'x' becomes (data-)tainted but 'y'
+     must stay untainted. */
+  x = t + y + 1;
+
+  /* Indirect data-dependency: since 't' is tainted, buf[t] becomes (data-)tainted. */
+  buf[t] = buf[1] + 1;
+
+  /* Data-dependency overapprox: since 'u' may take 't' in else-branch, then 'u'
+     becomes (data-)tainted. */
   if (undet)
-    y = 2;
-  else
-    y = x;
-  buf[y] = buf[0] + 1;
-  t = 1;
-  Frama_C_dump_each();
-}
-
-void taint_2(int t, int u) {
-  int x = 0;
-  int y = 4;
-  int buf[5] = { 0 };
-  while (t > 0) {
-    x = x + t;
-    t--;
-  }
-  buf[u] = 0;
-  buf[x] = buf[0] + 1;
-  buf[y] = buf[0];
-  /* 'y' must not be tainted here. */
-  if (y > 0) {
-    buf[1] = 2; // Hence neither 'buf[1]' must not.
-  }
-  Frama_C_dump_each();
-}
-
-void taint_3(int t) {
-  int x, y, z, u;
-  if (t > 0) {
-    x = 1;
-  }
-  global = x + 1;
-  y = z = u = 0;
-  while (z < 3) {
-    Frama_C_domain_show_each(z, y);
-    if (y % 2 == 0) {
-      y += 2;
-      Frama_C_domain_show_each(z, y);
-    }
-    //@ taint z;
-    z++;
-  }
-  if (!u)
     u = 1;
   else
-    u = 2;
+    u = t;
+
+  /* Basic control-dependency: since 't' is tainted, 'w' becomes
+     (control-)tainted no matter what. */
+  if (t > 1)
+    w = 1;
+  else
+    w = 2;
+
+  /* data-tainted: t, x, u, buf[t] (and 'tainted')
+     control-tainted: w
+     untainted: y */
   Frama_C_dump_each();
 }
 
-void taint_4() {
-  int z = 1;
-  if (global) {
-    /* Although 'z' is not tainted, all left-values in 'taint_1' must be tainted
-       as 'taint_1' is called depending on the value of 'global', which is
-       tainted. */
-    taint_1(z);
-    Frama_C_domain_show_each(z); // 'z' must remain untainted here.
-  }
-}
+void taint_assume_1() {
+  int x, y, z;
 
-/*@ assigns *t \from u; */
-void taint_5(int* t, int u);
-
-void taint_6() {
-  int i, u = 0;
-  /* Since 'i' becomes taint at some point, it will make 'u' tainted via a
-     control-dependency. At the end, both are tainted because of data- and
-     control- dependencies. */
-  for (i = 0; i < 8 || u <= 9; i++) {
-    //@ split i;
-    if (i == 5) {
-      //@ taint i;
-      i++;
+  x = y = z = 0;
+  /* 'x' becomes (data-)tainted at the end of first iteration, so the first two
+     call to Frama_C_dump_each must report no taint. On second iteration, the
+     first call to Frama_C_dump_each must report only 'x' as (data-)tainted,
+     while the second call must report 'y' as (control-)tainted, hence
+     (data-)tainted too because of add-assign. On the third iteration, the two
+     calls must report both 'x' and 'y' as data- and control-tainted. */
+  while (x < 3) {
+    Frama_C_dump_each();
+    if (y % 2 == 0) {
+      y += 2;
+      Frama_C_dump_each();
     }
-    u++;
+    //@ taint x;
+    x++;
   }
+
+  if (!z)
+    /* Although 'z' does not post-dominate the 'x < 3', this latter is no more
+       active here, hence 'z' remains untainted. */
+    z++;
+  /* data-tainted: x, y
+     control-tainted: x, y
+     untainted: z */
   Frama_C_dump_each();
 }
 
-void taint_7(int t) {
-  int x, y;
+void taint_assume_2() {
+  int x, y = 0;
+  /* As 'x' becomes (data-)tainted at some point, it will make 'y' tainted via a
+     control-dependency. At the end, both must be data- and control-tainted. */
+  for (x = 0; x < 8 || y <= 9; x++) {
+    //@ split x;
+    if (x == 5) {
+      //@ taint x;
+      x++;
+    }
+    y++;
+  }
+  /* data-tainted: x, y
+     control-tainted: x, y */
+  Frama_C_dump_each();
+}
+
+/*@ assigns *l \from t; */
+void taint_spec_assigns(int* l, int t);
+
+void taint_goto_1() {
+  int x, y, z, t;
+  //@ taint t;
+  t = undet;
   if (t > 0) {
     x = 1;
     goto L;
   }
   else {
-    /* Since this assign is always executed,
-       'y' should not be tainted. */
+    /* Since this assign is always executed, 'y' must not be tainted. */
     L: y = 0;
   }
+  z = 1;
+
+  /* data-tainted: t
+     control-tainted: x
+     untainted: y, z */
   Frama_C_dump_each();
 }
 
+void taint_goto_2() {
+  int x, y, z, t;
+  x = undet;
+  //@ taint t;
+  t = undet;
+  if (x > 0) {
+    if (t < 100) {
+      x = 1;
+      goto L;
+    }
+    /* As 'x' is untainted, 'z' must remain untainted. */
+    z = 1;
+  }
+  return;
+  /* Since this assign may be executed depending on the condition on 't', which
+     is tainted, 'y' must be (control-)tainted. */
+  L: y = 0;
+
+  /* data-tainted: t
+     control-tainted: x, y
+     untainted: z */
+  Frama_C_dump_each();
+}
+
+void taint_call(int t) {
+  int x = 0;
+  if (t >= 0) {
+    /* This call depends on the tainted 't', hence all left-values appearing in
+       taint_basic must be control-tainted. */
+    taint_basic(t);
+  }
+  /* 'x' must remain untainted here. */
+  Frama_C_domain_show_each(x);
+}
+
 int main(void) {
-  int w, z;
-  //@ taint w;
-  w = 0;
-  taint_1(w);
-  Frama_C_domain_show_each(w);
-  z = 0;
-  w = 2;
-  //@ taint w, z;
-  taint_2(w, z);
-  Frama_C_domain_show_each(w, z);
-  z = 1;
-  //@ taint w;
-  taint_3(w);
-  Frama_C_domain_show_each(w, global);
-  taint_4();
-  //@ taint w;
-  taint_5(&z, w);
-  Frama_C_domain_show_each(z, w);
-  taint_6();
-  w = 1;
-  //@ taint undet;
-  taint_7(undet);
+  //@ taint tainted;
+  taint_basic(tainted);
+
+  tainted = 0;
+  taint_assume_1();
+  taint_assume_2();
+
+  int l;
+  taint_spec_assigns(&l, tainted);
+  /* Here 'l' must be tainted. */
+  Frama_C_domain_show_each(l);
+
+  taint_goto_1();
+  taint_goto_2();
+
+  //@ taint tainted;
+  taint_call(tainted);
+
   return 0;
 }
