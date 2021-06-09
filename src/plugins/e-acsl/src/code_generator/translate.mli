83dd56665b5fa68f5a46302f3af3fb515f2cefb6
Basile Desloges <basile.desloges@cea.fr>
2021-06-09 14:24:52 +0200

    [eacsl] Expose `Translate.term_to_exp`

--- src/plugins/e-acsl/src/code_generator/translate.mli
+++ src/plugins/e-acsl/src/code_generator/translate.mli
@@ -53,6 +53,9 @@ val translate_predicate:
     If [pred_to_print] is set, then the runtime check will use this predicate as
     message. *)
 
+val term_to_exp: kernel_function -> Env.t -> term -> exp * Env.t
+(** Convert an ACSL term into a corresponding C expression. *)
+
 val translate_rte_annots:
   (Format.formatter -> 'a -> unit) ->
   'a ->
