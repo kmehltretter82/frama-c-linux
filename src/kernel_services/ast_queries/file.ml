09c0d624801f384ab5642f506429860471459802
Andre Maroneze <andre.maroneze@cea.fr>
2021-06-22 11:08:29 +0200

    [Kernel] fix typo in 16-bit machdep

--- src/kernel_services/ast_queries/file.ml
+++ src/kernel_services/ast_queries/file.ml
@@ -310,7 +310,7 @@ let default_machdeps =
   [ "x86_16", Machdeps.x86_16;
     "x86_32", Machdeps.x86_32;
     "x86_64", Machdeps.x86_64;
-    "gcc_x86_16", Machdeps.x86_16;
+    "gcc_x86_16", Machdeps.gcc_x86_16;
     "gcc_x86_32", Machdeps.gcc_x86_32;
     "gcc_x86_64", Machdeps.gcc_x86_64;
     "ppc_32", Machdeps.ppc_32;
