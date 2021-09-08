f2fbfec1a5de8c7b7fb9412ca773ae5030d4164c
Thibaut Benjamin <thibaut.benjamin@cea.fr>
2021-09-08 10:10:30 +0200

    [e-acsl] separate files for assigns clause generation

--- src/plugins/e-acsl/src/code_generator/logic_functions.ml
+++ src/plugins/e-acsl/src/code_generator/logic_functions.ml
@@ -23,8 +23,6 @@
 open Cil_types
 open Cil_datatype
 
-exception NoAssigns
-
 (**************************************************************************)
 (********************** Forward references ********************************)
 (**************************************************************************)
@@ -57,71 +55,6 @@ let term_to_exp_ref
 let result_as_extra_argument = Gmp_types.Z.is_t
 (* TODO: to be extended to any compound type? E.g. returning a struct is not
    good practice... *)
-
-(*****************************************************************************)
-(****************** Generation of function assigns ***************************)
-(*****************************************************************************)
-
-(* If an argument contains a pointer type, then it is undecidable which assigns
-   clause should be generated, so skip the assigns generation in this case *)
-let rec is_ptr_free typ = match Cil.unrollType typ with
-  | TVoid _
-  | TInt (_, _)
-  | TFloat (_, _) -> true
-  | TPtr (_, _) -> false
-  | TArray (ty, _, _, _) -> is_ptr_free ty
-  | TFun (_, _, _, _) ->
-    (* a function cannot be an argument of a function *)
-    assert false
-  | TNamed (_, _) ->
-    (* The named types are unfolded with [Cil.unrolltype] *)
-    assert false
-  | TEnum (_, _)
-  | TBuiltin_va_list _ -> true
-  | TComp (cinfo, _, _) ->
-    match cinfo.cfields with
-    | None -> raise NoAssigns
-    | Some fields ->
-      List.for_all
-        (fun fi -> fi.fbitfield <> None || is_ptr_free fi.ftype)
-        fields
-
-(* For a GMP argument of a function, we generate an assigns from its address,
-   since these are the only semantically valid operations on integers.
-   For the argument [__e_acsl_mpz_struct * x], this function generates the
-   expression [*(( __e_acsl_mpz_struct * )x)] *)
-let deref_gmp_arg ~loc var =
-  Smart_exp.lval ~loc
-    (Mem (Cil.mkAddrOrStartOf ~loc (Var var , NoOffset)), NoOffset)
-
-let rec get_assigns_from ~loc env lprofile lv =
-  match lprofile with
-  | [] -> []
-  | lvar :: lvars ->
-    let var = Env.Logic_binding.get env lvar  in
-    if is_ptr_free var.vtype then
-      Smart_exp.lval ~loc (Cil.var var) :: get_assigns_from ~loc env lvars lv
-    (* If the argument contains a pointer, but is an integer, then it is
-       necessarily a GMP type *)
-    else if lvar.lv_type = Linteger then
-        (deref_gmp_arg ~loc var) :: (get_assigns_from ~loc env lvars lv)
-      else begin
-        Options.warning ~current:true "skipping function %a when generating\
-                                       assigns because pointers as arguments\
-                                       is not yet supported"
-        Printer.pp_logic_var lv;
-        raise NoAssigns
-    end
-
-(* Special case when the function takes an extra argument as its result:
-   For the argument [__e_acsl_mpz_t *__retres_arg], this function generates the
-   expression [( *__retres_arg )[0]] *)
-let get_gmp_integer ~loc vi =
-  Smart_exp.lval
-    ~loc
-    (Mem
-       (Smart_exp.lval  ~loc (Var vi, NoOffset)),
-     (Index (Cil.zero ~loc, NoOffset)))
 
 (*****************************************************************************)
 (****************** Generation of function bodies ****************************)
@@ -281,13 +214,13 @@ let generate_kf ~loc fname env ret_ty params_ty li =
       List.fold_left2 add env li.l_profile params
     in
     let assigns_from =
-      try Some (get_assigns_from ~loc env li.l_profile li.l_var_info)
-      with NoAssigns -> None
+      try Some (Genassigns.get_assigns_from ~loc env li.l_profile li.l_var_info)
+      with Genassigns.NoAssigns -> None
     in
     let assigned_var =
       Logic_const.new_identified_term
         (if res_as_extra_arg then
-            Logic_utils.expr_to_term (get_gmp_integer ~loc ret_vi)
+            Logic_utils.expr_to_term (Genassigns.get_gmp_integer ~loc ret_vi)
         else
           Logic_const.tresult fundec.svar.vtype)
     in
