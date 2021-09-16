ea186dfc8a0516c6c07074ed433f2cc426f73fea
Loïc Correnson <loic.correnson@cea.fr>
2021-09-16 08:43:43 +0200

    [ivette] sandbox component

---
+++ ivette/src/ivette/sandbox.tsx
@@ -0,0 +1,12 @@
+/* -------------------------------------------------------------------------- */
+/* --- Sandbox Ivette Component.                                          --- */
+/* --- Only appears in DEVEL mode.                                        --- */
+/* --- Please, keep it empty.                                             --- */
+/* -------------------------------------------------------------------------- */
+
+import React from 'react';
+import { Label } from 'dome/controls/labels';
+
+export default function Sandbox() {
+  return <Label>Hello World!</Label>;
+}
