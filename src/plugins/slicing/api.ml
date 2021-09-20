c71c91ae5f0781466e10653d0ff8f8446c56c43b
Patrick Baudin <patrick.baudin@cea.fr>
2021-09-20 15:23:16 +0200

    [Slicing] adds missing mli files

--- src/plugins/slicing/api.ml
+++ src/plugins/slicing/api.ml
@@ -164,8 +164,8 @@ module Select = struct
   let dyn_t = SlicingTypes.Sl_select.ty
   type set = SlicingCmds.set
   module S = Cil_datatype.Varinfo.Map.Make(SlicingTypes.Fct_user_crit)
+  type selections = S.t
   let dyn_set = S.ty
-
   (** {2 Journalized selectors } *)
 
   let empty_selects = Journal.register
