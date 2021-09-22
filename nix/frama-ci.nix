3f045879f74322e958f6a5ac9fd01abfaa1bf1ec
Virgile Prevosto <virgile.prevosto@cea.fr>
2021-09-22 14:19:12 +0000

    Update nix/frama-ci.*

--- nix/frama-ci.nix
+++ nix/frama-ci.nix
@@ -5,7 +5,7 @@ let
     src = builtins.fetchGit {
             "url" = "https://bobot:${password}@git.frama-c.com/frama-c/Frama-CI.git";
             "name" = "Frama-CI";
-            "rev" = "a14f8e86909fdeed3a8a89cc6f28b5f3ce6a8a47";
+            "rev" = "738ec3236ef5217ee68fabad869f217b37410430";
             "ref" = "fix/interface-files";
     };
     pkgs = import "${src}/pkgs.nix" {};
