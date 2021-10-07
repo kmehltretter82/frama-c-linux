dd415c2a357c16aa8aed7f5fb47e13203bbeb1a2
Julien Signoles <julien.signoles@cea.fr>
2021-10-07 12:36:26 +0200

    [e-acsl:tests] lint

--- src/plugins/e-acsl/tests/arith/extended_quantifiers.c
+++ src/plugins/e-acsl/tests/arith/extended_quantifiers.c
@@ -19,13 +19,15 @@ int main(void) {
 
   /*@ assert \numof(2, 10, \lambda integer k; k - 2 >= 0) == 9; */;
   /*@ assert \numof(UINT_MAX - 5, UINT_MAX, \lambda integer k; k % 2 == 1)
-    @        == 3; */;
+    @        == 3; */
+  ;
 
   /*@ assert \product(1, 100, \lambda integer k; k) >= 3628800; */;
   /*@ assert \product(1, 10, \lambda integer k; k) == 3628800; */;
   /*@ assert \product(-10, 10, \lambda integer k; k) == 0; */;
   /*@ assert \product(-20, -1, \lambda integer k; 2 * k)
-    @     == \product(1, 20, \lambda integer k; 2 * k); */;
+    @     == \product(1, 20, \lambda integer k; 2 * k); */
+  ;
 
   return 0;
 }
