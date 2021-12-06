fbff099b2676b67b7ffaea9609784169e3c17e8e
David Bühler <david.buhler@cea.fr>
2021-12-06 13:41:07 +0100

    [kernel] Datatype: do not fail on unmarshable datatypes when [rehash] is defined.

--- src/libraries/datatype/datatype.ml
+++ src/libraries/datatype/datatype.ml
@@ -226,12 +226,12 @@ struct
 
   let mk_full_descr d =
     let descr =
-      if rehash == undefined then
-        if Descr.is_unmarshable d then Descr.unmarshable
-        else begin
-          check rehash "rehash" name "descriptor";
-          assert false
-        end
+      if Descr.is_unmarshable d then Descr.unmarshable
+      else
+      if rehash == undefined then begin
+        check rehash "rehash" name "descriptor";
+        assert false
+      end
       else
       if rehash == identity then d
       else
