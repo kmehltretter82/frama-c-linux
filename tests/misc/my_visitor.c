798c846a6f0e7a2e31f2caa1015b803f20ca93f1
Patrick Baudin <patrick.baudin@cea.fr>
2021-12-08 14:17:28 +0100

    [tests] renames generated result files

--- tests/misc/my_visitor.c
+++ tests/misc/my_visitor.c
@@ -1,10 +1,10 @@
 /* run.config
  PLUGIN:
  MODULE: @PTEST_NAME@
-   EXECNOW: LOG my_visitor_sav.res LOG my_visitor_sav.err BIN my_visitor.sav @frama-c@ @PTEST_FILE@ -main f -save @PTEST_DIR@/@PTEST_NAME@.sav > @PTEST_RESULT@/@PTEST_NAME@_sav.res 2> @PTEST_RESULT@/@PTEST_NAME@_sav.err
-   OPT: -load %{dep:@PTEST_DIR@/@PTEST_NAME@.sav} -no-my-visitor -print
+   EXECNOW: LOG my_visitor_sav.res LOG my_visitor_sav.err BIN my_visitor.sav @frama-c@ @PTEST_FILE@ -main f -save @PTEST_RESULT@/@PTEST_NAME@.sav > @PTEST_RESULT@/@PTEST_NAME@_sav.res 2> @PTEST_RESULT@/@PTEST_NAME@_sav.err
+   OPT: -load %{dep:@PTEST_RESULT@/@PTEST_NAME@.sav} -no-my-visitor -print
  MODULE:
-   OPT: -load %{dep:@PTEST_DIR@/@PTEST_NAME@.sav} -print
+   OPT: -load %{dep:@PTEST_RESULT@/@PTEST_NAME@.sav} -print
 */
 int f() {
   int y = 0;
