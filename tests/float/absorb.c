83ec9987e73788b8f71d3bb3d28521caa629bdc3
Patrick Baudin <patrick.baudin@cea.fr>
2022-01-24 18:34:58 +0100

    [tests] test that does not add missing @PTEST_FILE@

--- tests/float/absorb.c
+++ tests/float/absorb.c
@@ -4,13 +4,13 @@
    EXECNOW: BIN absorb.sav LOG absorb_sav.res LOG absorb_sav.err @frama-c@ -save @PTEST_RESULT@/absorb.sav @PTEST_FILE@ > @PTEST_RESULT@/absorb_sav.res 2> @PTEST_RESULT@/absorb_sav.err
  PLUGIN: @EVA_PLUGINS@
    EXECNOW: BIN absorb.sav2 LOG absorb_sav2.res LOG absorb_sav2.err @frama-c@ -load %{dep:@PTEST_RESULT@/absorb.sav} -eva @EVA_CONFIG@ -float-hex -save @PTEST_RESULT@/absorb.sav2 > @PTEST_RESULT@/absorb_sav2.res 2> @PTEST_RESULT@/absorb_sav2.err
- COMMENT: CMD: @frama-c@ @PTEST_OPTIONS@
+ COMMENT: the following CMD redefinition omits adding @PTEST_FILE@ on purpose.
+ CMD: @frama-c@ @PTEST_OPTIONS@
    OPT: -load %{dep:@PTEST_RESULT@/absorb.sav2} -deps -out -input
 */
 /* run.config*
    DONTRUN:
 */
-
 #include "__fc_builtin.h"
 
 float x = 1.0, y = 0.0, z, t, min_f, min_fl, den;
