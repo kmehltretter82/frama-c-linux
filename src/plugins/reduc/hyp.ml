3b580d4dc63174e7c8dc909bde95e5645e6c248a
Valentin Perrelle <valentin.perrelle@cea.fr>
2022-02-07 16:57:30 +0100

    [Reduc] Use the new Eva API

--- src/plugins/reduc/hyp.ml
+++ src/plugins/reduc/hyp.ml
@@ -36,12 +36,11 @@ class hypotheses_visitor (env: Collect.env) = object(self)
 
   method! vstmt_aux stmt =
     let kf = Option.get (self#current_kf) in
-    let state = Db.Value.get_stmt_state stmt in
     if Collect.should_annotate_stmt env stmt then begin
       let vars = Collect.get_relevant_vars_stmt env kf stmt in
       List.iter
         (fun e ->
-           let p_opt = pred_opt_from_expr_state state e in
+           let p_opt = pred_opt_from_expr_state stmt e in
            Option.iter (Misc.assert_and_validate ~kf stmt) p_opt)
         vars
     end;
