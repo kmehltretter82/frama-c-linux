74a9d6b6768c9e2cfd5c63731e87deff698e772d
Andre Maroneze <andre.maroneze@cea.fr>
2022-02-16 20:55:30 +0100

    [tests] dune: fix test directory 'libc'

--- tests/libc/runtime.c
+++ tests/libc/runtime.c
@@ -1,6 +1,6 @@
 /* run.config*
    COMMENT: tests that the runtime can compile without errors (for PathCrawler, E-ACSL, ...)
-   CMD: gcc -D__FC_MACHDEP_X86_64 @PTEST_SHARE_DIR@/libc/__fc_runtime.c -Wno-attributes -std=c99 -Wall -Wwrite-strings -o @DEV_NULL@
+   CMD: gcc -D__FC_MACHDEP_X86_64 @FRAMAC_SHARE@/libc/__fc_runtime.c -Wno-attributes -std=c99 -Wall -Wwrite-strings -o @DEV_NULL@
    OPT:
  */
 
