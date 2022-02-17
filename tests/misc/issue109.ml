16c86842c95472d8b7d5ffe489c97cd350e9dac1
Andre Maroneze <andre.maroneze@cea.fr>
2022-02-17 08:20:34 +0100

    [tests] dune: fix part of test directory 'misc'

--- tests/misc/issue109.ml
+++ tests/misc/issue109.ml
@@ -8,7 +8,7 @@ let ptest_file =
 let main () =
   Eva.Analysis.compute ();
   Dynamic.Parameter.String.set "" "";
-  Dynamic.Parameter.String.set "" (ptest_file "tests/misc/" "issue109.i");
+  Dynamic.Parameter.String.set "" (ptest_file "" "issue109.i");
   File.init_from_cmdline ();
   Eva.Analysis.compute ()
 
