f17526230af8d23b14f38888286c91dfbc87a2c7
Loïc Correnson <loic.correnson@cea.fr>
2022-02-23 14:33:58 +0100

    [server] fix documentation

--- src/plugins/server/data.mli
+++ src/plugins/server/data.mli
@@ -159,11 +159,11 @@ val declare :
     {[
       (* ---- Exemple of Record Data --- *)
       type r
-      let s = Record.signature ~page ~kind ~name ~descr () in
+      let s = Record.signature () in
       let fd_a = Record.field s ~name:"a" ~descr:"..." (module A) in
       let fd_b = Record.field s ~name:"b" ~descr:"..." (module B) in
-
-      module M = (val (Record.publish s) : Record with type r = r)
+      let r = Record.publish s ~page ~kind ~name ~descr
+      module M = (val r) : Record with type r = r)
 
       let make a b = M.default |> M.set fd_a a |> M.set fd_b b
     ]}
