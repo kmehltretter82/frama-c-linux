434a6de3d4abbc8d1be2ed40f083666f33681cb2
Julien Signoles <julien.signoles@cea.fr>
2022-03-01 14:39:26 +0100

    [e-acsl] style

--- src/plugins/e-acsl/src/analyses/typing.ml
+++ src/plugins/e-acsl/src/analyses/typing.ml
@@ -987,10 +987,10 @@ let typing_visitor lenv = object
   method !vpredicate p =
     (* Do not raise a warning for e-acsl errors at preprocessing time,
        those errrors are stored in the table and warnings are raised at
-       translation time*)
-    let _ = try type_named_predicate ~lenv p
-      with Error.Not_yet _ | Error.Typing_error _  -> ()
-    in
+       translation time *)
+    ignore
+      (try type_named_predicate ~lenv p
+       with Error.Not_yet _ | Error.Typing_error _  -> ());
     Cil.SkipChildren
 end
 
