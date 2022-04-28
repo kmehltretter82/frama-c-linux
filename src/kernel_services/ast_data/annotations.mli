4c17e7f9669fab75635ced69c46b422f9d4e33f5
Virgile Prevosto <virgile.prevosto@m4x.org>
2022-04-28 17:24:55 +0200

    [doc] remove unused @plugin dev guide directives

--- src/kernel_services/ast_data/annotations.mli
+++ src/kernel_services/ast_data/annotations.mli
@@ -297,13 +297,13 @@ val add_check:
   Emitter.t -> ?kf:kernel_function -> stmt -> predicate -> unit
 (** Add a checking assertion attached to the given statement. If [kf] is
     provided, the function runs faster.
-    @plugin development guide *)
+*)
 
 val add_admit:
   Emitter.t -> ?kf:kernel_function -> stmt -> predicate -> unit
 (** Add an hypothesis assertion attached to the given statement. If [kf] is
     provided, the function runs faster.
-    @plugin development guide *)
+*)
 
 val add_global: Emitter.t -> global_annotation -> unit
 (** Add a new global annotation into the program. *)
