08b872a0ccd875502718e7c2dc6f893e1543f22e
Nathan Koskas de Diego <nathan.koskasdediego@cea.fr>
2022-05-03 17:43:11 +0200

    update nix/{default,frama-ci}.nix

--- nix/frama-ci.nix
+++ nix/frama-ci.nix
@@ -5,7 +5,7 @@ let
     src = builtins.fetchGit {
             "url" = "https://bobot:${password}@git.frama-c.com/frama-c/Frama-CI.git";
             "name" = "Frama-CI";
-            "rev" = "22ccb4e8951b6ac6eb62b0b0c6a53f9a98c94951";
+            "rev" = "e33541f771435c6b1014acf1b042f569deadb663";
             "ref" = "master";
     };
     pkgs = import "${src}/pkgs.nix" {};
