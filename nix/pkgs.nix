46ff0fab86ece5952fce057f89de83b53ee1abea
Allan Blanchard <allan.blanchard@cea.fr>
2022-05-17 13:10:23 +0200

    [ci] add odoc recent version

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -7,6 +7,7 @@ let
     headache = oself.callPackage ./headache.nix {};
     mlmpfr = oself.callPackage ./mlmpfr.nix {};
     ocp-indent = oself.callPackage ./ocp-indent.nix {};
+    odoc = oself.callPackage ./odoc.nix {};
     psmt2-frontend = oself.callPackage ./psmt2-frontend.nix {};
     why3 = oself.callPackage ./why3.nix {};
     # Helpers
