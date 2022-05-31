2fdd2d2d0b2004369cfb9cd5a2a7cf9cf87c3f74
Loïc Correnson <loic.correnson@cea.fr>
2022-05-31 16:07:04 +0200

    [Db] generalization of [on|off|with|while]_progress

--- src/kernel_services/plugin_entry_points/db.mli
+++ src/kernel_services/plugin_entry_points/db.mli
@@ -947,55 +947,47 @@ type daemon
    daemon to be executed on each {!yield}.
    @param debounced the least amount of time between two successive calls to the
    daemon, in milliseconds (default is 0ms).
-   @param on_delayed the callback invoked as soon as the time since the last
+   @param on_delayed callback is invoked as soon as the time since the last
    {!yield} is greater than [debounced] milliseconds (or 100ms at least).
+   @param on_finished callback is invoked when the callback is unregistered.
 *)
 val on_progress :
-  ?debounced:int -> ?on_delayed:(int -> unit) -> (unit -> unit) -> daemon
+  ?debounced:int -> ?on_delayed:(int -> unit) -> ?on_finished:(unit -> unit) ->
+  (unit -> unit) -> daemon
 
 (** Unregister the [daemon]. *)
 val off_progress : daemon -> unit
+
+(** [while_progress ?debounced ?on_delayed ?on_finished trigger] is similar to
+    [on_progress] but the daemon is automatically unregistered
+    as soon as [trigger] returns [false].
+    Same optional parameters than [on_progress].
+*)
+val while_progress :
+  ?debounced:int -> ?on_delayed:(int -> unit) -> ?on_finished:(unit -> unit) ->
+  (unit -> bool) -> unit
+
+(**
+   [with_progress ?debounced ?on_delayed trigger job data] executes the given
+   [job] on [data] while registering [trigger] as temporary (debounced) daemon.
+   The daemon is finally unregistered at the end of the computation.
+   Same optional parameters than [on_progress].
+*)
+val with_progress :
+  ?debounced:int -> ?on_delayed:(int -> unit) -> ?on_finished:(unit -> unit) ->
+  (unit -> unit) -> ('a -> 'b) -> 'a -> 'b
 
 (** Trigger all daemons immediately. *)
 val flush : unit -> unit
 
 (** Trigger all registered daemons (debounced).
     This function should be called from time to time by all analysers taking
-    time. In GUI mode, this will make the interface reactive. *)
+    time. In GUI or Server mode, this will make the clients responsive. *)
 val yield : unit -> unit
 
-(** Interrupt the currently running job at the next call to {!yield} as a
-    [Cancel] exception is raised. *)
+(** Interrupt the currently running job: the next call to {!yield}
+    will raise a [Cancel] exception. *)
 val cancel : unit -> unit
-
-(**
-   [with_progress ?debounced ?on_delayed trigger job data] executes the given
-   [job] on [data] while registering [trigger] as temporary (debounced) daemon.
-   The daemon is finally unregistered at the end of the computation.
-
-   Illustrative example, where [...] is the debounced time:
-   {[
-     job data :    |<-------------------------------------------------->|<daemon removed>
-                   yields   :       x   x  x x    x             x   x    x   xxx xxx
-         trigger  :       |..........   |..........   |..........  |.........
-     delayed  :                                   !
-         notes    :      (1)           (2)           (3)
-   ]}
-
-   + First yield, normal trigger.
-   + Debounced yields leads to this second trigger.
-   + Delayed warning invoked since there was no yield for more than debounced period.
-
-   Raises every exception raised during the execution of [job] on [data].
-
-   @param debounced the least amount of time, in milliseconds, between two
-   successive calls to the daemon (default is 0ms).
-   @param on_delayed the callback invoked as soon as the time since the last
-   [yield] is greater than [debounced] milliseconds (or 100ms at least).
-*)
-val with_progress :
-  ?debounced:int -> ?on_delayed:(int -> unit) ->
-  (unit -> unit) -> ('a -> 'b) -> 'a -> 'b
 
 (**
    Pauses the currently running process for the specified time, in milliseconds.
