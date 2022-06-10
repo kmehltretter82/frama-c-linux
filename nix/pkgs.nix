20d153533395f8f920010ee1d8f928e3e4ed780a
Allan Blanchard <allan.blanchard@cea.fr>
2022-06-10 11:30:13 +0200

    [ci] check headers

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -10,12 +10,21 @@ let
     odoc = oself.callPackage ./odoc.nix {};
     psmt2-frontend = oself.callPackage ./psmt2-frontend.nix {};
     why3 = oself.callPackage ./why3.nix {};
+
     # Helpers
     mk_tests = oself.callPackage ./mk_tests.nix {};
     mk_plugin = oself.callPackage ./mk_plugin.nix {};
+
+    # Shell containing checkers (hdrck, ocp-indent)
+    frama-c-checkers-shell = oself.callPackage ./frama-c-checkers-shell.nix {
+      git = pkgs.git ;
+    };
+
     # Builds
     frama-c = oself.callPackage ./frama-c.nix {};
+    frama-c-hdrck = oself.callPackage ./frama-c-hdrck.nix {};
     lint = oself.callPackage ./lint.nix {};
+
     # Tests
     default-config-tests = oself.callPackage ./default-config-tests.nix {};
     e-acsl-tests = oself.callPackage ./e-acsl-tests.nix {};
@@ -24,8 +33,10 @@ let
     kernel-tests = oself.callPackage ./kernel-tests.nix {};
     plugins-tests = oself.callPackage ./plugins-tests.nix {};
     wp-tests = oself.callPackage ./wp-tests.nix {};
+
     # Internal tests
     internal-tests = oself.callPackage ./internal-tests.nix {};
+
     # Release
     api-doc = oself.callPackage ./api-doc.nix {};
     manuals = oself.callPackage ./manuals.nix {};
