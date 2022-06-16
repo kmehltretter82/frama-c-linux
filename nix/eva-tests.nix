c7894cfa8e93e30b180412e52335d7ec42a4d018
Allan Blanchard <allan.blanchard@cea.fr>
2022-06-16 16:15:09 +0200

    [ci] Split Eva tests

--- nix/eva-tests.nix
+++ nix/eva-tests.nix
@@ -1,13 +1,15 @@
-{ mk_tests } :
+{ mk_tests, config } :
+
+let eva-tests = "eva-tests" + (if config == "" then "" else "-" + config); in
+let ptests = "ptests_config" + (if config == "" then "" else "_" + config) ; in
+let tbuiltins = " @tests/builtins/" + ptests ; in
+let tfloat = " @tests/float/" + ptests ; in
+let tidct = " @tests/idct/" + ptests ; in
+let tvalue = " @tests/value/" + ptests ; in
 
 mk_tests {
-  tests-name = "eva-tests";
+  tests-name = eva-tests ;
   tests-command = ''
     dune exec -- frama-c-ptests tests
-    dune build -j1 --display short \
-      @tests/builtins/ptests \
-      @tests/float/ptests \
-      @tests/idct/ptests \
-      @tests/value/ptests
-  '';
+    dune build -j1 --display short'' + tbuiltins + tfloat + tidct + tvalue + "\n" ;
 }
