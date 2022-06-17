249682ff6c7f8cc9729dd9634a1224455e783038
Patrick Baudin <patrick.baudin@cea.fr>
2022-06-17 08:07:41 +0200

    lint some files

--- doc/developer/tutorial/viewcfg/src/dump_function_memo_clear_cache.ml
+++ doc/developer/tutorial/viewcfg/src/dump_function_memo_clear_cache.ml
@@ -1,5 +1,5 @@
 let dump_function fundec fmt =
-  if not (Value_is_computed.get ()) && Eva.Analysis.is_computed () then begin 
+  if not (Value_is_computed.get ()) && Eva.Analysis.is_computed () then begin
     Value_is_computed.set true;
     let selection = State_selection.with_dependencies Cfg_graph_state.self in
     Project.clear ~selection ();
