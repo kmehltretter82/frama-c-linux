aafe44ccad43bf2d5cfc20d2cfa880c2ab388a39
Patrick Baudin <patrick.baudin@cea.fr>
2022-07-04 14:38:07 +0200

    [WP] tests - Adds explicit deps to WP cache env_var

--- src/plugins/wp/tests/wp_gallery/binary-multiplication.c
+++ src/plugins/wp/tests/wp_gallery/binary-multiplication.c
@@ -3,7 +3,7 @@
 */
 
 /* run.config_qualif
-   DEPS: @WP_SESSION@/script/BinaryMultiplication_*.json
+   DEPS: @PTEST_DEPS@ @WP_SESSION@/script/BinaryMultiplication_*.json
    OPT: -wp-rte -wp-prover=alt-ergo,script -wp-prop=-lack @USING_WP_SESSION@
 */
 
--- src/plugins/wp/tests/wp_tip/split.i
+++ src/plugins/wp/tests/wp_tip/split.i
@@ -1,5 +1,5 @@
 /* run.config
-   DEPS: @WP_SESSION@/script/test_*.json
+   DEPS: @PTEST_DEPS@ @WP_SESSION@/script/test_*.json
    OPT: -wp-par 1 -wp-no-print -wp-prover qed,script -wp-msg-key script @USING_WP_SESSION@
 */
 /* run.config_qualif
