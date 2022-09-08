4f1ec0fe5c3354d4d6fd633bc9974426a4984584
Andre Maroneze <andre.maroneze@cea.fr>
2022-09-08 10:55:36 +0200

    [Kernel] add libdir/libdirs (as FRAMAC_LIB)

--- src/kernel_internals/runtime/fc_config.mli
+++ src/kernel_internals/runtime/fc_config.mli
@@ -61,6 +61,15 @@ val framac_libc: Filepath.Normalized.t
 (** Directory where Frama-C libc headers are.
     @since 19.0-Potassium *)
 
+val libdirs: Filepath.Normalized.t list
+(** Directories where library and executable files are, in order of
+    priority.
+    @since Frama-C+dev *)
+
+val libdir: Filepath.Normalized.t
+(** Last directory of libdirs (the directory of frama-c installation)
+    @since Frama-C+dev *)
+
 val plugin_dir: Filepath.Normalized.t list
 (** Directory where the Frama-C dynamic plug-ins are. *)
 
--- src/kernel_internals/runtime/special_hooks.ml
+++ src/kernel_internals/runtime/special_hooks.ml
@@ -31,10 +31,12 @@ let print_config () =
           "Frama-C %s@\n\
            Environment:@\n  \
            FRAMAC_SHARE  = %S@\n  \
-           FRAMAC_PLUGIN = %S%t@."
+           FRAMAC_PLUGIN = %S@\n  \
+           FRAMAC_LIB    = %S%t@."
           Fc_config.version_and_codename
           (String.concat ":" (Filepath.Normalized.to_string_list Fc_config.datadirs))
           (String.concat ":" (Filepath.Normalized.to_string_list Fc_config.plugin_dir))
+          (String.concat ":" (Filepath.Normalized.to_string_list Fc_config.libdirs))
           (fun fmt ->
              if Fc_config.preprocessor = "" then
                Format.fprintf fmt "@\nWarning: no default pre-processor"
@@ -67,7 +69,7 @@ let () = Cmdline.run_after_early_stage print_version
 let print_sharepath = print_configl Kernel.PrintShare.get Fc_config.datadirs
 let () = Cmdline.run_after_early_stage print_sharepath
 
-let print_libpath = print_configl Kernel.PrintLib.get Fc_config.plugin_dir
+let print_libpath = print_configl Kernel.PrintLib.get [Fc_config.libdir]
 let () = Cmdline.run_after_early_stage print_libpath
 
 let print_pluginpath =
