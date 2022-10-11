70ab1b6bd66d47ea02ac060bf7cca26331a22d5f
Allan Blanchard <allan.blanchard@cea.fr>
2022-10-11 10:26:05 +0200

    [ci] generate server doc

--- nix/api-doc.nix
+++ nix/api-doc.nix
@@ -1,6 +1,7 @@
 { lib
 , stdenv
 , frama-c
+, pandoc
 , odoc
 } :
 
@@ -9,27 +10,28 @@ stdenv.mkDerivation rec {
   version = frama-c.version;
   slang = frama-c.slang;
 
-  src = frama-c.src;
-
-  nativeBuildInputs = frama-c.nativeBuildInputs;
+  src = frama-c.build_dir + "/dir.tar";
+  sourceRoot = ".";
 
   buildInputs = frama-c.buildInputs ++ [
+    pandoc
     odoc
   ];
 
-  preConfigure = frama-c.preConfigure;
-
   buildPhase = ''
     dune build -j1 @doc
-
     cp -r _build/default/_doc/_html frama-c-api
-
     echo ".dummy" > excluded
     tar czf frama-c-api.tar.gz -X excluded frama-c-api
+
+    make server-doc NO_BUILD_FRAMAC=yes
+    cp -r doc/server frama-c-server-api
+    tar czf frama-c-server-api.tar.gz frama-c-server-api
   '';
 
   installPhase = ''
     mkdir -p $out
     cp frama-c-api.tar.gz $out
+    cp frama-c-server-api.tar.gz $out
   '';
 }
