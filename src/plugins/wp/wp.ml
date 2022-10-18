35dca2a81f9634a97c1571a3ece26fb654ee39a1
Loïc Correnson <loic.correnson@cea.fr>
2022-10-18 10:41:17 +0200

    [wp] documenting API

--- src/plugins/wp/wp.ml
+++ src/plugins/wp/wp.ml
@@ -22,18 +22,74 @@
 
 (** This the API of the WP plug-in *)
 
-(** {1 WP Core calculus} *)
+(* -------------------------------------------------------------------------- *)
+(** {1 High-Level External API}
 
-module CfgAnnot = CfgAnnot
-module CfgCalculus = CfgCalculus
-module CfgCompiler = CfgCompiler
-module CfgDump = CfgDump
-module CfgGenerator = CfgGenerator
-module CfgInfos = CfgInfos
-module CfgInit = CfgInit
-module CfgWP = CfgWP
+   The following modules are the recommanded entry points for using WP
+   programmatically. They are meant to be relatively stable over time.
 
-(** {1 Memory models}*)
+*)
+(* -------------------------------------------------------------------------- *)
+
+(** WP Proof Obligation Generator and Management *)
+module VC = VC
+
+(** Provers and Proof Obligations Results *)
+module VCS = VCS
+
+(** WP Plugin Interface *)
+module Wp_parameters = Wp_parameters
+
+(* -------------------------------------------------------------------------- *)
+(** {1 Advanced Usage API}
+
+   The following modules entry points for using WP advanced features,
+   such as proof obligation manipulation, tactics and strategies.
+   These modules might expose internal features of WP that are subject
+   to change. Developpers using this API are encouraged to contact us
+   for a roadmap information and further collaboration.
+*)
+(* -------------------------------------------------------------------------- *)
+
+(** High-Level Term Representation *)
+module Repr = Repr
+
+(** Low-Level Logic Terms and Predicates *)
+module Lang = Lang
+
+(** Generated Logic Definitions *)
+module Definitions = Definitions
+
+(** Proof Task and Simplifiers *)
+module Conditions = Conditions
+
+(** Tactics Entry Points *)
+module Tactical = Tactical
+
+(** Strategies Entry Points *)
+module Strategy = Strategy
+
+(** WP Proof Obligation Generator *)
+module Generator = Generator
+
+(** Command Line Processing *)
+module Register = Register
+
+(* -------------------------------------------------------------------------- *)
+(** {1 Low-Level Internal API}
+
+   The following modules are _not_ intended to be used externally. The target
+   audience is WP plug-in developpers. However, developpers interested in
+   such low-level features are encouraged to contact us for more informations.
+*)
+(* -------------------------------------------------------------------------- *)
+
+(** {2 Model Registration} *)
+
+module Factory = Factory
+module WpContext = WpContext
+
+(** {2 Memory Models} *)
 
 module MemDebug = MemDebug
 module MemEmpty = MemEmpty
@@ -45,98 +101,99 @@ module MemVal = MemVal
 module MemVar = MemVar
 module MemZeroAlias = MemZeroAlias
 
-(** {2 Alias analysis and hypotheses}*)
+(** {2 Other Models} *)
+
+module Cint = Cint
+module Cfloat = Cfloat
+module Cmath = Cmath
+module Cstring = Cstring
+
+(** {2 State Model} *)
+
+module Sigma = Sigma
+module Passive = Passive
+module Mstate = Mstate
+
+(** {2 Model Hypotheses}*)
 
 module MemoryContext = MemoryContext
 module RefUsage = RefUsage
 module WpTarget = WpTarget
+module AssignsCompleteness = AssignsCompleteness
 
-(** {1 Compiler}*)
+(** {2 Region Analysis} *)
 
+module Region = Region
+module Layout = Layout
+module RegionAccess = RegionAccess
+module RegionAnalysis = RegionAnalysis
+module RegionAnnot = RegionAnnot
+module RegionDump = RegionDump
+
+(** {2 Compilers} *)
+
+module Sigs = Sigs
+module Driver = Driver
+module Context = Context
+module Ctypes = Ctypes
+module Cvalues = Cvalues
+module Clabels = Clabels
 module CodeSemantics = CodeSemantics
-module Conditions = Conditions
-module Definitions = Definitions
 module LogicAssigns = LogicAssigns
 module LogicBuiltins = LogicBuiltins
 module LogicCompiler = LogicCompiler
 module LogicSemantics = LogicSemantics
 module LogicUsage = LogicUsage
 module StmtSemantics = StmtSemantics
-
-module AssignsCompleteness = AssignsCompleteness
-module Cache = Cache
-module Cfloat = Cfloat
-module Cint = Cint
-module Clabels = Clabels
-module Cleaning = Cleaning
-module Cmath = Cmath
-module Context = Context
-module Cstring = Cstring
-module Ctypes = Ctypes
-module Cvalues = Cvalues
-module Driver = Driver
 module Dyncall = Dyncall
-module Factory = Factory
-module Filter_axioms = Filter_axioms
-module Filtering = Filtering
-module Footprint = Footprint
-module Generator = Generator
-module Lang = Lang
-module Layout = Layout
-module Letify = Letify
 module Matrix = Matrix
-module Mstate = Mstate
 module NormAtLabels = NormAtLabels
-module Passive = Passive
-module Pcfg = Pcfg
-module Pcond = Pcond
-module Plang = Plang
-module ProofSession = ProofSession
+
+(** {2 Core Engine} *)
+
+module CfgAnnot = CfgAnnot
+module CfgCalculus = CfgCalculus
+module CfgCompiler = CfgCompiler
+module CfgDump = CfgDump
+module CfgGenerator = CfgGenerator
+module CfgInfos = CfgInfos
+module CfgInit = CfgInit
+module CfgWP = CfgWP
+module WpReached = WpReached
+module WpPropId = WpPropId
+module WpRTE = WpRTE
+
+(** {2 Proof Engine} *)
+
+module Wpo = Wpo
+module Auto = Auto
+module Cache = Cache
+module Cleaning = Cleaning
+module Letify = Letify
+module Splitter = Splitter
+module Filtering = Filtering
+
+(** {2 Prover Interface} *)
+
+module Why3Provers = Why3Provers
+module Filter_axioms = Filter_axioms
 module Prover = Prover
-module ProverScript = ProverScript
-module ProverSearch = ProverSearch
 module ProverTask = ProverTask
 module ProverWhy3 = ProverWhy3
 
-module RegionAccess = RegionAccess
-module RegionAnalysis = RegionAnalysis
-module RegionAnnot = RegionAnnot
-module RegionDump = RegionDump
-module Region = Region
-module Register = Register
-module Repr = Repr
-module Rformat = Rformat
-module Sigma = Sigma
-module Sigs = Sigs
-module Splitter = Splitter
-module Stats = Stats
-module VC = VC
-module VCS = VCS
-module Warning = Warning
-module Why3Provers = Why3Provers
-module WpContext = WpContext
-module Wp_error = Wp_error
-module Wp_eva = Wp_eva
-module Wpo = Wpo
-module Wp_parameters = Wp_parameters
-module WpPropId = WpPropId
-module WpReached = WpReached
-module WpReport = WpReport
-module Wprop = Wprop
-module WpRTE = WpRTE
-module WpTac = WpTac
+(** {2 Script Engine} *)
 
-(** {1 Interactive proof} *)
-
-module Auto = Auto
+module Script = Script
+module Footprint = Footprint
 module ProofEngine = ProofEngine
 module ProofScript = ProofScript
-module Script = Script
-module Strategy = Strategy
-module Tactical = Tactical
+module ProverScript = ProverScript
+module ProverSearch = ProverSearch
+module ProofSession = ProofSession
 
-(** {2 Tactics}*)
+(** {2 Tactics} *)
 
+module WpTac = WpTac
 module TacArray = TacArray
 module TacBitrange = TacBitrange
 module TacBittest = TacBittest
@@ -160,3 +217,23 @@ module TacSequence = TacSequence
 module TacShift = TacShift
 module TacSplit = TacSplit
 module TacUnfold = TacUnfold
+
+(** {2 Error Management} *)
+
+module Warning = Warning
+module Wp_error = Wp_error
+
+(** {2 Printers and Reporting} *)
+
+module Pcfg = Pcfg
+module Pcond = Pcond
+module Plang = Plang
+module Rformat = Rformat
+module Stats = Stats
+module WpReport = WpReport
+
+(** {2 EVA Proxy} *)
+
+module Wp_eva = Wp_eva
+
+(* -------------------------------------------------------------------------- *)
