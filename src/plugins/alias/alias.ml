50eb84c5bffca9e5821247223e0c1305cb11e099
Tristan Le Gall <tristan.le-gall@cea.fr>
2022-12-05 16:57:22 +0100

    [alias] test ocp-indent 2

---
+++ src/plugins/alias/alias.ml
@@ -0,0 +1,48 @@
+(**************************************************************************)
+(*                                                                        *)
+(*  This file is part of the Frama-C plug-in 'Alias' (alias).             *)
+(*                                                                        *)
+(*  Copyright (C) 2022-2022                                               *)
+(*    CEA (Commissariat à l'énergie atomique et aux énergies              *)
+(*         alternatives)                                                  *)
+(*                                                                        *)
+(*  you can redistribute it and/or modify it under the terms of the GNU   *)
+(*  Lesser General Public License as published by the Free Software       *)
+(*  Foundation, version 2.1.                                              *)
+(*                                                                        *)
+(*  It is distributed in the hope that it will be useful,                 *)
+(*  but WITHOUT ANY WARRANTY; without even the implied warranty of        *)
+(*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *)
+(*  GNU Lesser General Public License for more details.                   *)
+(*                                                                        *)
+(*  See the GNU Lesser General Public License version 2.1                 *)
+(*  for more details (enclosed in the file LICENSE)                       *)
+(*                                                                        *)
+(**************************************************************************)
+
+let compute () =
+  failwith "not implemented"
+
+let clear () =
+  failwith "not implemented"
+
+let get_class_before_statement _ =
+  failwith "not implemented"
+
+let get_class_after_statement _ =
+  failwith "not implemented"
+
+let  get_class_fundec _ =
+  failwith "not implemented"
+
+let fold_fundec_stmts _ =
+  failwith "not implemented"
+
+let is_equivalent  _ =
+  failwith "not implemented"
+
+let fold_points_to _ =
+  failwith "not implemented"
+
+let fold_points_to_closure  _ =
+  failwith "not implemented"
