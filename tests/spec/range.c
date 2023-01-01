dca49ebb69436aa0df5452589dce19fbf06d0bed
Virgile Prevosto <virgile.prevosto@m4x.org>
2023-01-01 11:20:14 +0100

    preparing test case for pub/frama-c#2639

--- tests/spec/range.c
+++ tests/spec/range.c
@@ -1,3 +1,6 @@
+/* run.config
+OPT: -print -ocode @PTEST_NAME@_reparse.c -then @PTEST_NAME@_reparse.c -print -ocode=""
+*/
 struct foo { char bar[4]; };
 
 /*@ assigns x->bar[0..3] \from x->bar[0..3]; */
@@ -9,3 +12,6 @@ struct bli { baz bli; };
 
 /*@ assigns x[0..3] \from y->bli[0..3]; */
 int g(baz x,struct bli* y);
+
+/*@ assigns *(x + (0 .. ((1 << 2) - 2))) \from \let range = (0 .. 4-1); x[range]; */
+int h(baz x);
