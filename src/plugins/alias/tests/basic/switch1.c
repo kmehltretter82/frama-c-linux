7909e1168c440d7ccd6254351c9243df555f6c16
Tristan Le Gall <tristan.le-gall@cea.fr>
2023-01-19 11:19:19 +0100

    [alias] switch test files

---
+++ src/plugins/alias/tests/basic/switch1.c
@@ -0,0 +1,17 @@
+// control structure and arrays
+// { a; b } are aliased
+
+int main ()
+{
+  int *a=0, *b=0, *c=0, *d=0, e=0;
+  switch (e) {
+  case 1:
+    *a=e;
+    break;
+  case 2:
+    *b=e;
+    break;
+  }
+    
+  return 0;
+}
