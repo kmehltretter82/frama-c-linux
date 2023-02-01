4955ba0451d58f7f23f21ba58ca52e6c6daa3610
Andre Maroneze <andre.maroneze@cea.fr>
2023-02-01 09:28:18 +0100

    [headers] update E-ACSL headers copyright (2022 -> 2023)

--- src/plugins/e-acsl/src/code_generator/literal_observer.mli
+++ src/plugins/e-acsl/src/code_generator/literal_observer.mli
@@ -2,7 +2,7 @@
 (*                                                                        *)
 (*  This file is part of the Frama-C's E-ACSL plug-in.                    *)
 (*                                                                        *)
-(*  Copyright (C) 2012-2022                                               *)
+(*  Copyright (C) 2012-2023                                               *)
 (*    CEA (Commissariat à l'énergie atomique et aux énergies              *)
 (*         alternatives)                                                  *)
 (*                                                                        *)
