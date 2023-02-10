ea3f5e2ba22f35f6bd96a6fad573d41cbb233667
Valentin Perrelle <valentin.perrelle@cea.fr>
2023-02-10 18:29:16 +0100

    [Eva] evast: implements type_of_exp for every origin

--- src/plugins/eva/ast/evast_utils.ml
+++ src/plugins/eva/ast/evast_utils.ml
@@ -22,7 +22,9 @@
 
 open Evast
 
-let rec type_of_offset basetyp = function
+type typ = Cil_types.typ
+
+let rec type_of_offset (basetyp : typ) : offset -> typ = function
   | NoOffset -> basetyp
   | Index (_, o) ->
     type_of_offset (Cil.typeOf_array_elem basetyp) o
@@ -36,17 +38,33 @@ let rec type_of_offset basetyp = function
     in
     type_of_offset (Cil.typeAddAttributes base_attrs fi.ftype) o
 
-let type_of_exp e =
-  match e.origin with
-  | Exp exp -> Cil.typeOf exp
-  | Built | Term _ -> assert false (* TODO *)
-
-let type_of_lval (host, offset) =
+let rec type_of_lval (host, offset) =
   let basetyp = match host with
     | Var vi -> vi.vtype
     | Mem addr -> Cil.typeOf_pointed (type_of_exp addr)
   in
   type_of_offset basetyp offset
+
+and type_of_exp (exp : exp) =
+  match exp.node with (* TODO: rely more on Cil by storing the Frama-C infered type on Evast const nodes ? *)
+  | Const (CInt64 (_, ik, _)) -> Cil_types.TInt (ik, [])
+  | Const (CChr _) -> Cil.intType
+  | Const (CStr _s) -> Cil.theMachine.stringLiteralType
+  | Const (CWStr _s) -> TPtr (Cil.theMachine.wcharType, [])
+  | Const (CReal (_, fk, _)) -> TFloat (fk, [])
+  | Const (CEnum {eival=e}) -> Cil.typeOf e
+  | Lval (lv) -> Cil.type_remove_qualifier_attributes (type_of_lval lv)
+  | SizeOf _ | SizeOfE _ | SizeOfStr _ -> Cil.theMachine.typeOfSizeOf
+  | AlignOf _ | AlignOfE _ -> Cil.theMachine.typeOfSizeOf
+  | UnOp (_, _, t) -> t
+  | BinOp (_, _, _, t) -> t
+  | CastE (t, _) -> t
+  | AddrOf (lv) -> TPtr (type_of_lval lv, [])
+  | StartOf (lv) ->
+    match Cil.unrollType (type_of_lval lv) with
+    | TArray (t,_,attrs) -> TPtr(t, attrs)
+    | _ ->  assert false
+
 
 let origin_exp e =
   match e.origin with
