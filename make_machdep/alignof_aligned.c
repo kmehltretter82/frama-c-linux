478e175392351c15f692fc57ce5c031da2cd05a1
Virgile Prevosto <virgile.prevosto@m4x.org>
2023-02-17 14:37:37 +0100

    [machdep] update alignof_aligned test to new infrastructure

--- make_machdep/alignof_aligned.c
+++ make_machdep/alignof_aligned.c
@@ -22,6 +22,21 @@
 
 #include "make_machdep_common.h"
 
-char array[1] __attribute__((aligned));
+typedef char array[1] __attribute__((__aligned__));
 
-unsigned char alignof_aligned = ALIGNOF(array);
+_Static_assert(ALIGNOF(array)!=1, "alignof_aligned is 1");
+_Static_assert(ALIGNOF(array)!=2, "alignof_aligned is 2");
+_Static_assert(ALIGNOF(array)!=3, "alignof_aligned is 3");
+_Static_assert(ALIGNOF(array)!=4, "alignof_aligned is 4");
+_Static_assert(ALIGNOF(array)!=5, "alignof_aligned is 5");
+_Static_assert(ALIGNOF(array)!=6, "alignof_aligned is 6");
+_Static_assert(ALIGNOF(array)!=7, "alignof_aligned is 7");
+_Static_assert(ALIGNOF(array)!=8, "alignof_aligned is 8");
+_Static_assert(ALIGNOF(array)!=9, "alignof_aligned is 9");
+_Static_assert(ALIGNOF(array)!=10, "alignof_aligned is 10");
+_Static_assert(ALIGNOF(array)!=11, "alignof_aligned is 11");
+_Static_assert(ALIGNOF(array)!=12, "alignof_aligned is 12");
+_Static_assert(ALIGNOF(array)!=13, "alignof_aligned is 13");
+_Static_assert(ALIGNOF(array)!=14, "alignof_aligned is 14");
+_Static_assert(ALIGNOF(array)!=15, "alignof_aligned is 15");
+_Static_assert(ALIGNOF(array)!=16, "alignof_aligned is 16");
