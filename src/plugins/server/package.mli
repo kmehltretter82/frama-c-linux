b472e294c62798d9c3436f697f3ccc4cdf0fb9a3
Loïc Correnson <loic.correnson@cea.fr>
2023-03-01 17:01:34 +0100

    [server] fix data mli doc

--- src/plugins/server/package.mli
+++ src/plugins/server/package.mli
@@ -33,19 +33,19 @@ type jtype =
   | Jboolean
   | Jnumber
   | Jstring
-  | Jalpha (* string primarily compared without case *)
-  | Jtag of string (* single constant string *)
-  | Jkey of string (* kind of a string used for indexing *)
-  | Jindex of string (* kind of an integer used for indexing *)
+  | Jalpha (** string primarily compared without case *)
+  | Jtag of string (** single constant string *)
+  | Jkey of string (** kind of a string used for indexing *)
+  | Jindex of string (** kind of an integer used for indexing *)
   | Joption of jtype
-  | Jdict of jtype (* dictionaries *)
-  | Jarray of jtype (* order matters *)
+  | Jdict of jtype (** dictionaries *)
+  | Jarray of jtype (** order matters *)
   | Jtuple of jtype list
   | Junion of jtype list
   | Jrecord of (string * jtype) list
-  | Jenum of ident * string list (* type and tags *)
-  | Jdata of ident * jtype (* type and definition *)
-  | Jself (* for (simply) recursive types *)
+  | Jenum of ident * string list (** type and tags *)
+  | Jdata of ident * jtype (** type and definition *)
+  | Jself (** for (simply) recursive types *)
 
 type fieldInfo = {
   fd_name: string;
