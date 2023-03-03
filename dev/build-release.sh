d28eef8e63e9fbf48161abe65cdb8947f9886684
Allan Blanchard <allan.blanchard@cea.fr>
2023-03-03 10:33:13 +0100

    [ci] escape \ during release build

--- dev/build-release.sh
+++ dev/build-release.sh
@@ -356,8 +356,8 @@ cat >$JSON_DATA <<EOL
     ]
   },
 EOL
-  echo "  \"description\": \"# Main changes since $PREVIOUS $PREVIOUS_NAME\n$(cat "$CHANGES" | sed -z 's/\n/\\n/g' | sed 's/\(\#.*\)/#\1/')\"" >> $JSON_DATA
-  echo "}" >> $JSON_DATA
+echo "  \"description\": \"# Main changes since $PREVIOUS $PREVIOUS_NAME\n$(cat "$CHANGES" | sed -z 's/\\/\\\\/g' | sed -z 's/\n/\\n/g' | sed -z 's/\(\#.*\)/#\1/')\"" >> $JSON_DATA
+echo "}" >> $JSON_DATA
 
 echo "Release data file built"
 
