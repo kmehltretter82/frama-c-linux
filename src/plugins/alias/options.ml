c8cf848159f486d0b1c91b8ac0a97b1b00cd4e36
Tristan Le Gall <tristan.le-gall@cea.fr>
2023-04-03 14:56:27 +0200

    [alias] lint

--- src/plugins/alias/options.ml
+++ src/plugins/alias/options.ml
@@ -65,7 +65,7 @@ module Warn = struct
   let unsupported_function = register_warn_category "unsupported:fn"
   let undefined_function = register_warn_category "undefined:fn"
   let no_return_stmt = register_warn_category "no-return"
-  let unsupported_address = register_warn_category "unsupported:addr" 
+  let unsupported_address = register_warn_category "unsupported:addr"
 end
 
 module DebugKeys = struct
