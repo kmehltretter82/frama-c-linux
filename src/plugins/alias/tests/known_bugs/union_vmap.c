76fc22d4c5a2963d6eb5f3a23e7bb0f48dee6af1
Jan Rochel <jan.rochel@cea.fr>
2023-04-06 11:14:34 +0200

    [alias] tests/known_bugs/union_vmap.c
    Reveals a problem with the union algorithm.
    It should use a union-find data structure.

---
+++ src/plugins/alias/tests/known_bugs/union_vmap.c
@@ -0,0 +1,19 @@
+char* CPS_SplitWord(char *line)
+{
+  return line;
+}
+
+void CPS_ParseKey()
+{
+	char **key;
+  char *s2 = CPS_SplitWord("a");
+  char *s3 = CPS_SplitWord("b");
+  if (*s3) {
+    *key = s3;
+  }
+  else {
+    *key = s2;
+  }
+}
+
+
