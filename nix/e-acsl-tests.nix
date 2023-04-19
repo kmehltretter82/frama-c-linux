69ddc8465359d8c4f439fab59018eb5523b54d42
Allan Blanchard <allan.blanchard@cea.fr>
2023-04-19 17:44:40 +0200

    [ci] E-ACSL dev tests are allowed to fail

--- nix/e-acsl-tests.nix
+++ nix/e-acsl-tests.nix
@@ -1,9 +1,12 @@
-{ mk_tests } :
+{ mk_tests, config } :
+
+let e-acsl-tests = "e-acsl-tests" + (if config == "" then "" else "-" + config); in
+let ptests = "ptests_config" + (if config == "" then "" else "_" + config) ; in
+let tests = " @src/plugins/e-acsl/tests/" + ptests ; in
 
 mk_tests {
-  tests-name = "e-acsl-tests";
+  tests-name = e-acsl-tests ;
   tests-command = ''
     dune exec -- frama-c-ptests -never-disabled tests src/plugins/e-acsl/tests
-    dune build -j1 --display short @src/plugins/e-acsl/tests/ptests
-  '';
+    dune build -j1 --display short'' + tests + "\n" ;
 }
