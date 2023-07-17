017e00e4cac0c30d0c186be7082d707c3815499e
Allan Blanchard <allan.blanchard@cea.fr>
2023-07-17 16:56:24 +0200

    [nix] do not cover default config

--- nix/default-config-tests.nix
+++ nix/default-config-tests.nix
@@ -4,6 +4,7 @@ let mk_tests_distrib = mk_tests.override {
   frama-c = frama-c-nocover ;
 }; in
 mk_tests_distrib {
+  cover = false ;
   tests-name = "src-distrib-tests";
   tests-command = ''
     dune exec -- frama-c-ptests -never-disabled tests src/plugins/*/tests
