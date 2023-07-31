3635401a109ef122749b1562ce25a69b8e52e001
Valentin Perrelle <valentin.perrelle@cea.fr>
2023-07-31 16:17:05 +0200

    [Eva] mthread: create the interferences record after the first analysis
    - The interferences is then created with the analyzer's domains
      structure instead that with the default analyzer.

--- src/plugins/eva/mthread/mtMain.ml
+++ src/plugins/eva/mthread/mtMain.ml
@@ -138,7 +138,6 @@ let mthread_run project =
     concurrent_accesses_by_nodes = [];
     known_ids = MtIds.no_known_ids;
   } in
-  let interferences = MtInterferences.initial () in
 
   (* We register our callback function *)
   hook_builtins (Some analysis);
@@ -165,6 +164,7 @@ let mthread_run project =
     let results = Eva_results.get_results () in
     main_th.th_value_results <- Some results;
 
+    let interferences = MtInterferences.initial () in
     MtAnalysisFixpoint.record_end_of_thread_analysis analysis interferences;
 
     (* We perform the analysis iterations *)
