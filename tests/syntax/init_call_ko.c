840e8e047fe320a75f7ccf6b47660b6c7abec820
Pierre Nigron <pierre.nigron@cea.fr>
2023-08-25 10:10:29 +0200

    Comment test + Oracle

--- tests/syntax/init_call_ko.c
+++ tests/syntax/init_call_ko.c
@@ -5,11 +5,11 @@
 char *b(char* c){ return c; }
 
 int main(void) {
-    char a[] = b("");
-    char m[] = 1;
-    char p[1] = 0;
-    extern char j[] = {0,1};
-    char f[];
-    static char n[];
+    char a[] = b(""); // cannot be initialized with a function call
+    char m[] = 1; // cannot be initialized with a constant
+    char p[1] = 0; // even if sized
+    extern char j[] = {0,1}; // extern local variable cannot be initialized
+    char f[]; // unsized array must be initialized
+    static char n[]; // even if static
     return 0;
 }
