87c72bb87df10008098458b7de8b3ed79c11f3d9
Pierre Nigron <pierre.nigron@cea.fr>
2023-09-06 09:12:58 +0200

    [test] Add test values of globals in recrusive main

---
+++ src/plugins/wp/tests/wp/main_called.c
@@ -0,0 +1,11 @@
+/* run.config
+   EXIT : 1
+   STDOPT : -wp -wp-prover qed
+ */
+int main_called = 0;
+
+/*@ ensures main_called == 0; */
+int main() {
+    if (main_called) { /*@ assert \false; */ }
+    else { main_called = 1; return main(); }
+}
