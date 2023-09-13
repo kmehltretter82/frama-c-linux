c88b8ad732fb28202c7971ab9fa5b46413f768d5
David Bühler <david.buhler@cea.fr>
2023-09-13 10:33:21 +0200

    [ivette] Updates generated server API.

--- ivette/src/frama-c/kernel/api/ast/index.ts
+++ ivette/src/frama-c/kernel/api/ast/index.ts
@@ -469,18 +469,18 @@ export const parseExpr: Server.GetRequest<
   >= parseExpr_internal;
 
 const parseLval_internal: Server.GetRequest<
-  { term: string, stmt: marker },
+  { stmt: marker, term: string },
   marker
   > = {
   kind: Server.RqKind.GET,
   name:   'kernel.ast.parseLval',
-  input:  Json.jObject({ term: Json.jString, stmt: jMarker,}),
+  input:  Json.jObject({ stmt: jMarker, term: Json.jString,}),
   output: jMarker,
   signals: [],
 };
 /** Parse a C lvalue and returns the associated marker */
 export const parseLval: Server.GetRequest<
-  { term: string, stmt: marker },
+  { stmt: marker, term: string },
   marker
   >= parseLval_internal;
 
