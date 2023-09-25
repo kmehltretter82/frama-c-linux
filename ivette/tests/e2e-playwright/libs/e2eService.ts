a0f1ce8ff7de7331cfdffd1119ef65908eda6389
Damien Iriberry <damien.iriberry@artal.fr>
2023-09-25 17:20:13 +0200

    Debug tests

--- ivette/tests/e2e-playwright/libs/e2eService.ts
+++ ivette/tests/e2e-playwright/libs/e2eService.ts
@@ -37,17 +37,16 @@ export const argsDefaultLaunch: string[] = [
 export const argsLaunchWithDefaultSettings: string[] = [
   "./dist/main/main.js",
   "--no-sandbox",
+  "--init-settings",
   "--command",
   "../bin/frama-c",
-  "--init-settings",
 ];
 
 // Electron launch of Ivette for Playwright
 // e2e tests using an additional C file loaded
 export const argsLaunchWithTestFile: string[] = [
   "./dist/main/main.js",
-  "--with-fixed-settings",
-  "./tests/settings.json",
+  "--no-sandbox",
   "--command",
   "../bin/frama-c",
   "../tests/test/adpcm.c",
