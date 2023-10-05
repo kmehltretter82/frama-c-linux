20aaead7e508c55f4a2ed95cd117254b56e079c1
Michele Alberti <michele.alberti@cea.fr>
2023-10-05 17:45:55 +0200

    [ivette/tests] Add playwright 'list' reporter, do not open 'html' report by default.

--- ivette/playwright.config.ts
+++ ivette/playwright.config.ts
@@ -41,8 +41,8 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: 1,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters. */
+  reporter: [['list', { printSteps: true }], ['html', { open: 'never' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
