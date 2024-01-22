fd06ef40a268a755464511412c263aa838c2f6ab
Loïc Correnson <loic.correnson@cea.fr>
2024-01-22 17:47:43 +0100

    [wp] probes are allowed without counter examples

---
+++ src/plugins/wp/tests/wp_plugin/matrix.c
@@ -0,0 +1,23 @@
+/* run.config_qualif
+   DONTRUN:
+ */
+
+// Matrix addessing
+
+int a[3][4][5];
+
+//@ ensures \false; assigns \nothing;
+void get(int i, int j, int k)
+{
+ A:
+  if (i) i++;
+ B:
+  if (j) i++;
+ C:
+  if (k) i++;
+  //@ probe A: \at(i,A) - \at(i,Pre);
+  //@ probe B: \at(i,B) - \at(i,Pre);
+  //@ probe C: \at(i,C) - \at(i,Pre);
+  //@ probe Offset: &a[i][j][k] - &a[0][0][0];
+  return;
+}
