9a4e7f695b6610d26898d6038593e6ef3855299b
Maxime Jacquemin <maxime.jacquemin@cea.fr>
2024-01-23 14:32:06 +0100

    [Librairies] Let bindings for the Option module
    The Stdlib does not export those let bindings so we have to do it.

---
+++ src/libraries/utils/option.ml
@@ -0,0 +1,32 @@
+(**************************************************************************)
+(*                                                                        *)
+(*  This file is part of Frama-C.                                         *)
+(*                                                                        *)
+(*  Copyright (C) 2007-2023                                               *)
+(*    CEA (Commissariat à l'énergie atomique et aux énergies              *)
+(*         alternatives)                                                  *)
+(*                                                                        *)
+(*  you can redistribute it and/or modify it under the terms of the GNU   *)
+(*  Lesser General Public License as published by the Free Software       *)
+(*  Foundation, version 2.1.                                              *)
+(*                                                                        *)
+(*  It is distributed in the hope that it will be useful,                 *)
+(*  but WITHOUT ANY WARRANTY; without even the implied warranty of        *)
+(*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *)
+(*  GNU Lesser General Public License for more details.                   *)
+(*                                                                        *)
+(*  See the GNU Lesser General Public License version 2.1                 *)
+(*  for more details (enclosed in the file licenses/LGPLv2.1).            *)
+(*                                                                        *)
+(**************************************************************************)
+
+include Stdlib.Option
+
+let zip l r = match l, r with Some l, Some r -> Some (l, r) | _ -> None
+
+module Operators = struct
+  let ( let* ) m f = bind m f
+  let ( let+ ) m f = map f m
+  let ( and* ) l r = zip l r
+  let ( and+ ) l r = zip l r
+end
