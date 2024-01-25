20452cc6d1d39d31e3561aa2922d041bc367ec72
Virgile Prevosto <virgile.prevosto@m4x.org>
2024-01-25 19:03:24 +0100

    [kernel] Better localization when start_pos=end_pos

--- src/kernel_internals/parsing/errorloc.ml
+++ src/kernel_internals/parsing/errorloc.ml
@@ -199,10 +199,13 @@ let pp_context_from_file ?(ctx=2) ?start_pos fmt pos =
           if last_error_line <> first_error_line then
             Format.fprintf fmt "\n"
           else begin
+            let len = pos.pos_cnum - pos.pos_bol - start_char + 1 in
+            (* output at least one '^' *)
+            let len = if len = 0 then 1 else len in
             let cursor =
               String.make 6 ' ' ^
               String.make (start_char - 1) ' ' ^
-              String.make (pos.pos_cnum - pos.pos_bol - start_char + 1) '^'
+              String.make len '^'
             in
             Format.fprintf fmt "%s\n" cursor
           end;
