34c4d462ec8271018261350cc4fdae2fc3887f94
David Bühler <david.buhler@cea.fr>
2024-01-30 16:00:27 +0100

    [Kernel] Adds test skeleton of module Linear_filter.

---
+++ tests/float/linear_filter_test.ml
@@ -0,0 +1,7 @@
+(* Programmatic tests of the invariant computation of linear filters by the
+   module Linear_filter. Run by linear_filter_test.i. *)
+
+let main _ =
+  ()
+
+let () = Db.Main.extend main
