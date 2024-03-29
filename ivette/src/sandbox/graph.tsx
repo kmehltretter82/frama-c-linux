dfd3015760cff991cce713c2032330fac75c6240
Guillaume FERREIRA <guillaume.ferreira@artal.fr>
2024-03-29 15:52:39 +0100

    Fix merge and type uuid

--- ivette/src/sandbox/graph.tsx
+++ ivette/src/sandbox/graph.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 /* ************************************************************************ */
 /*                                                                          */
 /*   This file is part of Frama-C.                                          */
