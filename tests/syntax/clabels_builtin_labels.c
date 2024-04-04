552a2e6cc77ac8ad236780331982345f53ade8fe
Allan Blanchard <allan.blanchard@cea.fr>
2024-04-04 11:00:25 +0200

    [tests] simpler test for builtin labels

--- tests/syntax/clabels_builtin_labels.c
+++ tests/syntax/clabels_builtin_labels.c
@@ -1,6 +1,6 @@
 int x;
 
-void named(void){
+void named_1(void){
   x = 4;
 Init: Pre: Old: Post: Here: LoopCurrent: LoopEntry: A:;
   x = 5;
@@ -10,27 +10,22 @@ Z: T: ;
   x = 7;
 }
 
-void post_over_old_1(void){ Old: Post: ;}
-void post_over_old_2(void){ Post: Old: ;}
+void named_2(void){
+  x = 4;
+A: Init: Pre: Old: Post: Here: LoopCurrent: LoopEntry: ;
+  x = 5;
+X: Y: ;
+  x = 6;
+Z: T: ;
+  x = 7;
+}
 
-void post_over_loop_1(void){ LoopEntry: Post: ;}
-void post_over_loop_2(void){ Post: LoopCurrent: ;}
-
-void post_over_other_1(void){ Here: Post: ;}
-void post_over_other_2(void){ Post: Pre: ;}
-void post_over_other_3(void){ Init: Post: Pre: ;}
-
-void old_over_loop_1(void){ LoopEntry: Old: ;}
-void old_over_loop_2(void){ Old: LoopCurrent: ;}
-
-void old_over_other_1(void){ Here: Old: ;}
-void old_over_other_2(void){ Old: Pre: ;}
-void old_over_other_3(void){ Init: Old: Pre: ;}
-
-void loop_over_other_1(void){ Here: LoopEntry: ;}
-void loop_over_other_2(void){ LoopCurrent: Pre: ;}
-void loop_over_other_3(void){ Init: LoopEntry: Pre: ;}
-
-void arbitray_other_1(void){ Here: Init: ;}
-void arbitray_other_2(void){ Init: Pre: ;}
-void arbitray_other_3(void){ Pre: Here: Init: ;}
+void named_3(void){
+  x = 4;
+Init: Pre: Old: Post: A: Here: LoopCurrent: LoopEntry: ;
+  x = 5;
+X: Y: ;
+  x = 6;
+Z: T: ;
+  x = 7;
+}
