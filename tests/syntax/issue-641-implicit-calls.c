6a9cf2f357614b1e2a34eaf4cd7d5a3e0d5d7a0b
Virgile Prevosto <virgile.prevosto@m4x.org>
2024-04-24 17:28:25 +0200

    [tests] add a few more tests for nested implicit calls

--- tests/syntax/issue-641-implicit-calls.c
+++ tests/syntax/issue-641-implicit-calls.c
@@ -3,6 +3,10 @@
     STDOPT: #"-cpp-extra-args=-DINCOMP1"
     STDOPT: #"-cpp-extra-args=-DINCOMP2"
     STDOPT: #"-cpp-extra-args=-DINCOMP3"
+    STDOPT: #"-cpp-extra-args=-DINCOMP4"
+    STDOPT: #"-cpp-extra-args=-DINCOMP5"
+    STDOPT: #"-cpp-extra-args=-DINCOMP6"
+    STDOPT: #"-cpp-extra-args=-DINCOMP7"
 */
 
 #ifdef INCOMP1
@@ -15,4 +19,20 @@
 
 #ifdef INCOMP3
     void foo(int x) { bar(bar); }
+#endif
+
+#ifdef INCOMP4
+    void foo(unsigned x) { bar((&bar)(0,12),x); }
+#endif
+
+#ifdef INCOMP5
+    void foo(unsigned x) { bar((&bar)(0),x); }
+#endif
+
+#ifdef INCOMP6
+    void foo(unsigned x) { bar(&bar); }
+#endif
+
+#ifdef INCOMP7
+    void foo(unsigned x) { bar(x,bar(0, 12)); }
 #endif
