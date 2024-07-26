6c0950202c665709f0e8609c2d04bd2a4b209341
Andre Maroneze <andre.maroneze@cea.fr>
2024-07-26 15:43:24 +0200

    [libc] add assigns for resolv.h

--- share/libc/resolv.h
+++ share/libc/resolv.h
@@ -29,41 +29,94 @@ __BEGIN_DECLS
 
 // Note: resolv.h is neither ISO-C nor POSIX
 
-struct __res_state;
+// Note: the __res_state struct is opaque in <resolv.h>, but to minimize issues
+// with initialization of incomplete structures (e.g. when using -lib-entry),
+// we fill it in with a generic field (the size is the one indicated in
+// <bits/types/res_state.h>). If necessary for a case study, consider importing
+// the full struct definition here.
+struct __res_state {
+  char __glibc_specific_fields[512];
+};
+
 typedef struct __res_state *res_state;
 
+volatile struct __res_state __fc_resolv; // internal state;
+
 // deprecated
+/*@
+  assigns \result \from indirect:__fc_resolv;
+  assigns __fc_resolv \from __fc_resolv;
+*/
 extern int res_init(void);
 
+/*@
+  assigns *statep \from *statep;
+  assigns \result \from indirect:*statep;
+*/
 extern int res_ninit(res_state statep);
 
+/*@
+  assigns \result, *statep, answer[0 .. anslen - 1] \from *statep,
+    indirect:dname[0..], indirect:class, indirect:type, indirect:anslen;
+*/
 extern int res_nquery(res_state statep,
                       const char *dname, int class, int type,
                       unsigned char *answer, int anslen);
 
+/*@
+  assigns \result, *statep, answer[0 .. anslen - 1] \from *statep,
+    indirect:dname[0..], indirect:class, indirect:type, indirect:anslen;
+*/
 extern int res_nsearch(res_state statep,
                        const char *dname, int class, int type,
                        unsigned char *answer, int anslen);
 
+/*@
+  assigns \result, *statep, answer[0 .. anslen - 1] \from *statep,
+    indirect:name[0..], indirect:domain[0..], indirect:class, indirect:type,
+    indirect:anslen;
+*/
 extern int res_nquerydomain(res_state statep,
                             const char *name, const char *domain,
                             int class, int type, unsigned char *answer,
                             int anslen);
 
+/*@
+  assigns \result, *statep, buf[0 .. buflen - 1] \from *statep,
+    indirect:op, indirect:dname[0..], indirect:class, indirect:datalen,
+    indirect:newrr[0..], indirect:buflen;
+*/
 extern int res_nmkquery(res_state statep,
                         int op, const char *dname, int class,
                         int type, const unsigned char *data, int datalen,
                         const unsigned char *newrr,
                         unsigned char *buf, int buflen);
 
+/*@
+  assigns \result, *statep, answer[0 .. anslen - 1] \from *statep,
+    msg[0 .. msglen - 1], indirect:anslen;
+*/
 extern int res_nsend(res_state statep,
                      const unsigned char *msg, int msglen,
                      unsigned char *answer, int anslen);
 
+/*@
+  //missing: assigns \from 'resolver'
+  assigns \result \from indirect:exp_dn[0..], indirect:dnptrs[0..][0..],
+                        indirect:comp_dn[0..];
+  assigns comp_dn[0 .. length-1] \from exp_dn[0..], dnptrs[0..][0..],
+                                       indirect:(*lastdnptr)[0..];
+*/
 extern int dn_comp(const char *exp_dn, unsigned char *comp_dn,
                    int length, unsigned char **dnptrs,
                    unsigned char **lastdnptr);
 
+/*@
+  //missing: assigns \from 'resolver'
+  assigns \result \from indirect:msg[0..], indirect:eomorig[0..],
+                        indirect:comp_dn[0..];
+  assigns exp_dn[0 .. length-1] \from msg[0..], eomorig[0..], comp_dn[0..];
+*/
 extern int dn_expand(const unsigned char *msg,
                      const unsigned char *eomorig,
                      const unsigned char *comp_dn, char *exp_dn,
