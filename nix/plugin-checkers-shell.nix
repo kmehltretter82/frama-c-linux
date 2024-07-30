3247db7f4de52f55b5109449e90cd42a934f18fd
Virgile Prevosto <virgile.prevosto@m4x.org>
2024-07-30 15:21:23 +0200

    [nix] clang_11 has been removed from recent nixpkgs

--- nix/plugin-checkers-shell.nix
+++ nix/plugin-checkers-shell.nix
@@ -1,6 +1,6 @@
 { lib
 , stdenv
-, clang_11
+, clang_12
 , frama-c
 , frama-c-hdrck
 , frama-c-lint
@@ -12,7 +12,7 @@
 stdenv.mkDerivation rec {
   name = "plugin-checkers-shell";
   buildInputs = [
-    clang_11
+    clang_12
     frama-c
     frama-c-hdrck
     frama-c-lint
