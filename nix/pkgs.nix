389b8ace8b02c7ebc8f3eb33ac46d9bd13523268
Allan Blanchard <allan.blanchard@cea.fr>
2024-07-31 08:14:38 +0200

    [nix] remove packages whose default is now OK

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -5,9 +5,6 @@ let
     alt-ergo = oself.callPackage ./alt-ergo.nix {};
     combinetura = oself.callPackage ./combinetura.nix {};
     mlmpfr = oself.callPackage ./mlmpfr.nix {};
-    odoc = oself.callPackage ./odoc.nix {};
-    odoc-parser = oself.callPackage ./odoc-parser.nix {};
-    ppxlib = oself.callPackage ./ppxlib.nix {};
     why3 = oself.callPackage ./why3.nix {};
 
     # Helpers
