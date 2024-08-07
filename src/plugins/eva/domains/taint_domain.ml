35abfdfa53ec5c810b9602ca65ab820558b13ddb
David Bühler <david.buhler@cea.fr>
2024-08-07 15:09:54 +0200

    [Eva] Taint domain: fixes the registration of the \tainted predicate.

--- src/plugins/eva/domains/taint_domain.ml
+++ src/plugins/eva/domains/taint_domain.ml
@@ -404,8 +404,7 @@ let () =
       bl_profile = ["p", a_type];
     }
   in
-  Logic_env.add_builtin_logic_function_gen
-    Logic_utils.is_same_builtin_profile builtin_logic_info
+  Logic_env.Builtins.extend (fun () -> Logic_builtin.add builtin_logic_info)
 
 (* Registers ACSL extension "taint" (statement annotation)
    and "taints" (behavior extension). *)
