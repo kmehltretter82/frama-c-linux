088fec10ac7fcda3847d3002c6fdcea61544f09c
Andre Maroneze <andre.maroneze@cea.fr>
2024-09-04 16:57:00 +0200

    [tests] remove excessively large libc oracles to avoid conflicts

--- src/plugins/markdown-report/tests/sarif/libc.c
+++ src/plugins/markdown-report/tests/sarif/libc.c
@@ -1,11 +1,11 @@
 /* run.config
  CMD: @frama-c@ -eva -eva-no-results -mdr-gen sarif -mdr-sarif-deterministic
- BIN: with-libc.sarif.unfiltered
-   OPT: -mdr-out ./with-libc.sarif.unfiltered
-   EXECNOW: LOG with-libc.sarif sed -e "s:@PTEST_SESSION@:PTEST_SESSION:" %{dep:with-libc.sarif.unfiltered} > with-libc.sarif 2> @NULL
+ BIN: with-libc.sarif.unchecked
+   OPT: -mdr-out ./with-libc.sarif.unchecked
+   COMMENT: the log with libc is too large, so we do not use it as textual oracle
  BIN: without-libc.sarif.unfiltered
    OPT: -mdr-no-print-libc -mdr-out ./without-libc.sarif.unfiltered
-   EXECNOW: LOG without-libc.sarif sed -e "s:@PTEST_SESSION@:PTEST_SESSION:" %{dep:without-libc.sarif.unfiltered} > without-libc.sarif 2> @NULL
+   EXECNOW: LOG without-libc.sarif sed -e "s:@PTEST_SESSION@:PTEST_SESSION:" %{dep:without-libc.sarif.unfiltered} > without-libc.sarif 2> @DEV_NULL@
 */
 #include <string.h>
 int main() {
