992a50bf446297132905704475da7d0dbb8240a1
David Bühler <david.buhler@cea.fr>
2024-09-04 10:14:40 +0200

    [Eva] Fixes alarms emitted on pointer conversions to intptr_t or uintptr_t.
    Such conversions are always valid for valid object pointers.

--- src/plugins/eva/engine/evaluation.ml
+++ src/plugins/eva/engine/evaluation.ml
@@ -169,6 +169,11 @@ let rec signed_counterpart typ =
   | TPtr _ -> signed_counterpart ((Machine.uintptr_type ()))
   | _ -> assert false
 
+let rec is_intptr_type = function
+  | TNamed ({torig_name = name} as t, _) ->
+    name = "intptr_t" || name = "uintptr_t" || is_intptr_type t.ttype
+  | _ -> false
+
 let return t = `Value t, Alarmset.none
 
 (* Intersects [alarms] with the only possible alarms from the dereference of
@@ -698,6 +703,20 @@ module Make
     then truncate_upper_bound overflow_kind expr dst value
     else return value
 
+  (* Special case of a pointer converted to intptr_t or uintptr_t:
+     the pointer must be a valid object pointer. *)
+  let cast_pointer_to_intptr context expr ~dst value =
+    let open Evaluated.Operators in
+    let+ value =
+      if Kernel.PointerDowncast.get () then
+        let truth = Value.assume_pointer value in
+        let alarm () = Alarms.Invalid_pointer (Eva_ast.to_cil_exp expr) in
+        reduce_by_truth ~alarm (expr, value) truth
+      else
+        return value
+    in
+    Value.rewrap_integer context dst value
+
   (* Relaxed semantics for downcasts into signed types:
      first converts the value to the signed counterpart of the source type, and
      then downcasts it into the signed destination type. Emits only alarms for
@@ -714,7 +733,13 @@ module Make
     in
     cast_integer Alarms.Signed_downcast expr ~src ~dst value
 
-  let cast_int_to_int context expr ~ptr ~src ~dst value =
+  (* The type of alarms emitted for a conversion depends on whether the source
+     type is an integer or a pointer. As a special case, conversions from a
+     pointer to "intptr_t" or "uintptr_t" only require the source pointer
+     to be a valid pointer to an object. *)
+  type conversion_alarm = IntDowncast | PtrDowncast | Pointer
+
+  let cast_int_to_int context expr ~alarm ~src ~dst value =
     (* Regain some precision in case a transfer function was imprecise.
        This should probably be done in the transfer function, though. *)
     let value =
@@ -722,11 +747,14 @@ module Make
       then Value.rewrap_integer context src value
       else value
     in
-    if Eval_typ.range_inclusion src dst
-    then return value (* Upcast, nothing to check. *)
+    if Eval_typ.range_inclusion src dst then
+      return value (* Upcast, nothing to check. *)
+    else if alarm = Pointer then
+      (* Conversion of a pointer to intptr_t or uintptr_t. *)
+      cast_pointer_to_intptr context expr ~dst value
     else
       let overflow_kind, warn =
-        if ptr
+        if alarm = PtrDowncast
         then Alarms.Pointer_downcast, Kernel.PointerDowncast.get
         else if dst.i_signed
         then Alarms.Signed_downcast, Kernel.SignedDowncast.get
@@ -776,18 +804,19 @@ module Make
     let bound_kind = Alarms.Lower_bound in
     truncate_float_bound fkind min_bound bound_kind expr value
 
-  let forward_cast context ~dst expr value =
+  let forward_cast context ~dst_typ expr value =
     let open Evaluated.Operators in
-    let src = expr.typ in
-    match Eval_typ.(classify_as_scalar src, classify_as_scalar dst) with
+    let src_typ = expr.typ in
+    match Eval_typ.(classify_as_scalar src_typ, classify_as_scalar dst_typ) with
     | None, _ | _, None -> return value (* Unclear whether this happens. *)
     | Some src_type, Some dst_type ->
       let& value =
         match src_type, dst_type with
         | TSPtr src, TSInt dst ->
-          cast_int_to_int context ~ptr:true ~src ~dst expr value
+          let alarm = if is_intptr_type dst_typ then Pointer else PtrDowncast in
+          cast_int_to_int context ~alarm ~src ~dst expr value
         | TSInt src, (TSInt dst | TSPtr dst) ->
-          cast_int_to_int context ~ptr:false ~src ~dst expr value
+          cast_int_to_int context ~alarm:IntDowncast ~src ~dst expr value
         | TSFloat src, (TSInt dst | TSPtr dst)  ->
           let reduce = true and assume_finite = true in
           let* value = restrict_float ~reduce ~assume_finite expr src value in
@@ -955,10 +984,10 @@ module Make
       let v = handle_overflow ~may_overflow env.context expr typ v in
       compute_reduction v (volatile1 || volatile2)
 
-    | CastE (dst, e) ->
+    | CastE (dst_typ, e) ->
       let* value, volatile = root_forward_eval env e in
-      let v = forward_cast env.context ~dst e value in
-      let v = match Cil.unrollType dst with
+      let v = forward_cast env.context ~dst_typ e value in
+      let v = match Cil.unrollType dst_typ with
         | TFloat (fkind, _) -> let* v in remove_special_float expr fkind v
         | TPtr _ -> let* v in assume_pointer env.context expr v
         | _ -> v
