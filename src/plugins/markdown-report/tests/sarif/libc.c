0487b54967968fb9407a06e215f0368b44f5fab4
Virgile Prevosto <virgile.prevosto@cea.fr>
2024-09-05 12:06:46 +0200

    [mdr] test libc.c has its sarif document checked for schema conformance

--- src/plugins/markdown-report/tests/sarif/libc.c
+++ src/plugins/markdown-report/tests/sarif/libc.c
@@ -6,6 +6,9 @@
  BIN: without-libc.sarif.unfiltered
    OPT: -mdr-no-print-libc -mdr-out ./without-libc.sarif.unfiltered
    EXECNOW: LOG without-libc.sarif sed -e "s:@PTEST_SESSION@:PTEST_SESSION:" %{dep:without-libc.sarif.unfiltered} > without-libc.sarif 2> @DEV_NULL@
+
+ ENABLED_IF: %{bin-available:check-jsonschema}
+ EXECNOW: LOG with-libc.sarif.checked check-jsonschema --schemafile $(jq '."$schema"' ./with-libc.sarif.unchecked -r) %{dep:with-libc.sarif.unchecked} > ./with-libc.sarif.checked
 */
 #include <string.h>
 int main() {
