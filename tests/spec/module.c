f3a03c43afe46e4f1a463a065a1ec0a664740e92
Virgile Prevosto <virgile.prevosto@cea.fr>
2024-09-18 17:54:44 +0200

    [tests] add some negative tests regarding scope resolution

--- tests/spec/module.c
+++ tests/spec/module.c
@@ -1,5 +1,6 @@
 /* run.config
    STDOPT:
+   STDOPT: +"-cpp-extra-args='-DILL_TYPED'"
  */
 
 /*@
@@ -19,3 +20,34 @@
   lemma AbsOp: \forall Foo::t x, integer n;
     B::opN(x,\abs(n)) == A::opN(x,\abs(n));
  */
+
+#ifdef ILL_TYPED
+
+/*@
+
+  import Foo \as F;
+  logic t x = F::e; // ill-formed: t should be F::t
+*/
+
+/*@
+  import Foo \as F;
+  import foo \as f;
+  logic F::t x = f::bar::inv(F::e); // OK
+  logic F::t y = bar::inv(F::e); // KO
+*/
+
+/*@
+  module A {
+     logic integer a = 0;
+     module B {
+       logic integer b = a + 1;
+     }
+  }
+
+import A::B \as b;
+
+logic integer z = b::a; // KO
+
+*/
+
+#endif
