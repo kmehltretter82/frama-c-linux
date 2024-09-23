f34ed476bf593ba541137caeec7049dffbe03359
Loïc Correnson <loic.correnson@cea.fr>
2024-09-23 16:48:20 +0200

    [wp] publish Why3 importer

--- src/plugins/wp/wp_parameters.mli
+++ src/plugins/wp/wp_parameters.mli
@@ -116,7 +116,6 @@ module Cache: Parameter_sig.String
 module CacheEnv: Parameter_sig.Bool
 module CacheDir: Parameter_sig.String
 module CachePrint: Parameter_sig.Bool
-module Import: Parameter_sig.String_list
 module Library: Parameter_sig.Filepath_list
 module Drivers: Parameter_sig.Filepath_list
 module Timeout: Parameter_sig.Int
