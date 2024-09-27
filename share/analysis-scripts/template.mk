d8cd578d11c8fe1d59d6d4991ac3f3c82d385dea
Andre Maroneze <andre.maroneze@cea.fr>
2024-09-27 14:01:40 +0200

    [analysis-scripts] change workflow for recursion, defaulting to stricter mode

--- share/analysis-scripts/template.mk
+++ share/analysis-scripts/template.mk
@@ -55,6 +55,10 @@ FCFLAGS     += \
 EVAFLAGS    += \
   -eva-warn-key builtins:missing-spec=abort \
   -eva-warn-key libc:unsupported-spec=abort \
+  -eva-warn-key recursion=abort \
+
+# Note: if the code has recursive calls, manually review them, add assigns as
+# needed, and remove the line '-eva-warn-key recursion=abort' above
 
 ## WP-specific flags
 WPFLAGS    += \
