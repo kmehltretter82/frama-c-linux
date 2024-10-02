83671e49cc43387c40820581c05fca5a9d1b1bc7
rlazarini <remi.lazarini@cea.fr>
2024-10-02 14:50:13 +0200

    [Ivette] addmarkdown component

---
+++ ivette/src/dome/renderer/text/markdown.tsx
@@ -0,0 +1,151 @@
+/* ************************************************************************ */
+/*                                                                          */
+/*   This file is part of Frama-C.                                          */
+/*                                                                          */
+/*   Copyright (C) 2007-2024                                                */
+/*     CEA (Commissariat à l'énergie atomique et aux énergies               */
+/*          alternatives)                                                   */
+/*                                                                          */
+/*   you can redistribute it and/or modify it under the terms of the GNU    */
+/*   Lesser General Public License as published by the Free Software        */
+/*   Foundation, version 2.1.                                               */
+/*                                                                          */
+/*   It is distributed in the hope that it will be useful,                  */
+/*   but WITHOUT ANY WARRANTY; without even the implied warranty of         */
+/*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          */
+/*   GNU Lesser General Public License for more details.                    */
+/*                                                                          */
+/*   See the GNU Lesser General Public License version 2.1                  */
+/*   for more details (enclosed in the file licenses/LGPLv2.1).             */
+/*                                                                          */
+/* ************************************************************************ */
+
+import React from 'react';
+import ReactMarkdown, { Components, Options } from 'react-markdown';
+
+import { classes } from 'dome/misc/utils';
+import { Icon } from 'dome/controls/icons';
+
+export interface IReplacement {
+  regex: RegExp,
+  transform: (key: number, match?: RegExpExecArray) => JSX.Element | null
+}
+
+export const iconTagReplacement: IReplacement = {
+  regex: /\[icon-([^\]]+)\]/g,
+  transform: (key: number, match?: RegExpExecArray) => {
+    return match ? <Icon key={key} id={match[1]}/> : null;
+  }
+};
+
+// --------------------------------------------------------------------------
+// --- Replacement function
+// --------------------------------------------------------------------------
+/**
+ * Replace all tag in the text.
+ * This function doesn't replace any tags added by a previous replacement.
+ */
+function replaceTagsByElement(
+  text: string,
+  replacement?: IReplacement[]
+): (string | JSX.Element | null)[] {
+  if(!replacement || replacement.length < 1) return [text];
+
+  const { regex, transform } = replacement[0];
+  replacement.shift();
+
+  const newContent = [];
+  let match;
+  let lastIndex = 0;
+  while ((match = regex.exec(text)) !== null) {
+    if (match.index > lastIndex) {
+      const before = replaceTagsByElement(
+        text.slice(lastIndex, match.index), replacement
+      );
+      before.forEach((elt) => newContent.push(elt));
+    }
+    newContent.push(transform(Math.random(), match));
+    lastIndex = regex.lastIndex;
+  }
+  if (lastIndex < text.length) {
+    const after = replaceTagsByElement(text.slice(lastIndex), replacement);
+    after.forEach((elt) => newContent.push(elt));
+  }
+  return newContent;
+}
+
+function replaceTags(
+  children: React.ReactNode,
+  replacement: IReplacement[]
+): React.ReactNode {
+  const childrenTab = React.Children.toArray(children);
+
+  const newContent = childrenTab.map((child) => {
+    if (typeof child === 'string') {
+      return replaceTagsByElement(child, replacement.slice());
+    }
+    return child;
+  });
+
+  return newContent;
+}
+
+// --------------------------------------------------------------------------
+// --- Markdown component
+// --------------------------------------------------------------------------
+type tagHtmlList = [ k: keyof Components, v: keyof Components ][]
+
+interface MarkdownProps {
+  /** classes for Markdown component */
+  className?: string;
+  /** html tag of the markdown to be processed and possible replacement */
+  htmlTag?: tagHtmlList;
+  /** Tab of tag replacement */
+  replacement?: IReplacement[];
+  /** Children */
+  children?: string | null;
+}
+
+export function Markdown(
+  props: MarkdownProps
+): JSX.Element {
+  const { className, replacement, htmlTag, children } = props;
+  const markdownClasses = classes(
+    "dome-xMarkdown", "dome-pages",
+    className,
+  );
+
+  const transformChildren = (c: React.ReactNode): React.ReactNode => {
+    return !replacement ? c : replaceTags(c, replacement);
+  };
+
+  const getComponentsOption = (): Components | null => {
+    if(!htmlTag) return null;
+
+    const getDynamicElement = (
+      tagName: keyof JSX.IntrinsicElements,
+      children: React.ReactNode
+    ): JSX.Element => {
+      const Tag = tagName;
+      return <Tag>{children}</Tag>;
+    };
+
+    const component = [];
+    for (const [key, val] of htmlTag) {
+      component.push([key, ({ children }: {
+        children: React.ReactNode;
+      }) => getDynamicElement(val, transformChildren(children))]);
+    }
+
+    return Object.fromEntries(component);
+  };
+
+  const options: Options = {
+    className: markdownClasses,
+    components: getComponentsOption()
+  };
+
+  return <ReactMarkdown {...options}>{ children }</ReactMarkdown>;
+}
+
+/* -------------------------------------------------------------------------- */
