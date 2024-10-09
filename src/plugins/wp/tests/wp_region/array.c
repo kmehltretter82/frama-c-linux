9453b222ac2e250fe1fee1f39a876cd357905b37
Loïc Correnson <loic.correnson@cea.fr>
2024-10-09 18:35:00 +0200

    [wp/region] optimized array test

--- src/plugins/wp/tests/wp_region/array.c
+++ src/plugins/wp/tests/wp_region/array.c
@@ -18,9 +18,6 @@ void add_first4(struct S * a , struct S * b )
 {
     a->content[0] += b->content[0];
     a->content[1] += b->content[1];
-    note1:
     a->content[2] += b->content[2];
-    //@ assert a!=b ==> a->content[2] == \at(a->content[2],note1) + b->content[2];
-    //@ assert a==b ==> a->content[2] == 2*\at(a->content[2],note1);
     a->content[3] += b->content[3];
 }
