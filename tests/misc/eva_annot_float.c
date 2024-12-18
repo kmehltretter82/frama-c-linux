e5d6759730de7e56c79b9bc24f4cf03bc0e08a3b
Valentin Perrelle <valentin.perrelle@cea.fr>
2024-12-18 16:34:19 +0100

    [Eva] improve Export test coverage

--- tests/misc/eva_annot_float.c
+++ tests/misc/eva_annot_float.c
@@ -1,10 +1,10 @@
 /* run.config
    PLUGIN: @EVA_PLUGINS@
-   OPT: -eva -eva-precision 2 -eva-annot main -print
+   OPT: -eva -eva-precision 2 -warn-special-float none -eva-annot main1,main2,main3 -print
 */
 
 /* -------------------------------------------------------------------------- */
-/* --- Testing EVA Annotations                                            --- */
+/* --- Testing EVA exported annotations                                   --- */
 /* -------------------------------------------------------------------------- */
 
 //@ ghost int world;
@@ -17,7 +17,7 @@ double a[20];
 */
 double value(void);
 
-double main(void) {
+double main1(void) {
   double s = 0;
   for (int i = 0; i < 20; i++) {
     double v = value();
@@ -25,6 +25,38 @@ double main(void) {
     s += v;
   }
   return s;
+}
+
+/*@
+  ensures (\is_finite(\result) ∧ 0.0 <= \result <= 100.0) ∨ \is_NaN(\result);
+  assigns \result,world \from world;
+*/
+double value_or_nan(void);
+
+void main2(void) {
+  for (int i = 0; i < 20; i++) {
+    double v = value_or_nan();
+    a[i] = v;
+  }
+}
+
+/*@
+  ensures \is_NaN(\result);
+  assigns \result,world \from world;
+*/
+double nan(void);
+
+void main3(void) {
+  for (int i = 0; i < 20; i++) {
+    double v = nan();
+    a[i] = v;
+  }
+}
+
+void main(void) {
+  main1();
+  main2();
+  main3();
 }
 
 /* -------------------------------------------------------------------------- */
