7619bb8e1349601c3965a6c5ce59dbe41658a797
Basile Desloges <basile.desloges@cea.fr>
2024-12-20 13:12:34 +0100

    [eva] mthread: Create and log dot file in tests

--- src/plugins/eva/tests/mthread/ccfg.c
+++ src/plugins/eva/tests/mthread/ccfg.c
@@ -1,7 +1,12 @@
+/* run.config
+   LOG: @LOG_MT_DOT_FILES_FILENAME@
+   STDOPT: +"-mt-non-shared-accesses @LOG_MT_DOT_FILES_OPTS@"
+*/
 /* This file is used to give an example of a cfg with many features */
 #include "mthread_pthread.h"
 #define NULL ((void*)0)
 
+volatile int nondet;
 int random(void);
 
 pthread_t  jobs[4];
@@ -26,15 +31,66 @@ void g2(int* v) {
     *(v+1) = 2;
 }
 
-void main() {
-  int i, arr[2];
-  void (*pf)(int*, int) = &g1;
+int a, b, c, d, e, f;
+pthread_mutex_t lock;
 
-  g1(NULL, 0);
-  g2(arr);
-  for (i=1;i<5;i++)
-    if (!x) {
-      (*pf)(&global1, i);
-      g2(global2);
-    }
+/*@ assigns a \from b; */
+void g3();
+
+/*@ assigns b \from a; */
+void g4();
+
+/*@ assigns *to \from *from; */
+void g5(int * to, int * from);
+
+int g6(int * from) {
+  return *from;
+}
+
+void *f1(void * _) {
+  pthread_mutex_lock(&lock);
+  g3();
+  pthread_mutex_unlock(&lock);
+  g5(&c, &d);
+  e = g6(&f);
+  return NULL;
+}
+
+void *f2(void * _) {
+  pthread_mutex_lock(&lock);
+  g4();
+  pthread_mutex_unlock(&lock);
+  g5(&d, &c);
+  f = g6(&e);
+  return NULL;
+}
+
+void main() {
+  if (nondet) {
+    int i, arr[2];
+    void (*pf)(int*, int) = &g1;
+
+    g1(NULL, 0);
+    g2(arr);
+    for (i=1;i<5;i++)
+      if (!x) {
+        (*pf)(&global1, i);
+        g2(global2);
+      }
+  } else {
+    pthread_t job_f1, job_f2;
+    a = 1; b = 1; c = 1; d = 1; e = 1; f = 1;
+    pthread_mutex_init(&lock, NULL);
+    pthread_create(&job_f1, NULL, f1, NULL);
+    pthread_create(&job_f2, NULL, f2, NULL);
+    int sum = 0;
+    pthread_mutex_lock(&lock);
+    sum += a;
+    sum += b;
+    pthread_mutex_unlock(&lock);
+    sum += c;
+    sum += d;
+    sum += e;
+    sum += f;
+  }
 }
