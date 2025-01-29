d30060e48145ff95a0420ed5fae6de285ab6577e
Andre Maroneze <andre.maroneze@cea.fr>
2025-01-29 14:19:17 +0100

    Cramify machdep-related tests; add test with -save/-load in different dirs

--- tests/misc/machdep.t/custom_machdep.c
+++ tests/misc/machdep.t/custom_machdep.c
@@ -1,10 +1,3 @@
-/* run.config*
- MACRO: machdep %{dep:@PTEST_DIR@/@PTEST_NAME@.yaml}
- OPT: -machdep @machdep@ -print
- COMMENT: we can't only use -D, as the __fc_machdep.h define takes precedence
- COMMENT: with -U, our cmdline definition is used in the code
- OPT: -machdep @machdep@ -cpp-extra-args="-UCUSTOM_MACHDEP -DCUSTOM_MACHDEP=42" -print
-*/
 // most of the following includes are not directly used, but they test if
 // the custom machdep has defined the necessary constants
 #include <ctype.h>
