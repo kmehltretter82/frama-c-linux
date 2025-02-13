c950f37b3a56e8133f1a163ac0243dc017204405
Maxime Jacquemin <maxime.jacquemin@cea.fr>
2025-02-13 12:46:04 +0100

    [Eva] Generic way to build an IEEE754 interval abstraction
    Generic in the sens that the base field and the used effect are
    parameters

---
+++ src/plugins/eva/IEEE754/field_interval.ml
@@ -0,0 +1,111 @@
+(**************************************************************************)
+(*                                                                        *)
+(*  This file is part of Frama-C.                                         *)
+(*                                                                        *)
+(*  Copyright (C) 2007-2025                                               *)
+(*    CEA (Commissariat à l'énergie atomique et aux énergies              *)
+(*         alternatives)                                                  *)
+(*                                                                        *)
+(*  you can redistribute it and/or modify it under the terms of the GNU   *)
+(*  Lesser General Public License as published by the Free Software       *)
+(*  Foundation, version 2.1.                                              *)
+(*                                                                        *)
+(*  It is distributed in the hope that it will be useful,                 *)
+(*  but WITHOUT ANY WARRANTY; without even the implied warranty of        *)
+(*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *)
+(*  GNU Lesser General Public License for more details.                   *)
+(*                                                                        *)
+(*  See the GNU Lesser General Public License version 2.1                 *)
+(*  for more details (enclosed in the file licenses/LGPLv2.1).            *)
+(*                                                                        *)
+(**************************************************************************)
+
+module type Name = sig val name : string end
+
+type 'k bounds = 'k Field.bounds = { lower : 'k ; upper : 'k }
+
+module Make (K : Field.S) (Effect : IEEE754.Effect) (Name : Name) = struct
+
+  module Scalar = K
+  type scalar = K.t
+
+  module Effect = Effect
+  type 'a effect = 'a Effect.t
+
+  type subset = scalar bounds
+
+  let between l u = { lower = l ; upper = u }
+  let singleton k = { lower = k ; upper = k }
+  let zero = singleton K.zero
+  let one  = singleton K.one
+  let top  = { lower = K.neg_inf ; upper = K.pos_inf }
+
+  let pretty fmt { lower ; upper } =
+    Format.fprintf fmt "@[[%a .. %a]@]" K.pretty lower K.pretty upper
+
+  let compare l r =
+    let c = K.compare l.lower r.lower in
+    if c = 0 then K.compare l.upper r.upper else c
+
+  let structural_descr =
+    let scalar = K.packed_descr in
+    Structural_descr.t_record [| scalar ; scalar |]
+
+  include Datatype.Make (struct
+    type t = subset
+    let name = Name.name
+    let reprs = [ zero ; one ; top ]
+    let structural_descr = structural_descr
+    let mem_project = Datatype.never_any_project
+    let rehash = Datatype.identity
+    let copy b = { lower = K.copy b.lower ; upper = K.copy b.upper }
+    let hash b = Hashtbl.hash(K.hash b.lower, K.hash b.upper)
+    let pretty = pretty
+    let compare = compare
+    let equal = Datatype.from_compare
+  end)
+
+  let is_included l r = K.(r.lower <= l.lower && l.upper <= r.upper)
+  let join l r = between (K.min l.lower r.lower) (K.max l.upper r.upper)
+  let narrow l r =
+    if K.(l.upper < r.lower || r.upper < l.lower) then `Bottom
+    else `Value (between (K.max l.lower r.lower) (K.min l.upper r.upper))
+
+  let lower  b = b.lower
+  let upper  b = b.upper
+  let bounds b = b
+
+  let neg b =
+    let open Effect.Operators in
+    let+ b in between (K.neg b.upper) (K.neg b.lower)
+  
+  let sqrt b =
+    let open Effect.Operators in
+    let+ { lower ; upper } = b in
+    join (K.sqrt lower) (K.sqrt upper)
+
+  let ( + ) l r =
+    let open Effect.Operators in
+    let+ l and+ r in between K.(l.lower + r.lower) K.(l.upper + r.upper)
+
+  let ( - ) l r =
+    let open Effect.Operators in
+    let+ l and+ r in between K.(l.lower - r.upper) K.(l.upper - r.lower)
+
+  let ( * ) l r =
+    let open Effect.Operators in
+    let* { lower = l  ; upper = u  } = l in
+    let+ { lower = l' ; upper = u' } = r in
+    let a, b, c, d = K.(l * l', l * u', u * l', u * u') in
+    between K.(min (min a b) (min c d)) K.(max (max a b) (max c d))
+
+  let ( / ) l r =
+    let open Effect.Operators in
+    let* { lower = l  ; upper = u  } = l in
+    let+ { lower = l' ; upper = u' } = r in
+    if K.(zero < l' || u' < zero) then
+      let a, b, c, d = K.(l / l', l / u', u / l', u / u') in
+      between K.(min (min a b) (min c d)) K.(max (max a b) (max c d))
+    else top
+
+end
---
+++ src/plugins/eva/IEEE754/field_interval.mli
@@ -0,0 +1,29 @@
+(**************************************************************************)
+(*                                                                        *)
+(*  This file is part of Frama-C.                                         *)
+(*                                                                        *)
+(*  Copyright (C) 2007-2025                                               *)
+(*    CEA (Commissariat à l'énergie atomique et aux énergies              *)
+(*         alternatives)                                                  *)
+(*                                                                        *)
+(*  you can redistribute it and/or modify it under the terms of the GNU   *)
+(*  Lesser General Public License as published by the Free Software       *)
+(*  Foundation, version 2.1.                                              *)
+(*                                                                        *)
+(*  It is distributed in the hope that it will be useful,                 *)
+(*  but WITHOUT ANY WARRANTY; without even the implied warranty of        *)
+(*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *)
+(*  GNU Lesser General Public License for more details.                   *)
+(*                                                                        *)
+(*  See the GNU Lesser General Public License version 2.1                 *)
+(*  for more details (enclosed in the file licenses/LGPLv2.1).            *)
+(*                                                                        *)
+(**************************************************************************)
+
+module type Name = sig val name : string end
+
+module Make (K : Field.S) (Effect : IEEE754.Effect) (_ : Name) :
+  IEEE754.Abstraction
+    with module Scalar = K
+     and module Effect = Effect
+     and type t = K.t Field.bounds
