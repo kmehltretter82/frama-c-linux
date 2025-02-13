a7f21dba01e12be5f58f573b21284321af82f87e
Maxime Jacquemin <maxime.jacquemin@cea.fr>
2025-02-13 12:46:36 +0100

    [Eva] Numerors: Domain instanciation

---
+++ src/plugins/eva/domains/numerors/domain.mli
@@ -0,0 +1,29 @@
+(**************************************************************************)
+(*                                                                        *)
+(*  This file is part of Frama-C.                                         *)
+(*                                                                        *)
+(*  Copyright (C) 2007-2025                                               *)
+(*    CEA (Commissariat à l'énergie atomique et aux énergies              *)
+(*         alternatives)                                                  *)
+(*                                                                        *)
+(*  you can redistribute it and/or modify it under the terms of the GNU   *)
+(*  Lesser General Public License as published by the Free Software       *)
+(*  Foundation, version 2.1.                                              *)
+(*                                                                        *)
+(*  It is distributed in the hope that it will be useful,                 *)
+(*  but WITHOUT ANY WARRANTY; without even the implied warranty of        *)
+(*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *)
+(*  GNU Lesser General Public License for more details.                   *)
+(*                                                                        *)
+(*  See the GNU Lesser General Public License version 2.1                 *)
+(*  for more details (enclosed in the file licenses/LGPLv2.1).            *)
+(*                                                                        *)
+(**************************************************************************)
+
+(** Numerors' abstract domain, which computes a sound overapproximation of the
+    floating-point semantic through the whole program. The domain's memory
+    model is for now based on the <Simple_memory> functor provided by Eva.
+    A reduced product with the Cvalue domain is performed at each step.
+    For more details, one can look at M. Jacquemin's thesis. *)
+
+val registered : Abstractions.Domain.registered
