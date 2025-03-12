4ca949bd9f1abf057e26b016b71d5c788e6020a5
Basile Desloges <basile.desloges@cea.fr>
2025-03-12 18:18:51 +0100

    [ci] Update Eva and Mthread nix files
    The `mthread-tests` CI target is removed and mthread tests are now run
    at the same time as Eva's other tests.
    
    Co-authored-by: David Bühler <david.buhler@cea.fr>

--- nix/mthread-tests.nix
+++
@@ -1,9 +0,0 @@
-{ mk_tests } :
-
-mk_tests {
-  tests-name = "mthread-tests";
-  tests-command = ''
-    dune exec -- frama-c-ptests -never-disabled src/plugins/eva/tests
-    dune build -j1 @src/plugins/eva/tests/mthread/ptests
-    '';
-}
