abd1ef2b8cbd4b3333ff514c7a9cf51c345fba45
Loïc Correnson <loic.correnson@cea.fr>
2025-03-13 16:18:27 +0100

    [region] fixed merge with overlapping ranges

--- ivette/src/frama-c/plugins/wp/seq.tsx
+++ ivette/src/frama-c/plugins/wp/seq.tsx
@@ -175,7 +175,8 @@ export function GoalView(props: GoalViewProps): JSX.Element {
     () => sequent.target(proxy, part, term),
     [sequent, proxy, part, term]
   );
-  const focus : Decorations = selection && { className: 'wp-selected', ...selection } ;
+  const focus : Decorations =
+    selection && { className: 'wp-selected', ...selection };
   const [hover, setHover] = React.useState<Decorations>(null);
   const onHover = React.useCallback((pos: Position | null) => {
     setHover(sequent.hover(pos));
