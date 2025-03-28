9269f23329d6e94ce163df2f73adb2b60913e067
Jan Rochel <jan.rochel@cea.fr>
2025-03-28 19:20:34 +0100

    [e-acsl] Varname: handle all binary and unary operators

--- src/plugins/e-acsl/src/libraries/varname.ml
+++ src/plugins/e-acsl/src/libraries/varname.ml
@@ -47,11 +47,39 @@ let get ~scope s =
 
 let clear_locals () = H.clear tbl
 
+let of_binop = function
+  | PlusA -> "plus"
+  | PlusPI -> "plus"
+  | MinusA -> "minus"
+  | MinusPI -> "minus"
+  | MinusPP -> "minus"
+  | Mult -> "mult"
+  | Div -> "div"
+  | Mod -> "mod"
+  | Shiftlt -> "shiftl"
+  | Shiftrt -> "shiftr"
+  | Lt -> "lt"
+  | Gt -> "gt"
+  | Le -> "le"
+  | Ge -> "ge"
+  | Eq -> "eq"
+  | Ne -> "ne"
+  | BAnd -> "and"
+  | BXor -> "xor"
+  | BOr -> "or"
+  | LAnd -> "and"
+  | LOr -> "or"
+
+let of_unop = function
+  | Neg -> "neg"
+  | BNot -> "not"
+  | LNot -> "not"
+
 let rec of_exp ?default exp = match exp.enode with
   | Lval (Var {vorig_name}, NoOffset) -> vorig_name
   | Const (CInt64 (i, _, _)) -> "const_" ^ Integer.to_string i
-  | BinOp (MinusA, x, y, _) -> "minus_" ^ of_exp x ^ "_" ^ of_exp y
-  | BinOp (PlusA, x, y, _) -> "plus_" ^ of_exp x ^ "_" ^ of_exp y
+  | BinOp (op, x, y, _) -> of_binop op ^ of_exp x ^ "_" ^ of_exp y
+  | UnOp (op, x, _) -> of_unop op ^ of_exp x
   | e ->
     match default with
     | None ->
