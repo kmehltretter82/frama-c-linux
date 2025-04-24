65e51e43e0418861faaafd299d15d14a39eafc69
David Bühler <david.buhler@cea.fr>
2025-04-24 18:31:04 +0200

    [Eva] In mthread tests, sets Eva verbosity to 1 but disables more message keys.

--- src/plugins/eva/tests/mthread/interrupt.c
+++ src/plugins/eva/tests/mthread/interrupt.c
@@ -1,7 +1,7 @@
 /* run.config*
-  STDOPT: +"-eva-verbose=1 -main main_interrupt_one_shot -mt-interrupt-handlers interrupt_oneshot"
-  STDOPT: +"-eva-verbose=1 -main main_interrupt_incr -mt-interrupt-handlers interrupt_incr"
-  STDOPT: +"-eva-verbose=1 -main main_interrupt_cross_incr -mt-interrupt-handlers interrupt_cross_incr1,interrupt_cross_incr2"
+  STDOPT: +"-main main_interrupt_one_shot -mt-interrupt-handlers interrupt_oneshot"
+  STDOPT: +"-main main_interrupt_incr -mt-interrupt-handlers interrupt_incr"
+  STDOPT: +"-main main_interrupt_cross_incr -mt-interrupt-handlers interrupt_cross_incr1,interrupt_cross_incr2"
 */
 int interrupt_received;
 
