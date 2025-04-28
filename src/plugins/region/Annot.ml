a5ae715c81a4c8d057b71f3ab5f07a5c50809e00
Cécile RUET-CROS <cecile.ruet-cros@cea.fr>
2025-04-28 16:59:50 +0200

    [region] + Annot.add_behavior

--- src/plugins/region/Annot.ml
+++ src/plugins/region/Annot.ml
@@ -40,44 +40,52 @@ let iadd_from env (it,from) = match from with
 
 let add_ip env ip = add_predicate env ip.ip_content.tp_statement
 
-let add_requires map kf ki b ip =
+let add_requires map kf ki b formal ip =
   let property = Property.ip_of_requires kf ki b ip in
-  add_ip { map ; property ; formal = Vmap.empty ; result = pure } ip
+  add_ip { map ; property ; formal ; result = pure } ip
 
-let add_assumes map kf ki b ip =
+let add_assumes map kf ki b formal ip =
   let property = Property.ip_of_assumes kf ki b ip in
-  add_ip { map ; property ; formal = Vmap.empty ; result = pure } ip
+  add_ip { map ; property ; formal ; result = pure } ip
 
-let add_assigns map kf ki b asgn =
+let add_assigns map kf ki b formal asgn =
   match asgn with
   | WritesAny -> ()
   | Writes ws ->
     let bhv = Property.Id_contract (Datatype.String.Set.empty,b) in
     let property = Option.get @@ Property.ip_of_assigns kf ki bhv asgn in
-    let env = { map ; property ; formal = Vmap.empty ; result = pure } in
+    let env = { map ; property ; formal ; result = pure } in
     List.iter (iadd_from env) ws
 
-let add_allocation map kf ki b alloc =
+let add_allocation map kf ki b formal alloc =
   match alloc with
   | FreeAllocAny -> ()
   | FreeAlloc (its1, its2) ->
     let bhv = Property.Id_contract (Datatype.String.Set.empty,b) in
     let property = Option.get @@ Property.ip_of_allocation kf ki bhv alloc in
-    let env = { map ; property ; formal = Vmap.empty ; result = pure } in
+    let env = { map ; property ; formal ; result = pure } in
     List.iter (iadd_it env) its1 ;
     List.iter (iadd_it env) its2
 
+let add_post_cond m kf ki b formal cs =
+  let add_post_cond map property (_,ip) =
+    add_ip { map ; property ; formal ; result = pure } ip
+  in
+  let post_conds = Property.ip_post_cond_of_behavior kf ki ~active:[] b in
+  List.iter2 (add_post_cond m) post_conds cs
+
 let add_extension _ = ()
 
-let add_post_cond _ = ()
-
 let add_behavior ~kf ~ki (m:map) (b:behavior) =
-  List.iter (add_requires m kf ki b) b.b_requires ;
-  List.iter (add_assumes m kf ki b)  b.b_assumes  ;
-  List.iter (add_post_cond)          b.b_post_cond ;
-  add_assigns m kf ki b              b.b_assigns ;
-  add_allocation m kf ki b           b.b_allocation ;
-  List.iter (add_extension)          b.b_extended
+  let formals = Kernel_function.get_formals kf in
+  let add_formal f v = Vmap.add v (of_typ (new_chunk m) v.vtype) f in
+  let f = List.fold_left add_formal Vmap.empty formals in
+  List.iter (add_requires m kf ki b f) b.b_requires ;
+  List.iter (add_assumes m kf ki b f)  b.b_assumes  ;
+  add_post_cond m kf ki b f            b.b_post_cond ;
+  add_assigns m kf ki b f              b.b_assigns ;
+  add_allocation m kf ki b f           b.b_allocation ;
+  List.iter (add_extension)            b.b_extended
 
 
 (* let add_code_annot ... = ... *)
