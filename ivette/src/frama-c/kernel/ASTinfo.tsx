1f0efafe7fa7f7ca1d2a584ea3878ddb17eb6a19
David Bühler <david.buhler@cea.fr>
2025-04-28 12:05:01 +0200

    [Ivette] Clears markers from the Inspector when the Frama-C project changes.

--- ivette/src/frama-c/kernel/ASTinfo.tsx
+++ ivette/src/frama-c/kernel/ASTinfo.tsx
@@ -37,6 +37,7 @@ import { Code } from 'dome/controls/labels';
 import { IconButton } from 'dome/controls/buttons';
 import * as Boxes from 'dome/layout/boxes';
 import { TitleBar } from 'ivette';
+import { projectChanged } from 'frama-c/kernel/api/services';
 
 // --------------------------------------------------------------------------
 // --- Marker Utility
@@ -265,6 +266,7 @@ export default function ASTinfo(): JSX.Element {
   });
   const clearMarkers = React.useCallback(() => setMarkers([]), []);
   Server.useShutdown(clearMarkers);
+  Server.onSignal(projectChanged, clearMarkers);
     // Scrolling Hooks
   const [inside, setInside] = React.useState(false);
   const scroll = React.useRef<HTMLDivElement>(null);
