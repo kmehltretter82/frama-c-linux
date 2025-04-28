016cc0b0fa0c70f093ec5bf69d66c7d6bb903a5a
David Bühler <david.buhler@cea.fr>
2025-04-28 11:25:18 +0200

    [Ivette] Resets Values table global states when the Frama-C project changes.

--- ivette/src/frama-c/plugins/eva/valuetable.tsx
+++ ivette/src/frama-c/plugins/eva/valuetable.tsx
@@ -33,6 +33,7 @@ import * as Ast from 'frama-c/kernel/api/ast';
 import * as Eva from 'frama-c/plugins/eva/api/general';
 import * as Values from 'frama-c/plugins/eva/api/values';
 import { EvaReady, EvaStatus } from './components/AnalysisStatus';
+import { projectChanged } from 'frama-c/kernel/api/services';
 
 import { classes } from 'dome/misc/utils';
 import { Icon } from 'dome/controls/icons';
@@ -967,6 +968,17 @@ function useEvaluationMode(props: EvaluationModeProps): void {
 export const CallstackState = new GlobalState<callstack>('Summary');
 const ScopesManagerState = new GlobalState(new ScopesManager());
 const FocusState = new GlobalState<Probe | undefined>(undefined);
+
+
+export function clearGlobalState(): void {
+  CallstackState.setValue('Summary');
+  ScopesManagerState.setValue(new ScopesManager());
+  FocusState.setValue(undefined);
+}
+
+{
+  Server.onSignal(projectChanged, clearGlobalState);
+}
 
 /* Component */
 function EvaTable(): JSX.Element {
