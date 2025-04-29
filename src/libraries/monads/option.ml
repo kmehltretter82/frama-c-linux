9d11d9ae1abf942ae3952226cc49fa6d698223f3
Thibault Martin <thi.martin.pro@pm.me>
2025-04-29 09:55:08 +0200

    [kernel] Avoid shadowing Stdlib modules

--- src/libraries/monads/option.ml
+++ src/libraries/monads/option.ml
@@ -20,13 +20,15 @@
 (*                                                                        *)
 (**************************************************************************)
 
-include Stdlib.Option
-
 module Minimal = struct
   type 'a t = 'a option
   let return v = Some v
-  let bind f m = bind m f
+  let bind f m = Stdlib.Option.bind m f
   let product l r = match l, r with Some l, Some r -> Some (l, r) | _ -> None
 end
 
 include Monad.Make_based_on_bind_with_product (Minimal)
+
+include Stdlib.Option
+
+let bind = Minimal.bind
