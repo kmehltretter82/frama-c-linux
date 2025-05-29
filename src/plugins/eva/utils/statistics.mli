63569f276cca38c740dcb7c5b6fe59c18c2a963a
Valentin Perrelle <valentin.perrelle@cea.fr>
2025-05-29 14:47:25 +0200

    [Eva] Enable float statistics gathering

--- src/plugins/eva/utils/statistics.mli
+++ src/plugins/eva/utils/statistics.mli
@@ -24,53 +24,64 @@
 (** This module keeps track of statistics collected by Eva during an
     analysis. *)
 
-(** Type of statistics. Type parameter ['a] show whether a statistic is tied:
+(** Type of statistics. Type parameter ['ty] is either bound to [int] or
+    [float] for integer and float statistics respectively and ['k] show whether
+    a statistic is tied:
     - to [kernel_function],
     - to Cil [stmt],
     - to the whole program, with [unit].  *)
-type 'a t
+type ('k, 'ty) t
 
 (** {2 Registered statistics } *)
 
-val iterations : Cil_types.stmt t
-val memexec_hits : Cil_types.kernel_function t
-val memexec_misses : Cil_types.kernel_function t
-val max_widenings : Cil_types.stmt t
-val max_unrolling : Cil_types.stmt t
-val partitioning_index_hits : unit t
-val partitioning_index_misses : unit t
+val iterations : (Cil_types.stmt, int) t
+val memexec_hits : (Cil_types.kernel_function, int) t
+val memexec_misses : (Cil_types.kernel_function, int) t
+val max_widenings : (Cil_types.stmt, int) t
+val max_unrolling : (Cil_types.stmt, int) t
+val partitioning_index_hits : (unit, int) t
+val partitioning_index_misses : (unit, int) t
 
 
 (** {2 Statistics registration } *)
 
+type _ typ =
+  | Int : int typ
+  | Float : float typ
+
 (** Registers a statistic tied to the whole program. *)
-val register_global_stat : string -> unit t
+val register_global_stat : string -> 'ty typ -> (unit,'ty) t
 
 (** Registers a statistic tied to functions. *)
-val register_function_stat : string -> Cil_types.kernel_function t
+val register_function_stat :
+    string -> 'ty typ ->(Cil_types.kernel_function, 'ty) t
 
 (** Registers a statistic tied to statements. *)
-val register_statement_stat : string -> Cil_types.stmt t
+val register_statement_stat : string -> 'ty typ -> (Cil_types.stmt, 'ty) t
 
 
 (** {2 Statistics retrieval } *)
 
 (** Get the current stat value for a given element (statement, function or unit
     according to the statistic type). **)
-val get : 'a t -> 'a -> int
+val get : ('k, 'ty) t -> 'k -> 'ty
 
 
 (** {2 Statistics update } *)
 
 (** Set the stat to the given value. *)
-val set : 'a t -> 'a -> int -> unit
+val set : ('k, 'ty) t -> 'k -> 'ty -> unit
 
-(** Adds 1 to the stat or set it to 1 if undefined. *)
-val incr : 'a t -> 'a -> unit
+(** Adds 1 to the stat or set it to 1 if the stat is currently undefined. *)
+val incr : ('k, int) t -> 'k -> unit
+
+(** Add a custom amount to the stat or set it to this amount the stat is
+    currently undefined. *)
+val add : ('k, 'ty) t -> 'k -> 'ty -> unit
 
 (** Set the stat to the maximum between the current value and the given
     value. *)
-val grow : 'a t -> 'a -> int -> unit
+val grow : ('k, 'ty) t -> 'k -> 'ty -> unit
 [@@@ api_end]
 
 (** Reset all statistics to zero. *)
