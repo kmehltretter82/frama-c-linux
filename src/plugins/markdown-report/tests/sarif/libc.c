969ffb52c19e596f9784bea5abe5af59fc2c2610
Andre Maroneze <andre.maroneze@cea.fr>
2025-06-25 23:27:49 +0200

    [markdown-report] fix filename extension for -mdr-out and add test

--- src/plugins/markdown-report/tests/sarif/libc.c
+++ src/plugins/markdown-report/tests/sarif/libc.c
@@ -1,14 +1,14 @@
 /* run.config
  CMD: @frama-c@ -eva -eva-no-results -mdr-gen sarif -mdr-sarif-deterministic
- BIN: with-libc.sarif.unchecked
-   OPT: -mdr-out ./with-libc.sarif.unchecked
-   COMMENT: the log with libc is too large, so we do not use it as textual oracle
+ BIN: with-libc-unchecked.sarif
+   OPT: -mdr-out ./with-libc-unchecked
+   COMMENT: the log with libc is too large, so we do not use it as textual oracle; we also test -mdr-out without a file extension
  BIN: without-libc.sarif.unfiltered
    OPT: -mdr-no-print-libc -mdr-out ./without-libc.sarif.unfiltered
    EXECNOW: LOG without-libc.sarif sed -e "s:@PTEST_SESSION@:PTEST_SESSION:" %{dep:without-libc.sarif.unfiltered} > without-libc.sarif 2> @DEV_NULL@
 
  ENABLED_IF: (and %{bin-available:check-jsonschema} %{bin-available:jq})
- EXECNOW: LOG with-libc.sarif.checked check-jsonschema --no-cache --schemafile $(jq '."$schema"' ./with-libc.sarif.unchecked -r) %{dep:with-libc.sarif.unchecked} > ./with-libc.sarif.checked
+ EXECNOW: LOG with-libc.sarif.checked check-jsonschema --no-cache --schemafile $(jq '."$schema"' ./with-libc-unchecked.sarif -r) %{dep:with-libc-unchecked.sarif} > ./with-libc.sarif.checked
 */
 #include <string.h>
 int main() {
