658f314995e37c8f527351ae825ed21751cf186a
Thibault Martin <thibault.martin@cea.fr>
2025-08-01 11:41:40 +0200

    [attributes] Remove old attribute Variant in doc example

--- src/kernel_services/ast_queries/ast_attributes.ml
+++ src/kernel_services/ast_queries/ast_attributes.ml
@@ -23,7 +23,7 @@ let get_name (an, _) =
    attribute for both the [x] attribute and the attributes of the list. For
    example, if have a call:
 
-   drop_attribute "__const" [ Attr("const", []) ; Attr("__const", []) ; Attr("__const__", []) ]
+   drop_attribute "__const" [ ("const", []) ; ("__const", []) ; ("__const__", []) ]
 
    The result is [].
 *)
