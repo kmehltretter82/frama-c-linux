ef4d1c9ff424c1e5f5fcdf5dbabc0f1fa326d7c8
Basile Desloges <basile.desloges@cea.fr>
2025-09-04 17:24:57 +0200

    [libc] Add __FC_INTERN to field _fc of Pthreads types

--- share/libc/__fc_define_pthread_types.h
+++ share/libc/__fc_define_pthread_types.h
@@ -15,21 +15,21 @@ __BEGIN_DECLS
 // stronger typing constraints
 
 #ifndef __have_pthread_attr_t
-typedef struct __fc_pthread_attr_t { int _fc; } pthread_attr_t;
+typedef struct __fc_pthread_attr_t { __FC_INTERN int _fc; } pthread_attr_t;
 #define __have_pthread_attr_t 1
 #endif
 
-typedef struct __fc_pthread_barrier_t { int _fc; } pthread_barrier_t;
-typedef struct __fc_pthread_barrierattr_t { int _fc; } pthread_barrierattr_t;
-typedef struct __fc_pthread_cond_t { int _fc; } pthread_cond_t;
-typedef struct __fc_pthread_condattr_t { int _fc; } pthread_condattr_t;
-typedef struct __fc_pthread_key_t { int _fc; } pthread_key_t;
-typedef struct __fc_pthread_mutex_t { int _fc; } pthread_mutex_t;
-typedef struct __fc_pthread_mutexattr_t { int _fc; } pthread_mutexattr_t;
-typedef struct __fc_pthread_once_t { int _fc; } pthread_once_t;
-typedef struct __fc_pthread_rwlock_t { int _fc; } pthread_rwlock_t;
-typedef struct __fc_pthread_rwlockattr_t { int _fc; } pthread_rwlockattr_t;
-typedef struct __fc_pthread_spinlock_t { int _fc; } pthread_spinlock_t;
+typedef struct __fc_pthread_barrier_t { __FC_INTERN int _fc; } pthread_barrier_t;
+typedef struct __fc_pthread_barrierattr_t { __FC_INTERN int _fc; } pthread_barrierattr_t;
+typedef struct __fc_pthread_cond_t { __FC_INTERN int _fc; } pthread_cond_t;
+typedef struct __fc_pthread_condattr_t { __FC_INTERN int _fc; } pthread_condattr_t;
+typedef struct __fc_pthread_key_t { __FC_INTERN int _fc; } pthread_key_t;
+typedef struct __fc_pthread_mutex_t { __FC_INTERN int _fc; } pthread_mutex_t;
+typedef struct __fc_pthread_mutexattr_t { __FC_INTERN int _fc; } pthread_mutexattr_t;
+typedef struct __fc_pthread_once_t { __FC_INTERN int _fc; } pthread_once_t;
+typedef struct __fc_pthread_rwlock_t { __FC_INTERN int _fc; } pthread_rwlock_t;
+typedef struct __fc_pthread_rwlockattr_t { __FC_INTERN int _fc; } pthread_rwlockattr_t;
+typedef struct __fc_pthread_spinlock_t { __FC_INTERN int _fc; } pthread_spinlock_t;
 typedef unsigned long pthread_t;
 __END_DECLS
 __POP_FC_STDLIB
