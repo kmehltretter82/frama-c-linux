e7ab74c054ff5026a0a8a3c545537bd619b747a9
Allan Blanchard <allan.blanchard@cea.fr>
2025-09-17 12:45:30 +0200

    [nix] fix Why3 -> downgrade Coq

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -10,7 +10,9 @@ let
     ocamlgraph = oself.callPackage ./ocamlgraph.nix {};
     ppxlib = oself.callPackage ./ppxlib.nix {};
     ppxlib_jane = oself.callPackage ./ppxlib_jane.nix {};
-    why3 = oself.callPackage ./why3.nix {};
+    why3 = oself.callPackage ./why3.nix {
+      coqPackages = pkgs.coqPackages_8_19 ;
+    };
 
     # Helpers
     mk_tests = oself.callPackage ./mk_tests.nix {};
--- nix/why3.nix
+++ nix/why3.nix
@@ -1,78 +1,79 @@
-{ callPackage
-, fetchurl
-, fetchpatch
-, lib
+{ lib
 , stdenv
 , ocaml
+, camlzip
 , findlib
-, ocamlgraph
-, zarith
 , menhir
 , menhirLib
-, mlmpfr
-, js_of_ocaml
-, js_of_ocaml-ppx
+, ocamlgraph
 , ppx_deriving
 , ppx_sexp_conv
-, camlzip
-, sexplib
 , re
-, num
-, lablgtk3-sourceview3
+, sexplib
+, zarith
 , coqPackages
-, rubber
-, hevea
-, emacs
 , autoreconfHook
 }:
 
 stdenv.mkDerivation rec {
   pname = "why3";
+  src = (import ./sources.nix {}).why3;
   version = src.version;
 
-  src = (import ./sources.nix {}).why3;
   nativeBuildInputs = [
     autoreconfHook
-  ];
-  buildInputs = [
     ocaml
     findlib
-    ocamlgraph
-    zarith
     menhir
-    menhirLib
-    mlmpfr
-    # Emacs compilation of why3.el
-    emacs
-    # Documentation
-    rubber
-    hevea
-    # GUI
-    lablgtk3-sourceview3
-    # WebIDE
-    js_of_ocaml
-    js_of_ocaml-ppx
-    # S-expression output for why3pp
+    coqPackages.coq
+  ];
+
+  buildInputs = [
+    ocamlgraph
     ppx_deriving
     ppx_sexp_conv
-    # Coq support
+    zarith
     coqPackages.coq
     coqPackages.flocq
   ];
 
-  propagatedBuildInputs = [ camlzip num re sexplib ];
+  propagatedBuildInputs = [
+    camlzip
+    menhirLib
+    re
+    sexplib
+    zarith
+  ];
 
   enableParallelBuilding = true;
 
-  configureFlags = [ "--enable-verbose-make" ];
+  configureFlags = [
+    "--enable-verbose-make"
+  ];
 
-  installTargets = [ "install" "install-lib" ];
+  outputs = [
+    "out"
+    "dev"
+  ];
+
+  installTargets = [
+    "install"
+    "install-lib"
+  ];
+
+  postInstall = ''
+    mkdir -p $dev/lib
+    mv $out/lib/ocaml $dev/lib/
+  '';
 
   meta = with lib; {
-    description = "A platform for deductive program verification";
-    homepage = "http://why3.lri.fr/";
+    description = "Platform for deductive program verification";
+    homepage = "https://why3.lri.fr/";
     license = licenses.lgpl21;
     platforms = platforms.unix;
-    maintainers = with maintainers; [ thoughtpolice vbgl ];
+    maintainers = with maintainers; [
+      thoughtpolice
+      vbgl
+    ];
   };
 }
