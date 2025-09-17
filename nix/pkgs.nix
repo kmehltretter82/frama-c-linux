d98ae829e2092f61204a95a50e092052acc64398
Allan Blanchard <allan.blanchard@cea.fr>
2025-09-17 13:58:02 +0200

    [nix] removes now useless packages

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -7,9 +7,6 @@ let
     combinetura = oself.callPackage ./combinetura.nix {};
     dolmen = oself.callPackage ./dolmen.nix {};
     mlmpfr = oself.callPackage ./mlmpfr.nix {};
-    ocamlgraph = oself.callPackage ./ocamlgraph.nix {};
-    ppxlib = oself.callPackage ./ppxlib.nix {};
-    ppxlib_jane = oself.callPackage ./ppxlib_jane.nix {};
     why3 = oself.callPackage ./why3.nix {
       coqPackages = pkgs.coqPackages_8_19 ;
     };
