21b0ade728baa6bf395bbf6add512ceb0df6599a
Virgile Prevosto <virgile.prevosto@cea.fr>
2025-09-23 11:47:34 +0200

    [tests] test inserting string literal on empty AST

---
+++ tests/misc/string_lit.t/string_literal.ml
@@ -0,0 +1,26 @@
+open Cil_types
+open Extlib
+
+let loc = Cil_datatype.Location.dummy
+
+let main () =
+  let prj = Project.create "empty" in
+  Project.set_current prj;
+  let str_lit =
+    Globals.Vars.add_string_literal ~loc (Str "example string literal")
+  in
+  let wstr_lit =
+    Globals.Vars.add_string_literal ~loc
+      (Wstr (List.map (Int64.of_int $ int_of_char) ['t'; 'e'; 's'; 't']))
+  in
+  Format.printf
+    "String literal: %a@." Printer.pp_str_literal
+    (Globals.Vars.get_string_literal str_lit);
+  Format.printf
+    "Wide string literal: %a@." Printer.pp_str_literal
+    (Globals.Vars.get_string_literal wstr_lit);
+  Demote_string_literal.demote str_lit;
+  Demote_string_literal.demote wstr_lit;
+  File.pretty_ast ()
+
+let () = Boot.Main.extend main
