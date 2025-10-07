54876df64dcad4ac46a0c88f3e7d54a162a76a31
Virgile Prevosto <virgile.prevosto@cea.fr>
2025-10-07 14:24:53 +0200

    [nix] uniformize version of clang used across nix derivations

--- nix/frama-c-checkers-shell.nix
+++ nix/frama-c-checkers-shell.nix
@@ -1,7 +1,7 @@
 { lib
 , stdenv
 , black
-, clang_19
+, clang
 , combinetura
 , frama-c-hdrck
 , frama-c-lint
@@ -16,7 +16,7 @@ stdenv.mkDerivation rec {
   name = "frama-c-checkers-shell";
   buildInputs = [
     black
-    clang_19
+    clang
     combinetura
     frama-c-hdrck
     frama-c-lint
