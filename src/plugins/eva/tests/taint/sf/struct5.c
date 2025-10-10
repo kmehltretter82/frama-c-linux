f0b5deea29b7f2f6ecedba09f0e3fd14ba6bd9a8
Michele Alberti <michele.alberti@cea.fr>
2025-10-10 18:19:46 +0200

    [Eva] Revise struct5.c test assertion.

--- src/plugins/eva/tests/taint/sf/struct5.c
+++ src/plugins/eva/tests/taint/sf/struct5.c
@@ -23,9 +23,10 @@ int main(void) {
         {{0}, {1}, {secret}},
         { [2] = {3, {secret}}, [1] = {5, {6}}, [0] = {7, {8}}}
     };
-    /*@ assert security_status(a.a_inner[0].i_y) == private; */
+    /*@ assert security_status(a.a_inner[0].i_y) == public; */
     /*@ assert security_status(a.a_simple[0].s_x) == public; */
-    /*@ assert security_status(a.a_simple[0].s_inner.i_y) == private; */
+    /*@ assert security_status(a.a_simple[0].s_inner.i_y) == public; */
+    /*@ assert security_status(a.a_simple[2].s_inner.i_y) == private; */
 
     return 0;
 }
