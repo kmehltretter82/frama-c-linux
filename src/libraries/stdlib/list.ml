34ce602b7e2a4bbcecb4584204b173b80495b4c4
Maxime Jacquemin <maxime.jacquemin@cea.fr>
2025-10-22 13:54:10 +0200

    [monads] Cleaner implementation of List.product

--- src/libraries/stdlib/list.ml
+++ src/libraries/stdlib/list.ml
@@ -6,19 +6,14 @@
 (*                                                                        *)
 (**************************************************************************)
 
-module L = Stdlib.List
-
 module Minimal = struct
   type 'a t = 'a list
   let return   x = [ x ]
-  let map   f xs = L.map f xs
-  let flatten xs = L.flatten xs
-  let product ls rs = L.combine ls rs
-  let product ls rs = L.rev @@
-    L.fold_left
-      (fun acc_l l -> L.fold_left (fun acc_r r -> (l,r)::acc_r) acc_l rs)
-      []
-      ls
+  let map   f xs = Stdlib.List.map f xs
+  let flatten xs = Stdlib.List.flatten xs
+  let product ls rs =
+    let pair l = map (fun r -> (l, r)) rs in
+    Stdlib.List.concat_map pair ls
 end
 
 include Monad.Make_based_on_map_with_product (Minimal)
