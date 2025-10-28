514aa32e9893053db54f67d05c2e006f0a5027c7
Allan Blanchard <allan.blanchard@cea.fr>
2025-10-28 16:33:43 +0100

    [doc] cleanup manual files

--- src/plugins/region/doc/manual/region-examples/pointer-2.c
+++ src/plugins/region/doc/manual/region-examples/pointer-2.c
@@ -1,3 +1,11 @@
+/**************************************************************************/
+/*                                                                        */
+/*  SPDX-License-Identifier LGPL-2.1                                      */
+/*  Copyright (C)                                                         */
+/*  CEA (Commissariat à l'énergie atomique et aux énergies alternatives)  */
+/*                                                                        */
+/**************************************************************************/
+
 void g(short x, int y) {
     short *p = &x;
     *p = 1;
