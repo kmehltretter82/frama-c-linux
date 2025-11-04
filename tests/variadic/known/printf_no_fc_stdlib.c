7e87b4074bf02fb62af0fdf1d426ceeaa79874ce
Virgile Prevosto <virgile.prevosto@cea.fr>
2025-11-04 09:38:41 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: Allan Blanchard <allan.blanchard@cea.fr>

--- tests/variadic/known/printf_no_fc_stdlib.c
+++ tests/variadic/known/printf_no_fc_stdlib.c
@@ -1,5 +1,5 @@
 /* run.config
-   ENABLED_IF: (= %{system} linux)
+   ENABLED_IF: (and (= %{system} linux) (= %{architecture} amd64))
    STDOPT: #"-no-frama-c-stdlib" +"-kernel-msg-key=\"-variadic\""
 */
 #include <stdio.h>
