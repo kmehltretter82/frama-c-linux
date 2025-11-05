1043c1502b602308904dfeed4816adffec7bf221
Andre Maroneze <andre.maroneze@cea.fr>
2025-11-05 22:34:04 +0100

    [make-machdep] improve musl compatibility
    musl defines __WORDSIZE in a different file. If the preprocessor accepts
    __has_include, then we try to include such file if it exists. Otherwise, do
    nothing. This ensures that at least musl-gcc is slightly improved when used to
    generate a machdep.

--- share/machdeps/make_machdep/wordsize.c
+++ share/machdeps/make_machdep/wordsize.c
@@ -8,6 +8,12 @@
 
 #include <features.h>
 
+#if defined __has_include
+#  if __has_include (<bits/reg.h>) // musl defines __WORDSIZE here
+#    include <bits/reg.h>
+#  endif
+#endif
+
 #if defined(__WORDSIZE)
 const int wordsize_is = __WORDSIZE;
 #endif
