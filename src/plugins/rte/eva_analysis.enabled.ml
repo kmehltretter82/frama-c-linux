ebc2482f5498d14fe1e26aca932f2aa9121df422
Allan Blanchard <allan.blanchard@cea.fr>
2025-11-07 14:10:55 +0100

    [rte] more idiomatic use of the Eva API

--- src/plugins/rte/eva_analysis.enabled.ml
+++ src/plugins/rte/eva_analysis.enabled.ml
@@ -1,4 +1,7 @@
 let is_available = true
 
 let is_computed kf =
-  Eva.Analysis.is_computed () && Eva.Results.is_called kf
+  Eva.Analysis.is_computed () &&
+  match Eva.Analysis.status kf with
+  | Eva.Analysis.Analyzed _ -> true
+  | _ -> false
