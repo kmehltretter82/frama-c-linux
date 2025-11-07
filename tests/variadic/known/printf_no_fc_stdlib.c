3dfa88cc93ff1045cb7ebabdf14d50aecbe4f299
Basile Desloges <basile.desloges@cea.fr>
2025-11-07 14:55:53 +0100

    [tests] Activate printf_no_fc_stdlib.c only on x86_64-pc-linux-gnu

--- tests/variadic/known/printf_no_fc_stdlib.c
+++ tests/variadic/known/printf_no_fc_stdlib.c
@@ -1,5 +1,7 @@
 /* run.config
-   ENABLED_IF: (and (= %{system} linux) (= %{architecture} amd64))
+   COMMENT: Only activate the test on linux x64 with glibc as it is dependent on
+   COMMENT: the content of the system's stdio.h
+   ENABLED_IF: (= %{ocaml-config:target} x86_64-pc-linux-gnu)
    STDOPT: #"-no-frama-c-stdlib" +"-kernel-msg-key=\"-variadic\""
 */
 #include <stdio.h>
