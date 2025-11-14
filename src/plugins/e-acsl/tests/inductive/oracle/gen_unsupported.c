48772ed274c84cb749983d253a9d010a405b4041
Jan Rochel <jan.rochel@cea.fr>
2025-11-14 11:44:08 +0100

    [e-acsl] inductives: add unsupported test case
    Here, there is no possible extraction because of a variable being used
    before being future-let-bound by a recursive inductive occurrence.

--- src/plugins/e-acsl/tests/inductive/oracle/gen_unsupported.c
+++ src/plugins/e-acsl/tests/inductive/oracle/gen_unsupported.c
@@ -59,6 +59,16 @@ inductive __gen_e_acsl_is_max_here(int *ptr, integer length, integer max) {
                         __gen_e_acsl_is_max_here(p, l, m),
                       Here);
   }
+ */
+/*@
+inductive fibo(integer i, integer x) {
+  case zero: fibo(0, 0);
+  case one: fibo(1, 1);
+  case other: \forall integer n, integer f1, integer f2;
+                n > 1 ==>
+                f1 >= 0 ==>
+                fibo(n - 1, f1) ==> fibo(n - 2, f2) ==> fibo(n, f1 + f2);
+  }
 
 */
 int t[4] = {1, 3, 2, 4};
