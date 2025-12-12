f845b6c1905affed1d1848aa29836025ef5d6e3e
Jan Rochel <jan.rochel@cea.fr>
2025-12-12 17:42:38 +0100

    [e-acsl] inductives: add second definition of GCD

--- src/plugins/e-acsl/tests/inductive/gcd.c
+++ src/plugins/e-acsl/tests/inductive/gcd.c
@@ -11,10 +11,18 @@
           y != 0 ==> gcd(y, x % y, z) ==> gcd(x, y, z);
     }
 
+    inductive gcd2(ℤ a, ℤ b, ℤ r) {
+      case eq: ∀ ℤ x; gcd2(x, x, x);
+      case gt: ∀ ℤ x, y, r; x > y ==> gcd2(x - y, y, r) ==> gcd2(x, y, r);
+      case lt: ∀ ℤ x, y, r; x < y ==> gcd2(x, y - x, r) ==> gcd2(x, y, r);
+    }
+
 */
 
 int main() {
   /*@ assert gcd(42, 24, 6); */
   /*@ assert !gcd(42, 24, 7); */
+  /*@ assert gcd2(42, 24, 6); */
+  /*@ assert !gcd2(42, 24, 7); */
   return 0;
 }
