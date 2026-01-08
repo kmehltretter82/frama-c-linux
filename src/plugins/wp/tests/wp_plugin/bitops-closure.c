9e07a9b9d92c91ebcb87c1aa8c744e1d8cd4c5d1
Allan Blanchard <allan.blanchard@cea.fr>
2026-01-08 10:00:38 +0100

    [wp] simplifies a test

--- src/plugins/wp/tests/wp_plugin/bitops-closure.c
+++ src/plugins/wp/tests/wp_plugin/bitops-closure.c
@@ -8,54 +8,26 @@
 
 #include <stdint.h>
 
-/*@ assigns \nothing; */
 void xor8u(uint8_t x, uint8_t y) { x ^= y; }
-
-/*@ assigns \nothing; */
 void or8u(uint8_t x, uint8_t y) { x |= y; }
-
-/*@ assigns \nothing; */
 void and8u(uint8_t x, uint8_t y) { x &= y; }
-
-/*@ requires y < 8;
-    assigns \nothing; */
+//@ requires y < 8;
 void sr8u(uint8_t x, uint8_t y) { x >>= y; }
 
-/*@ assigns \nothing; */
 void xor16u(uint16_t x, uint16_t y) { x ^= y; }
-
-/*@ assigns \nothing; */
 void or16u(uint16_t x, uint16_t y) { x |= y; }
-
-/*@ assigns \nothing; */
 void and16u(uint16_t x, uint16_t y) { x &= y; }
-
-/*@ requires y < 16;
-    assigns \nothing; */
+//@ requires y < 16;
 void sr16u(uint16_t x, uint16_t y) { x >>= y; }
 
-/*@ assigns \nothing; */
 void xor8s(int8_t x, int8_t y) { x ^= y; }
-
-/*@ assigns \nothing; */
 void or8s(int8_t x, int8_t y) { x |= y; }
-
-/*@ assigns \nothing; */
 void and8s(int8_t x, int8_t y) { x &= y; }
-
-/*@ requires 0 <= y < 8;
-    assigns \nothing; */
+//@ requires 0 <= y < 8;
 void sr8s(int8_t x, int8_t y) { x >>= y; }
 
-/*@ assigns \nothing; */
 void xor16s(int16_t x, int16_t y) { x ^= y; }
-
-/*@ assigns \nothing; */
 void or16s(int16_t x, int16_t y) { x |= y; }
-
-/*@ assigns \nothing; */
 void and16s(int16_t x, int16_t y) { x &= y; }
-
-/*@ requires 0 <= y < 16;
-    assigns \nothing; */
+//@ requires 0 <= y < 16;
 void sr16s(int16_t x, int16_t y) { x >>= y; }
