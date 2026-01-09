6b301dc8c60a6767991eb7de7140d59ac456c12c
Valentin Perrelle <valentin.perrelle@cea.fr>
2026-01-09 15:16:05 +0100

    [kernel] fclib: remove Pretty.pretty_iter

--- src/libraries/stdlib/pretty.ml
+++ src/libraries/stdlib/pretty.ml
@@ -10,24 +10,9 @@ type 'a aformatter = Format.formatter -> 'a -> unit
 type tformatter = Format.formatter -> unit
 type nonrec 'a format = ('a,Format.formatter,unit) format
 
-(* These functions are similar to Pretty_utils.pp_iter, but must be redefined
-   here to avoid cyclic dependencies as Pretty_utils depends on Array and List.
-   They also allow for a slightly more generic formats. *)
-
-let pretty_iter ~format ~item ~sep ~iter pp_item fmt collection =
-  let need_sep = ref false in
-  let pp_elements fmt =
-    iter (fun elt ->
-        if !need_sep then Format.fprintf fmt sep else need_sep := true;
-        Format.fprintf fmt item pp_item elt;
-      ) collection;
-  in
-  Format.fprintf fmt format pp_elements
-
-let pretty_iter2 ~format ~item ~sep ~iter pp_key pp_val fmt collection =
-  let iter f = iter (fun k v -> f (k,v))
-  and pp fmt (k,v) = Format.fprintf fmt item pp_key k pp_val v in
-  pretty_iter ~format ~sep ~item:"%a" ~iter pp fmt collection
+(* This functions is inspired by Pretty_utils functions and must be redefined
+   here to avoid cyclic dependencies as Pretty_utils depends on Array and
+   List. *)
 
 let pretty_seq ~format ~item ~sep ?(last=sep) ?empty pp_item fmt seq =
   match Seq.uncons seq with
--- src/libraries/stdlib/pretty.mli
+++ src/libraries/stdlib/pretty.mli
@@ -19,32 +19,6 @@ type tformatter = Format.formatter -> unit
 (** Formats used with [fprintf] and alike. *)
 type nonrec 'a format = ('a,Format.formatter,unit) format
 
-(** Build a pretty printer for a collection using an iterator and a pretty
-    printer of its items.
-    @param format defines the format used to print the collection, e.g. "@[%t@]"
-    @param item defines the format for an item of the collection, e.g. "@[%a@]"
-    @param sep defines the format for the separator between items, e.g "@;,"
-    @param iter is an iterator over the items of the collection. *)
-val pretty_iter:
-  format:(tformatter -> unit) format ->
-  item:('a aformatter -> 'a -> unit) format ->
-  sep:unit format ->
-  iter:(('a -> unit) -> 'b -> unit) ->
-  'a aformatter -> 'b aformatter
-
-(** Build a pretty printer for a map collection using an iterator and a
-    pretty printer of its bindings.
-    @param format defines the format used to print the collection, e.g. "@[%t@]"
-    @param item defines the format for a binding, e.g. "@[%a ->@ %a@]"
-    @param sep defines the format for the separator between bindings, e.g "@;,"
-    @param iter is an iterator over the elements of the collection. *)
-val pretty_iter2:
-  format:(tformatter -> unit) format ->
-  item:('a aformatter -> 'a -> 'b aformatter -> 'b -> unit) format ->
-  sep:unit format ->
-  iter:(('a -> 'b -> unit) -> 'c -> unit) ->
-  'a aformatter -> 'b aformatter -> 'c aformatter
-
 (** Pretty prints a sequence.
     @param format defines the format used to print the collection, e.g. "@[%t@]"
     @param item defines the format for an item of the collection, e.g. "@[%a@]"
