8abbc72df11c901cc0b70c1aa9f28130079c56e8
Jan Rochel <jan.rochel@cea.fr>
2026-01-22 11:38:09 +0100

    [e-acsl] new example tests/inductive/sorted.c

---
+++ src/plugins/e-acsl/tests/inductive/sorted.c
@@ -0,0 +1,38 @@
+/* run.config
+   COMMENT: based on ACSL Mini-Tutorial (https://github.com/acsl-language/acsl)
+   STDOPT: +"-eva-unroll-recursive-calls 9"
+*/
+
+#include <stddef.h>
+
+typedef struct list {
+  int hd;
+  struct list *next;
+} list;
+
+/*@
+  predicate Pvalid(list *ptr) = \valid(ptr);
+
+  inductive sorted_decr(list* node) {
+    case sorted_nil: sorted_decr(\null);
+    case sorted_singleton:
+    \forall list* node;
+       Pvalid(node) && node->next == \null ==>
+          sorted_decr(node);
+    case sorted_next:
+    \forall list* node;
+       Pvalid(node) && Pvalid(node->next) &&
+       node->hd >= node->next->hd &&
+          sorted_decr(node->next) ==> sorted_decr(node);
+  }
+*/
+
+list last = {.hd = 2, .next = NULL};
+list next = {.hd = 3, .next = &last};
+list root = {.hd = 1, .next = &next};
+
+int main() {
+  //@ assert sorted_decr(&next);
+  //@ assert !sorted_decr(&root);
+  return 0;
+}
