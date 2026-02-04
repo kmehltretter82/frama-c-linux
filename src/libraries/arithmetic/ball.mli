3a2c6ab1c292277d364655838eb1a46b9d13037f
Maxime Jacquemin <maxime.jacquemin@cea.fr>
2026-02-04 14:49:11 +0100

    [arithmetic] Documention of the [Ball] module

--- src/libraries/arithmetic/ball.mli
+++ src/libraries/arithmetic/ball.mli
@@ -6,20 +6,47 @@
 (*                                                                        *)
 (**************************************************************************)
 
-
-(* Operations on n dimensional closed balls over a field 𝕂. *)
-
+(** This module provides a representation of closed balls in a classical
+    vector space over a field 𝕂 equiped with the ∞-norm. *)
 module Make (K : Field.S) : sig
+
   open Nat
   open Linear.Space (K)
+
+  (** Balls are represented as a center and a radius. The radius components
+      are always all positives. *)
   type 'n t = { center : 'n vector ; radius : 'n vector }
+
+  (** The call [make center radius] returns a ball of center [center] and
+      of radius [abs radius], with [abs] the componentwise absolute value
+      on vectors as defined in {!Linear}. *)
   val make : 'n succ vector -> 'n succ vector -> 'n succ t
-  val zero : 'n succ nat -> 'n succ t
+
+  (** The call [point center] returns a ball of radius zero and of
+      center [center], i.e a point in the vector space. *)
   val point : 'n succ vector -> 'n succ t
+
+  (** The call [zero n] returns the point zero in a [n] dimensionnal
+      vector space. *)
+  val zero : 'n succ nat -> 'n succ t
+
+  (** Pretty printer. *)
   val pretty : 'n succ t Pretty_utils.formatter
+
+  (** Balls based on the ∞-norm can also be seen as a collection of intervals.
+      The call [bounds b] returns thus the bounds in each dimension of
+      the closed space defined by [b]. The [lower b] (reps. [upper b]) function
+      returns only the lower bounds (resp. upper bounds). *)
+  val bounds : 'n t -> 'n vector * 'n vector
   val lower : 'n t -> 'n vector
   val upper : 'n t -> 'n vector
-  val bounds : 'n t -> 'n vector * 'n vector
+
+  (** The call [is_included l r] returns true if and only if all points in
+      the ball [l] are also in the ball [r]. *)
   val is_included : 'n t -> 'n t -> bool
+
+  (** Minkowsky sum of two balls, i.e {m l + r} is the ball {m z} such as
+      {m \forall x \in l, y \in r, x + y \in z}. *)
   val ( + ) : 'n t -> 'n t -> 'n t
+
 end
