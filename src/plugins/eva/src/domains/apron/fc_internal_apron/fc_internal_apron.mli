b2fe53a1d2d0f7cb2d3ec544518a128906ed5400
Maxime Jacquemin <maxime.jacquemin@cea.fr>
2026-02-05 10:32:21 +0100

    [Eva] Hidding all Apron modules in a unique internal library
    Thus avoiding polluting the namespace and thus clash with
    our Box module

---
+++ src/plugins/eva/src/domains/apron/fc_internal_apron/fc_internal_apron.mli
@@ -0,0 +1,17 @@
+(**************************************************************************)
+(*                                                                        *)
+(*  SPDX-License-Identifier LGPL-2.1                                      *)
+(*  Copyright (C)                                                         *)
+(*  CEA (Commissariat à l'énergie atomique et aux énergies alternatives)  *)
+(*                                                                        *)
+(**************************************************************************)
+
+(** This module/library is used to avoid polluting the namespace with generic
+    names from Apron library
+    @since Frama-C+dev
+*)
+
+include module type of Apron with type 'a Manager.t = 'a Apron.Manager.t
+module Box : module type of Box
+module Oct : module type of Oct
+module Polka : module type of Polka
