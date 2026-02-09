7b8977df617e1637d965625d3b8c267efa32f196
Loïc Correnson <loic.correnson@cea.fr>
2026-02-09 11:47:43 +0100

    [region/ivette] added save-model dialog

--- ivette/src/frama-c/plugins/region/index.tsx
+++ ivette/src/frama-c/plugins/region/index.tsx
@@ -16,6 +16,8 @@ import * as Tools from 'dome/frame/toolbars';
 import { Label } from 'dome/controls/labels';
 import { LCD } from 'dome/controls/displays';
 import { IconButton } from 'dome/controls/buttons';
+import { showSaveFile } from 'dome/dialogs';
+import { writeFile } from 'dome/system';
 import { Vfill, Vbox } from 'dome/layout/boxes';
 import * as Ivette from 'ivette';
 import * as Server from 'frama-c/server';
@@ -25,12 +27,24 @@ import { MemoryView } from './memory';
 import { AccessList } from './access';
 import './style.css';
 
+function saveModel(model: string, fct: string): void {
+  showSaveFile({
+    label: 'Save',
+    title: 'Save DOT model for current memory map',
+    path: Server.getPath(`region-${fct}.dot`),
+    filters: [{ name: 'Graphviz File', extensions: ["dot"] }],
+  }).then(file => {
+    if (file) writeFile(file, model);
+  });
+}
+
 function RegionAnalys(): JSX.Element {
   const [kf, setKf] = React.useState<States.Scope>();
   const [kfName, setName] = React.useState<string>();
   const [pinned, setPinned] = React.useState(false);
   const [running, setRunning] = React.useState(false);
   const [selected, setSelected] = React.useState<Region.node>();
+  const [model, setModel] = React.useState('');
   const setComputing = Dome.useProtected(setRunning);
   const { scope, marker } = States.useCurrentLocation();
   const { kind, name } = States.useDeclaration(scope);
@@ -57,11 +71,12 @@ function RegionAnalys(): JSX.Element {
       setComputing(false);
     }
   }
+  const fct = kfName ?? '---';
   return (
     <>
       <Tools.ToolBar>
         <Label label='Function' />
-        <LCD className='wp-region-lcd' label={kfName ?? '---'} />
+        <LCD className='wp-region-lcd' label={fct} />
         <Tools.Button
           icon={running ? 'EXECUTE' : 'MEDIA.PLAY'}
           title='Run region analysis on the selected function'
@@ -76,13 +91,20 @@ function RegionAnalys(): JSX.Element {
           selected={pinned}
           onClick={() => setPinned(!pinned)}
         />
+        <IconButton
+          icon='DOWNLOAD'
+          display={kfName !== undefined && model !== ''}
+          title='Save DOT model'
+          onClick={() => saveModel(model, fct)}
+        />
       </Tools.ToolBar>
       <Vfill>
         <MemoryView regions={regions}
           label={label}
           localized={localized}
           selected={selected}
-          onSelection={setSelected} />
+          onSelection={setSelected}
+          onModelChanged={setModel} />
       </Vfill>
       <Vbox>
         <AccessList region={region} selection={marker} />
