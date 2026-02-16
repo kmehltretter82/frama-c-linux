2a6a8f655717c2e308ed34a17ae21d5678ca9b1d
David Bühler <david.buhler@cea.fr>
2026-02-16 09:23:24 +0100

    [Eva] Removes unused code from Mt_interferences.
    As this module does not use the Mthread domain anymore.

--- src/plugins/eva/src/mthread/mt_interferences.ml
+++ src/plugins/eva/src/mthread/mt_interferences.ml
@@ -8,44 +8,39 @@
 
 let concurrent_writes thread shared_bases =
   let module Engine = (val Engine.current ()) in
-  match Engine.Dom.get Mt_domain.Domain.key with
-  (* Domain disabled, no information about writes *)
-  | None -> Position.Local.Set.empty
-  (* Domain enabled *)
-  | Some _extract ->
-    let add_pos stmt (cs : Callstack.t) _state acc =
-      if cs.thread <> Thread.id thread
-      then acc
-      else
-        let pos = Position.local stmt cs in
-        (* TODO: Maybe take the memory read/written for all callstacks of the
-           given statement? (can be done directly by Inout_access). *)
-        let filter = Inout_access.keep_globals_only in
-        let accesses = Inout_access.at ~filter pos in
-        let written_bases = Locations.Zone.get_bases accesses.write in
-        if Base.SetLattice.(intersects (inject shared_bases) written_bases)
-        then Position.Local.Set.add (stmt, cs) acc
-        else acc
-    in
-    let add_stmt acc stmt =
-      let is_write_stmt = match stmt.Cil_types.skind with
-        | Cil_types.Instr (Set _ | Call _ | Local_init _) -> true
-        | _ -> false
-      in
-      if is_write_stmt
-      then match Engine.get_stmt_state_by_callstack ~after:true stmt with
-        | `Top | `Bottom -> acc (* TODO: handle Tops *)
-        | `Value table ->
-          Callstack.Hashtbl.fold (add_pos stmt) table acc
+  let add_pos stmt (cs : Callstack.t) _state acc =
+    if cs.thread <> Thread.id thread
+    then acc
+    else
+      let pos = Position.local stmt cs in
+      (* TODO: Maybe take the memory read/written for all callstacks of the
+         given statement? (can be done directly by Inout_access). *)
+      let filter = Inout_access.keep_globals_only in
+      let accesses = Inout_access.at ~filter pos in
+      let written_bases = Locations.Zone.get_bases accesses.write in
+      if Base.SetLattice.(intersects (inject shared_bases) written_bases)
+      then Position.Local.Set.add (stmt, cs) acc
       else acc
+  in
+  let add_stmt acc stmt =
+    let is_write_stmt = match stmt.Cil_types.skind with
+      | Cil_types.Instr (Set _ | Call _ | Local_init _) -> true
+      | _ -> false
     in
-    let add_kf kf acc =
-      match kf.Cil_types.fundec with
-      | Declaration _ -> acc
-      | Definition (fundec,_) ->
-        List.fold_left add_stmt acc fundec.Cil_types.sallstmts
-    in
-    Globals.Functions.fold add_kf Position.Local.Set.empty
+    if is_write_stmt
+    then match Engine.get_stmt_state_by_callstack ~after:true stmt with
+      | `Top | `Bottom -> acc (* TODO: handle Tops *)
+      | `Value table ->
+        Callstack.Hashtbl.fold (add_pos stmt) table acc
+    else acc
+  in
+  let add_kf kf acc =
+    match kf.Cil_types.fundec with
+    | Declaration _ -> acc
+    | Definition (fundec,_) ->
+      List.fold_left add_stmt acc fundec.Cil_types.sallstmts
+  in
+  Globals.Functions.fold add_kf Position.Local.Set.empty
 
 let shared_bases analysis_state =
   let shared_zones = analysis_state.Mt_thread.concurrent_accesses in
