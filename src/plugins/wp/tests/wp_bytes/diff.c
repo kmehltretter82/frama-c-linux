99bc1a27c7da24af056b8d494fabfa2119049559
Allan Blanchard <allan.blanchard@cea.fr>
2026-02-17 13:36:12 +0100

    [wp] further tests loc_diff in Bytes

--- src/plugins/wp/tests/wp_bytes/diff.c
+++ src/plugins/wp/tests/wp_bytes/diff.c
@@ -1,14 +1,15 @@
 /* run.config
-   OPT: -wp-model typed
+   OPT: -wp-model typed -wp-fct f
    OPT: -wp-model bytes
 */
 
 /* run.config_qualif
-   OPT: -wp-model typed
+   OPT: -wp-model typed -wp-fct f
    OPT: -wp-model bytes
 */
 
 /*@
+  requires -10 <= i <= 10 && -10 <= j <= 10 ;
   ensures \result == i - j;
   assigns \nothing;
   */
@@ -16,6 +17,19 @@ int f (int *a, int i, int j)
 {
   int *p = a + i ;
   int *q = a + j ;
+  /*@ probe DIFF: p - q; */
+  return p - q ;
+}
+
+/*@
+  requires -10 <= i <= 10 && -10 <= j <= 10 ;
+  ensures \result == (i - j) * sizeof(int);
+  assigns \nothing;
+  */
+int g (int *a, int i, int j)
+{
+  char *p = a + i ;
+  char *q = a + j ;
   /*@ probe DIFF: p - q; */
   return p - q ;
 }
