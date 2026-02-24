2006a95f41b92ec104398a7ab87d0741bc6d7758
David Bühler <david.buhler@cea.fr>
2026-02-24 17:56:29 +0100

    [Eva] Improves documentation of Domain_store interface.

--- src/plugins/eva/src/domains/domain_store.mli
+++ src/plugins/eva/src/domains/domain_store.mli
@@ -12,9 +12,11 @@ open Eval
 module type InputDomain = sig
   include Datatype.S
 
-  (* The domain name, shown in some logs and in the GUI. *)
+  (** The domain name, used to enable it via -eva-domains. *)
   val name: string
 
+  (** The state representing all possible concrete states. Returned when the
+      analysis has not started, or states have not been saved. *)
   val top: t
 end
 
@@ -22,23 +24,46 @@ end
 module type S = sig
   type t
 
-  (** Called once at the analysis beginning for the entry state of the main
-      function. *)
+  (** Registers the state computed after the initialization of global variables.
+      Called only once at the start of the analysis. *)
   val set_global_state: t -> unit
 
+  (** Registers the initial state for the given function:
+      - for the given [callstack] if provided;
+      - for any callstack otherwise. *)
   val set_initial_state: ?callstack:Callstack.t -> kernel_function -> t -> unit
+
+  (** Registers the state computed before or after a statement:
+      - for the given [callstack] if provided;
+      - for any callstack otherwise. *)
   val set_stmt_state: ?callstack:Callstack.t -> after:bool -> stmt -> t -> unit
 
-  (** Allows accessing the states inferred by an Eva analysis after it has
-      been computed with the domain enabled. *)
+  (** Functions [get_*] below return:
+      - [Domains.top] if no analysis has started or if states are not stored.
+      - otherwise, the state set by the last call to the corresponding [set]
+        function above with the same arguments.
+      - [`Bottom] if no such call has been made. *)
 
+  (** Returns the last state set by [set_global_state], if any.*)
   val get_global_state: unit -> t or_bottom
+
+  (** Returns the last state set by [set_initial_state] for the given function
+      and callstack (if provided). *)
   val get_initial_state: ?callstack:Callstack.t -> kernel_function -> t or_bottom
+
+  (** Returns the last state set by [set_stmt_state] for the given statement
+      and callstack (if provided). *)
   val get_stmt_state: ?callstack:Callstack.t -> after:bool -> stmt -> t or_bottom
 
+  (** Returns all callstacks from previous calls to [set_initial_state] for the
+      given function. *)
   val kf_callstacks: kernel_function -> Callstack.t Seq.t or_top
+
+  (** Returns all callstacks from previous calls to [set_stmt_state ~after:false]
+      for the given statement. *)
   val stmt_callstacks: stmt -> Callstack.t Seq.t or_top
 
+  (** Are states of this domain saved? *)
   val is_enabled: unit -> bool
 end
 
