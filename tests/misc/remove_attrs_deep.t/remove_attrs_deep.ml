a817196f764e1289d8d85fd466db4b2a2fc10025
Virgile Prevosto <virgile.prevosto@cea.fr>
2026-03-06 18:10:51 +0100

    [tests] some unit tests for remove_qualifiers_deep

---
+++ tests/misc/remove_attrs_deep.t/remove_attrs_deep.ml
@@ -0,0 +1,40 @@
+let check ty =
+  let vis = object
+    inherit Cil.nopCilVisitor
+    method! vattr (a,_) =
+      if a = "const" then begin
+        let error =
+          Format.asprintf "Type %a was not cleaned correctly@."
+            Cil_printer.pp_typ ty
+        in
+        failwith error
+      end else Cil.SkipChildren
+  end
+  in
+  ignore (Cil.visitCilType vis ty)
+
+let test ty = check (Ast_types.remove_qualifiers_deep ty)
+
+let tbase = Cil_builder.Type.(const int)
+
+let tptr = Cil_builder.Type.ptr tbase
+
+let tarr = Cil_builder.Type.array tbase
+
+let tptr_arr = Cil_builder.Type.ptr tarr
+
+let tarr_ptr = Cil_builder.Type.array tptr
+
+let tptr_ptr = Cil_builder.Type.(ptr (const tptr))
+
+let tfun = Cil_builder.Type.proto tptr_ptr ["",tptr_arr,[]] false
+
+let () =
+  List.iter test
+    [ Cil_builder.Type.cil_typ tbase;
+      Cil_builder.Type.cil_typ tptr;
+      Cil_builder.Type.cil_typ tarr;
+      Cil_builder.Type.cil_typ tptr_arr;
+      Cil_builder.Type.cil_typ tarr_ptr;
+      Cil_builder.Type.cil_typ tptr_ptr;
+      Cil_builder.Type.cil_typ tfun ]
---
+++ tests/misc/remove_attrs_deep.t/run.t
@@ -0,0 +1,4 @@
+  $ dune build --cache=disabled --root . @install
+
+  $ dune exec --cache=disabled -- ./remove_attrs_deep.exe
+
