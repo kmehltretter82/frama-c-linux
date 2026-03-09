28af69247e3ec122fa0d8f7887c20b0399e04817
Virgile Prevosto <virgile.prevosto@cea.fr>
2026-03-09 18:17:35 +0100

    [tests] having a test module with the same name than a kernel module is a bad idea

--- tests/misc/remove_attrs_deep.ml
+++ tests/misc/remove_attrs_deep.ml
@@ -1,6 +1,6 @@
 let check ty =
   let vis = object
-    inherit Cil.copyCilVisitor
+    inherit Cil.nopCilVisitor
     method! vattr (a,_) =
       if a = "const" then begin
         let error =
@@ -8,13 +8,14 @@ let check ty =
             Cil_printer.pp_typ ty
         in
         failwith error
-      end else Cil.DoChildren
+      end else Cil.SkipChildren
   end
   in
-  let ty' = Cil.visitCilType vis ty in
+  let ty' = Ast_types.remove_qualifiers_deep ty in
+  ignore (Cil.visitCilType vis ty');
   Format.printf "@[<v>%a@;%a@;@;@]" Cil_printer.pp_typ ty Cil_printer.pp_typ ty'
 
-let test ty = check (Ast_types.remove_qualifiers_deep ty)
+let test ty = check ty
 
 let tbase = Cil_builder.Type.(const int)
 
@@ -28,7 +29,9 @@ let tarr_ptr = Cil_builder.Type.array tptr
 
 let tptr_ptr = Cil_builder.Type.(ptr (const tptr))
 
-let tfun = Cil_builder.Type.proto tptr_ptr ["",tptr_arr,[]] false
+let tfun = Cil_builder.Type.proto tptr_ptr ["x",tptr_arr,[]] false
+
+let tfun2 = Cil_builder.Type.proto tptr ["x", tptr,[]; "y", tptr_ptr,[]] false
 
 let () =
   List.iter test
@@ -38,4 +41,6 @@ let () =
       Cil_builder.Type.cil_typ tptr_arr;
       Cil_builder.Type.cil_typ tarr_ptr;
       Cil_builder.Type.cil_typ tptr_ptr;
-      Cil_builder.Type.cil_typ tfun ]
+      Cil_builder.Type.cil_typ tfun;
+      Cil_builder.Type.cil_typ tfun2;
+    ]
