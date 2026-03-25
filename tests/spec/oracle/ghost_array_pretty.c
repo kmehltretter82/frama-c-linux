dc8afdc48f91b4e393eb51b0d0555c5b54e7f7cd
Maxime Jacquemin <maxime.jacquemin@cea.fr>
2026-03-25 17:15:38 +0100

    [Cil_printer] Always display TStartOf constructs as &(tab[0])

--- tests/spec/oracle/ghost_array_pretty.c
+++ tests/spec/oracle/ghost_array_pretty.c
@@ -8,8 +8,8 @@ uint32 s[2];
 /*@
 predicate equal_t_s{L}(ℤ i) =
   \at(t[i] ≡
-      (unsigned long)((uint16)(*((uint8 *)((uint32 *)s) + i) ≡
-                               *(((uint8 *)((uint32 *)s) + i) + 1))),
+      (unsigned long)((uint16)(*((uint8 *)(&(s[0])) + i) ≡
+                               *(((uint8 *)(&(s[0])) + i) + 1))),
       L);
  */
 uint8 Array[10];
