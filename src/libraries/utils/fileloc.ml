3eae240e62d8a1a996c8d63c05468da7e24a3be5
Valentin Perrelle <valentin.perrelle@cea.fr>
2026-05-21 12:01:07 +0200

    [kernel] Factorizes Fileloc's pretty and pretty_long

--- src/libraries/utils/fileloc.ml
+++ src/libraries/utils/fileloc.ml
@@ -19,8 +19,19 @@ module Prototype = struct
   let hash loc = fst loc |> Filepos.hash
   let pretty_debug = pp
 
-  let pretty fmt (pos_start, pos_end) =
-    let pp_path = Filepath.pretty in
+  type formats = {
+    path: (string -> unit) Pretty.format;
+    line: (int -> unit) Pretty.format;
+    line_range: (int -> int -> unit) Pretty.format;
+    column: (int -> unit) Pretty.format;
+    column_range: (int -> int -> unit) Pretty.format;
+    pos_range:
+      (Filepos.t Pretty.aformatter -> Filepos.t ->
+       Filepos.t Pretty.aformatter -> Filepos.t ->
+       unit)  Pretty.format;
+  }
+
+  let pretty_generic ~formats fmt (pos_start, pos_end) =
     if not (Filepos.is_known pos_start) && not (Filepos.is_known pos_end) then
       Filepos.pretty fmt pos_start
     else
@@ -29,51 +40,48 @@ module Prototype = struct
       let path1 = Filepos.path pos_start and path2 = Filepos.path pos_end
       and line1 = Filepos.line pos_start and line2 = Filepos.line pos_end
       and col1 = Filepos.column pos_start and col2 = Filepos.column pos_end in
-      if Filepath.equal path1 path2 then
-        if line1 <= 0 || line2 <= 0 then
-          Format.fprintf fmt "%a" pp_path path1
-        else if line1 = line2 then
-          if col1 <= 0 || col2 <= 0 then
-            Format.fprintf fmt "%a:%d" pp_path path1 line1
-          else if col1 = col2 then
-            Format.fprintf fmt "%a:%d:%d" pp_path path1 line1 col1
-          else
-            Format.fprintf fmt "%a:%d:%d-%d" pp_path path1 line1 col1 col2
-        else
-          Format.fprintf fmt "%a:%d-%d" pp_path path1 line1 line2
-      else
-        Format.fprintf fmt "%a-%a"
+      if Filepath.equal path1 path2 then begin
+        Format.fprintf fmt formats.path (Filepath.to_string path1);
+        if line1 > 0 && line2 > 0 then
+          if line1 <> line2 then
+            Format.fprintf fmt formats.line_range line1 line2
+          else begin
+            Format.fprintf fmt formats.line line1;
+            if col1 > 0 && col2 > 0 then begin
+              if col1 <> col2 then
+                Format.fprintf fmt formats.column_range col1 col2
+              else
+                Format.fprintf fmt formats.column col1
+            end
+          end
+      end else
+        Format.fprintf fmt formats.pos_range
           Filepos.pretty pos_start
           Filepos.pretty pos_end
 
-  let pretty_long fmt (pos_start, pos_end) =
-    let pp_path fmt path = Format.fprintf fmt "%S" (Filepath.to_string path) in
-    if not (Filepos.is_known pos_start) && not (Filepos.is_known pos_end) then
-      Filepos.pretty fmt pos_start
-    else
-      let pos_start = Filepos.original pos_start
-      and pos_end = Filepos.original pos_end in
-      let path1 = Filepos.path pos_start and path2 = Filepos.path pos_end
-      and line1 = Filepos.line pos_start and line2 = Filepos.line pos_end
-      and col1 = Filepos.column pos_start and col2 = Filepos.column pos_end in
-      if Filepath.equal path1 path2 then
-        if line1 <= 0 || line2 <= 0 then
-          Format.fprintf fmt "%a" pp_path path1
-        else if line1 = line2 then
-          if col1 <= 0 || col2 <= 0 then
-            Format.fprintf fmt "%a, line %d" pp_path path1 line1
-          else if col1 = col2 then
-            Format.fprintf fmt "%a, line %d, character %d"
-              pp_path path1 line1 col1
-          else
-            Format.fprintf fmt "%a, line %d, characters %d-%d"
-              pp_path path1 line1 col1 col2
-        else
-          Format.fprintf fmt "%a, lines %d-%d" pp_path path1 line1 line2
-      else
-        Format.fprintf fmt "between %a and %a"
-          Filepos.pretty_long pos_start
-          Filepos.pretty_long pos_end
+  let pretty =
+    let formats =
+      { path = "%s";
+        line = ":%d";
+        line_range = ":%d-%d";
+        column = ":%d";
+        column_range = ":%d-%d";
+        pos_range = "%a-%a"
+      }
+    in
+    pretty_generic ~formats
+
+  let pretty_long =
+    let formats =
+      { path = "%S";
+        line = ", line %d";
+        line_range = ", lines %d-%d";
+        column = ", character %d";
+        column_range = ", characters %d-%d";
+        pos_range = "between %a and %a"
+      }
+    in
+    pretty_generic ~formats
 
   let pretty_long_with_inclusions fmt loc =
     pretty_long fmt loc;
