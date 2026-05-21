27a5083e5398a17f89f10bc19dc2608d2ca31f3f
Valentin Perrelle <valentin.perrelle@cea.fr>
2026-05-21 12:10:50 +0200

    [kernel] Do not remove temporary files twice
    - Temporary files are created with `Temp_files.file` which already uses `cleanup_at_exit`.

--- src/kernel_services/ast_queries/file.ml
+++ src/kernel_services/ast_queries/file.ml
@@ -383,10 +383,6 @@ let pretty_machdep ?fmt ?machdep () =
 (** {2 Initializations} *)
 (* ************************************************************************* *)
 
-let safe_remove_file (f : Filepath.t) =
-  if not (Kernel.KeepTempFiles.get ()) then
-    Temp_files.cleanup_at_exit f
-
 let cpp_name cmd =
   let cmd = List.hd (String.split_on_char ' ' cmd) in
   Filename.basename cmd
@@ -615,7 +611,6 @@ let parse_cabs cpp_command = function
     Kernel.feedback "Parsing %a (with preprocessing)"
       Filepath.pretty f;
     if Sys.command cpp_command <> 0 then begin
-      safe_remove_file ppf;
       abort_with_detailed_pp_message f cpp_command
     end else
       Kernel.debug ~dkey:Kernel.dkey_pp
@@ -645,16 +640,13 @@ let parse_cabs cpp_command = function
                 (replace_in_cpp_cmd cmdl "")
                 ppf
           with Sys_error msg ->
-            safe_remove_file ppf;
             Kernel.abort "preprocessing of annotations failed (%s)" msg
         in
-        safe_remove_file ppf ;
         ppf'
       end else ppf
     in
     let (cil,(_,defs)) = Frontc.parse ~original:f ppf in
     cil.fileName <- f;
-    safe_remove_file ppf;
     (cil,(f,defs))
   | External (f,suf) ->
     if not (Filesystem.exists f) then
@@ -698,7 +690,6 @@ let () =
     in
     let (cil,(_,defs)) = Frontc.parse ~original:path ppf in
     cil.fileName <- path;
-    safe_remove_file ppf;
     (cil,(path,defs))
   in
   new_file_type ".ci" handle
