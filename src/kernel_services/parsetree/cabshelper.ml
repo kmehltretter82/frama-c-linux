48753c44b9121f67bcda03f566d1b98068522b86
Virgile Prevosto <virgile.prevosto@cea.fr>
2026-05-29 16:02:44 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: Valentin Perrelle <valentin.perrelle@cea.fr>

--- src/kernel_services/parsetree/cabshelper.ml
+++ src/kernel_services/parsetree/cabshelper.ml
@@ -54,7 +54,7 @@ struct
 
   let get (first,last) =
     Kernel.debug ~dkey:Kernel.dkey_comments
-      "Searching for comments between positions %a and %a@."
+      "@[<hv>Searching for comments between positions@ %a and@ %a@.@]"
       Filepos.pretty_debug first Filepos.pretty_debug last;
     if not (Filepos.is_known first) || not (Filepos.is_known last)
     then begin
