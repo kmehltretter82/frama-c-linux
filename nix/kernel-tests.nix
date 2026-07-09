f32be125f86e6e3bdc4589209973e43e3985304a
Basile Desloges <basile.desloges@cea.fr>
2026-07-09 17:03:28 +0200

    [nix] Update kernel and plugins derivations

--- nix/kernel-tests.nix
+++ nix/kernel-tests.nix
@@ -4,17 +4,7 @@ mk_tests {
   tests-name = "kernel-tests";
   tests-command = ''
     dune exec -- frama-c-ptests -never-disabled tests
-    dune build -j1 \
-      @tests/cil/ptests \
-      @tests/dynamic/ptests \
-      @tests/jcdb/ptests \
-      @tests/libc/ptests \
-      @tests/misc/ptests \
-      @tests/pretty_printing/ptests \
-      @tests/saveload/ptests \
-      @tests/spec/ptests \
-      @tests/syntax/ptests \
-      @tests/variadic/ptests
+    dune build -j1 @tests/ptests
     dune runtest -j1 tests
     dune build -j1 @runtest-frama_c_kernel @runtest-parsing
     make -C share/machdeps check-schema
