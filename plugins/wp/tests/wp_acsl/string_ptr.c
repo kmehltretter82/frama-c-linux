dc439ef10a0a3d088f91ee53814763df5e4852f9
Loïc Correnson <loic.correnson@cea.fr>
2026-07-23 16:43:36 +0200

    [wp] fixed initialization

---
+++ plugins/wp/tests/wp_acsl/string_ptr.c
@@ -0,0 +1,19 @@
+/* run.config
+  OPT: -wp-no-qed -wp-prop A,Z
+  OPT: -wp-no-qed -wp-prop A,Z -wp-literals
+*/
+
+/* run.config_qualif
+  DONTRUN:
+*/
+
+// String Literals
+
+char *a = "Abc";
+
+//@ assigns \result \from \nothing;
+int main(void) {
+  //@ check A: a[0] == 'A' ;
+  //@ check Z: a[sizeof("Abc")-1] == '\0' ;
+  return 0;
+}
